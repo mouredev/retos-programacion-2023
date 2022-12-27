3844035a453614578cef93717c9c1e1b045fdb8a
Wakicode <joambrus@hotmail.com>
2022-12-27 22:08:11 +0100

    Reto#0

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/kotlin/wakicode.kt
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/kotlin/wakicode.kt
@@ -1,16 +1,4 @@
 
-
-/*
- * Reto#0
- * @author Joaquin Martinez Rus
- * 27dic22
- * Escribe un programa que muestre por consola (con un print) los
- * números de 1 a 100 (ambos incluidos y con un salto de línea entre
- * cada impresión), sustituyendo los siguientes:
- * - Múltiplos de 3 por la palabra "fizz".
- * - Múltiplos de 5 por la palabra "buzz".
- * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
- */
 fun main(args: Array<String>) {
     println((1..100).map { when {
         it % 15 ==0 -> "fizzbuzz"
