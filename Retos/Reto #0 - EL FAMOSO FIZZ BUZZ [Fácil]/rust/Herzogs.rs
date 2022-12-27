dec82b74d2a7f7a7e3b445bf769f08f2180ea51c
Cristian Feldman <crisefeld@gmail.com>
2022-12-27 16:49:58 -0300

    agregado reto #0 en rust

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/rust/Herzogs.rs
@@ -0,0 +1,14 @@
+
+fn main() {
+    for num in 1..100{
+        if num%3==0 && num %5==0{
+         println!("{} - fizzbuzz",num);
+        }else if num%3 == 0{
+         println!("{} - fizz",num);
+        }else if num%5==0{
+         println!("{} - buzz",num);
+        }else{
+         println!("{}",num);
+        }
+    }
+}
