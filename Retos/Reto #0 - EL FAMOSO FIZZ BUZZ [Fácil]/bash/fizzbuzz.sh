6ef947128a6e8a7dea58569cea74f56acbca5faa
veoDoble <gustavoguerrerom@gmail.com>
2022-12-26 23:58:16 -0300

    fix

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/bash/fizzbuzz.sh
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/bash/fizzbuzz.sh
@@ -7,15 +7,12 @@ while [ $fin -ge $contador ]
 do
   if [[ $(($contador % 3)) == 0 ]] && [[ $(($contador % 5)) == 0 ]]
   then
-      echo $contador
       echo "fizzbuzz"
   elif  [[ $(($contador % 5)) == 0 ]]
   then
-      echo $contador
       echo "buzz"
   elif [[ $(($contador % 3)) == 0 ]]
   then
-      echo $contador
       echo "fizz"
   fi
   let contador++
