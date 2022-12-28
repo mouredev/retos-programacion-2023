13e207308ec8194cf1829014be1b56470f02fe5a
nahueld97 <nahueld97@outlook.com>
2022-12-28 12:07:46 -0300

    Solucion reto #0

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/Solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=100;i++)
+		{
+			if(i%3==0)
+			{
+				if(i%5==0)
+					System.out.println("fizzbuzz");
+				else
+					System.out.println("fizz");
+			}
+			else
+			{
+				if(i%5==0)
+					System.out.println("buzz");
+				else
+					System.out.println(i);
+			}
+		}
+
+	}
+
+}
