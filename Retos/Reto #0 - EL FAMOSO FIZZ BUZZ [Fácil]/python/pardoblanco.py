6c498c482b908d4ad30b3f9672db0de6cdf3cddf
Alberto Pardo <alberto.pardo@ricohsom.com>
2022-12-28 11:27:07 +0100

    Cambio el iterable 'numero' por la letra 'n'

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/python/pardoblanco.py
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/python/pardoblanco.py
@@ -1,9 +1,9 @@
-for numero in range(1, 100):
-    if numero%3 == 0 and numero%5 == 0:
+for n in range(1, 100):
+    if n%3 == 0 and n%5 == 0:
         print("fizzbuzz\n")
-    elif numero%5 == 0:
+    elif n%5 == 0:
         print("buzz\n")
-    elif  numero%3 == 0:
+    elif  n%3 == 0:
         print("fizz\n")
     else:
-        print("{}\n".format(numero))
+        print("{}\n".format(n))
