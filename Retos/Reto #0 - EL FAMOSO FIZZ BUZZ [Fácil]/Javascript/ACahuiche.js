ebe5226d7c17ddb01e1e46e747ab67ed1610835f
ACahuiche <alberto.cahuiche@outlook.com>
2022-12-28 20:32:00 -0600

    Javascript - Ejercicio 0

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/Javascript/ACahuiche.js
@@ -0,0 +1,18 @@
+function fizzbuzz() {
+    for(var i = 1; i <= 100; i++) {
+      if(i % 3 == 0) {
+        if(i % 5 == 0) {
+          console.log('Fizzbuzz');
+        }
+        else {
+          console.log('Fizz');
+        }
+      }
+      else if(i % 5 == 0) {
+          console.log('Buzz');
+      }
+      else {
+        console.log(i);
+      }
+    }
+  }
