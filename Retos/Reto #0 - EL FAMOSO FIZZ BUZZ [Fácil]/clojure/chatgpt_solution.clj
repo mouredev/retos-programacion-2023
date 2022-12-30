3a8ad3fd8bafdfb4f7c570938b19815c6fbb978e
Jorge Diaz <jorge7soccer@gmail.com>
2022-12-30 04:45:49 +0000

    Add the prompt I gave ChatGPT

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/clojure/chatgpt_solution.clj
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/clojure/chatgpt_solution.clj
@@ -1,5 +1,15 @@
 (ns chatgpt-solution)
 
+; Write me a program in Clojure.
+; These are the instructions:
+; A program that prints out numbers 1 to 100
+; on a newline while replacing numbers that
+; are multiples of 3 with the string “fizz”,
+; replacing numbers that are multiples of 5
+; with the string “buzz”, and replacing the
+; numbers that are multiple of both 3 & 5
+; with the string “fizzbuzz”.
+
 (defn fizzbuzz []
   (doseq [i (range 1 101)]
     (if (and (zero? (mod i 3)) (zero? (mod i 5)))
