5e19b7735f19e829b88e09e86310cdb76ee8b775
RicardoZM <ricardo_zorita@hotmai.com>
2023-01-02 23:13:16 +0100

    Reto #1

---
+++ Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/java/ricardozm.java
@@ -0,0 +1,8 @@
+/*
+ * Escribe un programa que reciba un texto y transforme lenguaje natural a
+ * "lenguaje hacker" (conocido realmente como "leet" o "1337"). Este lenguaje
+ *  se caracteriza por sustituir caracteres alfanuméricos.
+ * - Utiliza esta tabla (https://www.gamehouse.com/blog/leet-speak-cheat-sheet/) 
+ *   con el alfabeto y los números en "leet".
+ *   (Usa la primera opción de cada transformación. Por ejemplo "4" para la "a")
+ */
