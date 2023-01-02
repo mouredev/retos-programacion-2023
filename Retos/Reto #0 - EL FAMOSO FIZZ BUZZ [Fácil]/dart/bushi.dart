fc9b3a073a94f1ad96cfb73eac1724c7975be5c5
Joaquin Rivero <rivero.dev@gmail.com>
2023-01-02 20:56:40 +0100

    Little Fix

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/dart/bushi.dart
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/dart/bushi.dart
@@ -1,14 +1,19 @@
 void main() {
+  _fizzbuzz();
+}
+
+void _fizzbuzz() {
   for (var i = 1; i <= 100; i++) {
-    var result = "$i. ";
-
-    if (i % 3 == 0) {
-      result += "fizz";
+    if (i % 3 == 0 && i % 5 == 0) {
+      print("fizzbuzz\n");
     }
-    if (i % 5 == 0) {
-      result += "buzz";
+    else if (i % 3 == 0) {
+      print("fizz\n");
     }
-
-    print("$result\n");
+    else if (i % 5 == 0) {
+      print("buzz\n");
+    } else {
+      print("$i\n");
+    }
   }
 }
--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/kotlin/bushi.kt
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/kotlin/bushi.kt
@@ -1,14 +1,17 @@
 fun main() {
+    fizzbuzz()
+}
+
+private fun fizzbuzz() {
     for (i in 1..100) {
-        var result = "$i. "
-
-        if (i % 3 == 0) {
-            result += "fizz"
+        if (i % 3 == 0 && i % 5 == 0) {
+            println("fizzbuzz")
+        } else if (i % 3 == 0) {
+            println("fizz")
+        } else if (i % 5 == 0) {
+            println("buzz")
+        } else {
+            println(i)
         }
-        if (i % 5 == 0) {
-            result += "buzz"
-        }
-
-        println(result)
     }
 }
