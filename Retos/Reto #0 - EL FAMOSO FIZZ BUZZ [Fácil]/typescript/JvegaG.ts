f97ad1453ff40c3c8640011a7e398a500bdf47d7
Jesus Vega <jesvega.93@gmail.com>
2023-01-02 15:16:29 -0500

    fix print in console

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/typescript/JvegaG.ts
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/typescript/JvegaG.ts
@@ -1,12 +1,19 @@
+/*
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */
 
-
-export const fizzBuzzFunction = (value : number): string => {
+export const fizzBuzzFunction = (value: number): string => {
     if( value % 3 === 0 && value % 5 === 0)
-        return `${value} - fizzbuzz`;
+        return `fizzbuzz`;
     else if (value % 3 === 0)
-        return `${value} - fizz`;
+        return `fizz`;
     else if (value % 5 === 0)
-        return `${value} - buzz`;
+        return `buzz`;
     else
         return `${value}`;
 }
