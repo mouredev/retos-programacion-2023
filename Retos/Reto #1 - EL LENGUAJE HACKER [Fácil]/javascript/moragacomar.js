76c0a5f432c82a82b34c3a42337f8d05ec17f9d4
Juan Moraga <poluxfc@gmail.com>
2023-01-02 18:17:39 -0300

    remove keys function

--- Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/javascript/moragacomar.js
+++ Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/javascript/moragacomar.js
@@ -14,9 +14,8 @@ const lenguaje = {
     "a": "4", "b": "I3", "c": "[", "d": ")", "e": "3", "f": "|=", "g": "&", "h": "#", "i": "1", "j": ",_|", "k": ">|", "l": "1", "m": "/\/\\", "n": "^/", "o": "0", "p": "|*", "q": "(_,)", "r": "I2", "s": "5", "t": "7", "u": "(_)", "v": "\/", "w": "\/\/", "x": "><", "y": "j", "z": "2", "1": "L", "2": "R", "3": "E", "4": "A", "5": "S", "6": "b", "7": "T", "8": "B", "9": "g", "0": "o"
 }
 
-const sustituir = (mensaje) => {
-    return mensaje.toLowerCase().split("").map(e => lenguaje[e]).join("")
-}
+const sustituir = (mensaje) => mensaje.toLowerCase().split("").map(e => lenguaje[e]).join("")
+
 
 const mensajeHacker = sustituir("HOLA, Juan Manuel 2023");
 
