c80182a5c0058e4c1bb39a838dd8c1c687d36ee5
Carlos A <cayala.w@gmail.com>
2023-01-03 00:34:48 -0500

    Reto #1 - Ruby

--- Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/ruby/test0n3.rb
+++ Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/ruby/test0n3.rb
@@ -2,10 +2,10 @@
 
 HACKER_ALPHABET = { 'A': '4', 'B': 'I3', 'C': '[', 'D': ')', 'E': '3', 'F': '|=',
                     'G': '&', 'H': '#', 'I': '1', 'J': ',_|', 'K': '>|', 'L': '1',
-                    'M': '[V]', 'N': '/V', 'O': '0', 'P': '|*', 'Q': '(_,)',
-                    'R': 'I2', 'S': '5', 'T': '7', 'U': '(_)', 'V': '|/', 'W': 'VV',
+                    'M': '/\\/\\', 'N': '^/', 'O': '0', 'P': '|*', 'Q': '(_,)',
+                    'R': 'I2', 'S': '5', 'T': '7', 'U': '(_)', 'V': '\/', 'W': '\/\/',
                     'X': '><', 'Y': 'j', 'Z': '2', '1': 'L', '2': 'R', '3': 'E', '4': 'A',
-                    '5': 'S', '6': 'b', '7': 'T', '8': 'B', '9': 'q', '0': 'o' }.freeze
+                    '5': 'S', '6': 'b', '7': 'T', '8': 'B', '9': 'g', '0': 'o' }.freeze
 
 def hacker_lang(input)
   input.upcase.chars.map do |char|
@@ -19,8 +19,10 @@ end
 
 TESTS = { input: ['some words', 'SOME WORDS', 'mixEd wOrdS and num63|22',
                   'wOrdS, num63|22; and punctuation.'],
-          output: ['50[V]3 VV0I2)5', '50[V]3 VV0I2)5', '[V]1><3) VV0I2)5 4/V) /V(_)[V]bE|RR',
-                   'VV0I2)5, /V(_)[V]bE|RR; 4/V) |*(_)/V[7(_)4710/V.'] }.freeze
+          output: ['50/\\/\\3 \/\/0I2)5',
+                   '50/\\/\\3 \/\/0I2)5',
+                   '/\\/\\1><3) \/\/0I2)5 4^/) ^/(_)/\\/\\bE|RR',
+                   '\/\/0I2)5, ^/(_)/\\/\\bE|RR; 4^/) |*(_)^/[7(_)4710^/.'] }.freeze
 
 errors = 0
 TESTS[:input].each_with_index do |test, index|
