e4393a7f24ee146a6844a4f3cba0b1391249f92e
Verónica Martínez <117473130+VeroMarpe@users.noreply.github.com>
2023-01-03 17:44:01 +0100

    Update veromarpe.py

--- Reto #0 | EL FAMOSO "FIZZ BUZZ” | Fácil/Python/veromarpe.py
+++ Reto #0 | EL FAMOSO "FIZZ BUZZ” | Fácil/Python/veromarpe.py
@@ -1,11 +1,14 @@
-for i in range (1,101):
+def fizzbuzz (): 
 
-    if i%3==0 and i%5!=0:
-       print("fizz") 
-    if i%5==0 and i%3!=0:
-        print("buzz")
-    if i%3==0 and i%5==0:
-        print("fizzbuzz")
-    if i%3!=0 and i%5!=0:
-        print(i)
+    for i in range (1,101):
+
+        if number % 3 == 0 and number % 5 == 0:
+            print("fizzbuzz") 
+        elif number % 3 == 0:
+            print("fizz")
+        elif number % 5 == 0:
+            print("buzz")
+        else:
+            print (number)
+       
         
