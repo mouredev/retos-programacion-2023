35157ecbff845c59df8dfd5978d57b47c0a0395b
jg20797 <jg20797@essex.ac.uk>
2023-01-04 02:03:04 +0100

    Solucion en Java

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/java/josevalverde.java
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/java/josevalverde.java
@@ -7,6 +7,8 @@ public class josevalverde {
                 System.out.println("Fizz");
             } else if (i % 5 == 0) {
                 System.out.println("Buzz");
+            } else {
+                System.out.println(i);
             }
         }
     }
