fdb1ee257367cc89e387c8fe0c63d6aac6a5d145
jg20797 <jg20797@essex.ac.uk>
2023-01-04 02:07:29 +0100

    Cambio de nombre de archivos

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/java/josevalver.java
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/java/josevalver.java
@@ -1,4 +1,4 @@
-public class josevalverde {
+public class josevalver {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
