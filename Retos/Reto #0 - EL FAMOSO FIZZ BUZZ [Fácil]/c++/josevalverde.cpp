fb87d3b82a6caa5c12f82ac7f1157acddabb5fef
jg20797 <jg20797@essex.ac.uk>
2023-01-04 02:00:54 +0100

    Solucion en C++

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/c++/josevalverde.cpp
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/c++/josevalverde.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    for (int i = 0; i <= 100; i++) {
+    for (int i = 1; i <= 100; i++) {
         if (i % 3 == 0 && i % 5 == 0) {
             printf("FizzBuzz\n");
         } else if (i % 3 == 0) {
