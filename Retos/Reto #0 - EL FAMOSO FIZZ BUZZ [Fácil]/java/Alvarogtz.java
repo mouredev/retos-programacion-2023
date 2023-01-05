f659c5444dd430cdb2c9c7cb93e4e25399f3ed98
Alvaro Zabala Gutierrez <alvaro.zabala@servernt.local>
2023-01-05 12:46:36 +0100

    Reto #0 - Java

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/java/Alvarogtz.java
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/java/Alvarogtz.java
@@ -10,12 +10,12 @@
 public class Alvarogtz {
     private static  void main(String[] args){
         for(int i = 1; i< 101; i++){
-            if(i%3 == 0){
-                System.out.println("flizz");
+            if(i%3 == 0 && i%5 == 0) {
+                System.out.println("fizzbuzz");
+            } else if(i%3 == 0){
+                System.out.println("fizz");
             }else if(i%5 == 0){
                 System.out.println("buzz");
-            }else if(i%3 == 0 && i%5 == 0) {
-                System.out.println("flizzbuzz");
             }else {
                 System.out.println(i);
             }
