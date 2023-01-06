554f3acb8557fecbc33407da01e86aae7b59401d
Ivan Hernandez <edgar.hernandez@moviro.mx>
2023-01-05 23:40:03 -0600

    Revert "Reto #0 - Typescript"
    This reverts commit f5612428acb30f26af1f92c1147ecdef743d9876.

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/typescript/IvanHernandezHe.ts
+++
@@ -1,9 +0,0 @@
-FizzBuzz(100)
-
-function FizzBuzz (limit)
-{
-    for (let i = 1; i <= limit; i++)
-    {
-        console.log((( i % 3 == 0) && ( i % 5 == 0)) ? "Fizzbuzz" : ( i % 3 == 0) ? "Fizz" : ( i % 5 == 0) ? "Buzz" : i)
-    }
-}
