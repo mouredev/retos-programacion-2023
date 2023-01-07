6254ff41c1aa10497e8bb43f6ef2d5ccdef9d1af
adrianpisabarrogarcia <adrian.pisabarro.garcia@gmail.com>
2023-01-07 17:56:42 +0100

    Reto #0 - javascript

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/javascript/adriantxupisi.js
@@ -0,0 +1,24 @@
+/*
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */
+
+for(let number = 1; number <= 100; number++){
+  console.log(fizzbuzz(number))
+}
+
+
+function fizzbuzz(number){
+  if(number % 3 == 0 && number % 5 == 0){
+    return "fizzbuzz"
+  }else if(number % 3 == 0){
+    return "fizz"
+  }else if(number % 5 == 0){
+    return "buzz"
+  }
+  return number
+}
