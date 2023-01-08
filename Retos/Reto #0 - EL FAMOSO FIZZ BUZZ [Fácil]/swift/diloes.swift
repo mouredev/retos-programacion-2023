dd95c8a7a17b95da4817168023afbae63e5f45cd
diloes <diloes@hotmail.com>
2023-01-09 00:39:52 +0100

    diloes add

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/swift/diloes.swift
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/swift/diloes.swift
@@ -9,12 +9,12 @@
 
 func resolveChallenge(){
     for index in 1...100{
-        if index % 3 == 0 {
-            print("fizz")
+        if index % 3 == 0 && index % 5 == 0 {
+            print("fizzbuzz")
         } else if index % 5 == 0 {
             print("buzz")
-        } else if index % 3 == 0 && index % 5 == 0 {
-            print("fizzbuzz")
+        } else if index % 3 == 0 {
+            print("fizz")
         } else {
             print(index)
         }
