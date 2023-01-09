96fb4d27d3976dae75822120f3d6bf71724b3e3d
leo-cerdan <leo.cerdan@gmail.com>
2023-01-09 09:36:17 +0100

    Solución Reto #1 - Python

--- Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/python/leo-cerdan.py
+++ Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/python/leo-cerdan.py
@@ -12,7 +12,8 @@ def lenguaje_hacker(text):
         "a":"4", "á":"4", "b":"I3", "c":"[", "d":")", "e":"3", "é":"3", "f":"|=", "g":"&",
         "h":"#", "i":"1", "í":"1", "j":",_|", "k":">|", "l":"1", "m":"/\/\ ", "n":"^/",
         "o":"0", "ó":"0", "p":"|*", "q":"(_,)", "r":"I2", "s":"5", "t":"7", "u":"(_)",
-        "ú":"(_)", "v":"\/", "w":"\/\/", "x":"><", "y":"j", "z":"2"
+        "ú":"(_)", "v":"\/", "w":"\/\/", "x":"><", "y":"j", "z":"2", "1":"L", "2":"R",
+        "3":"E", "4":"A", "5":"S", "6":"b", "7":"T", "8":"B", "9":"g", "0":"o"
     }
 
     text = text.lower()
@@ -28,5 +29,6 @@ def main():
     print(lenguaje_hacker("Reto hecho en Python"))
     print(lenguaje_hacker("Máquina"))
     print(lenguaje_hacker("FUSIÓN"))
+    print(lenguaje_hacker("51786"))
 
 main()
