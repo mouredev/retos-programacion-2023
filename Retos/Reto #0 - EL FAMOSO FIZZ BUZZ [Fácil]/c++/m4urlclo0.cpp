8331418ac8217b67b52498a3b6ee06f029460acd
m4urlcl0 <84283424+m4urlclo0@users.noreply.github.com>
2023-01-09 21:10:37 -0300

    Reto #0 - C++

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/c++/m4urlclo0.cpp
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/c++/m4urlclo0.cpp
@@ -1,48 +1,28 @@
-ï»¿/*
- * Escribe un programa que reciba un texto y transforme lenguaje natural a
- * "lenguaje hacker" (conocido realmente como "leet" o "1337"). Este lenguaje
- *  se caracteriza por sustituir caracteres alfanumÃ©ricos.
- * - Utiliza esta tabla (https://www.gamehouse.com/blog/leet-speak-cheat-sheet/)
- *   con el alfabeto y los nÃºmeros en "leet".
- *   (Usa la primera opciÃ³n de cada transformaciÃ³n. Por ejemplo "4" para la "a")
+/*
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
  */
 
 #include <iostream>
-#include <string>
-#include <map>
-
-std::map<char, std::string> alphabet = {
-	{'a', "4"},      {'b', "I3"},
-	{'c', "["},      {'d', ")"},
-	{'e', "3"},      {'f', "|="},
-	{'g', "&"},      {'h', "#"},
-	{'i', "1"},      {'j', ",_|"},
-	{'k', ">|"},     {'l', "1"},
-	{'m', "[]V[]"},  {'n', "^/"},
-	{'o', "0"},      {'p', "|*"},
-	{'q', "(_,)"},   {'r', "I2"},
-	{'s', "5"},      {'t', "7"},
-	{'u', "(_)"},    {'v', "|/"},
-	{'w', "VV"},     {'x', "><"},
-	{'y', "j"},      {'z', "2"},
-	{' ', " "}
-};
-
-std::string leetHackerAlphabet(std::string Text) {
-	std::string _Text = "";
-	for (int i = 0; i < Text.length(); i++) {
-		_Text += alphabet[tolower(Text[i])];
-	}
-	return _Text;
-}
 
 int main() {
-	std::string TextPru = "";
-	std::cout << "m4urlclo0 " << char(26) << " " << leetHackerAlphabet("m4urlclo0") << std::endl;
-	std::cout << "mouredev  " << char(26) << " " << leetHackerAlphabet("mouredev")  << std::endl;
-	std::cout << "\nIngrese un texto de prueba: ";
-	std::getline(std::cin, TextPru);
-	std::cout << "Texto Ingresado " << char(26) << " " << leetHackerAlphabet(TextPru) << std::endl;
-	std::system("pause");
+	for (int i = 1; i <= 100; i++) {
+		if (i % 3 == 0 && i % 5 == 0) {
+			std::cout << "fizzbuzz" << std::endl;
+		} 
+		else if (i % 5 == 0) {
+			std::cout << "buzz" << std::endl;
+		} 
+		else if (i % 3 == 0) {
+			std::cout << "fizz" << std::endl;
+		} 
+		else {
+			std::cout << i << std::endl;
+		}
+	}
 	return 0;
 }
