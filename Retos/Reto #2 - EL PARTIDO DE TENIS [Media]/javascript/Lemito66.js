0a580d5502fde04cd3c828d144644d099284f460
Emill <lemito66@gmail.com>
2023-01-10 16:54:46 -0500

    error al momento de saber quien gana

--- Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/javascript/Lemito66.js
+++ Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/javascript/Lemito66.js
@@ -2,7 +2,7 @@
  * Escribe un programa que muestre cómo transcurre un juego de tenis y quién lo ha ganado.
  * El programa recibirá una secuencia formada por "P1" (Player 1) o "P2" (Player 2), según quien
  * gane cada punto del juego.
- * 
+ *
  * - Las puntuaciones de un juego son "Love" (cero), 15, 30, 40, "Deuce" (empate), ventaja.
  * - Ante la secuencia [P1, P1, P2, P2, P1, P2, P1, P1], el programa mostraría lo siguiente:
  *   15 - Love
@@ -13,29 +13,53 @@
  *   Deuce
  *   Ventaja P1
  *   Ha ganado el P1
- * - Si quieres, puedes controlar errores en la entrada de datos.   
- * - Consulta las reglas del juego si tienes dudas sobre el sistema de puntos.   
+ * - Si quieres, puedes controlar errores en la entrada de datos.
+ * - Consulta las reglas del juego si tienes dudas sobre el sistema de puntos.
  */
 
 const juegoDelTenis = (arregloDePunto) => {
-    jugadores = {
-        P1:0,
-        P2:0,
+  jugadores = {
+    P1: 0,
+    P2: 0,
+  };
+  marcador = ["Love", "15", "30", "40"];
+  try {
+    if (marcador.length > 0) {
+      for (let puntos of arregloDePunto) {
+        jugadores[puntos.toUpperCase()] += 1;
+        imprimirResultados(jugadores, marcador);
+      }
     }
-    marcador = ['Love', '15', '30', '40']
-    try {
-        if (length(marcador) > 0){
-            for (const puntos of arregloDePunto) {
-                jugadores[puntos.toUpperCase()] +=1
-                if (jugadores['P1'] === 3 && jugadores['P2'] ===3) {
-                    console.log('Deuce');
-                } else {
-                    
-                }
-            }
-        }
-    } catch (error) {
-        console.log(error);
+    else{
+        console.log('La lista esta vacia');
     }
-}
+  } catch (error) {
+    console.log(error);
+  }
 
+};
+
+const imprimirResultados = (jugadores, marcador) => {
+    let diferenciaDePunto = 0;
+    if (jugadores["P1"] === 3 && jugadores["P2"] === 3) {
+      console.log("Deuce");
+    } else if (jugadores["P1"] >= 4 || jugadores["P1"] >= 4) {
+      diferenciaDePuntos = jugadores["P1"] - jugadores["P2"];
+      if (diferenciaDePunto === 0) {
+        console.log("Deuce");
+      } else if (diferenciaDePuntos === 1) {
+        console.log("Ventaja P1");
+      } else if (diferenciaDePuntos === -1) {
+        console.log("Ventaja P2");
+      } else if (diferenciaDePuntos >= 2) {
+        console.log("Game P1");
+      } else {
+        console.log("Game P2");
+      }
+    } else {
+      console.log(`${marcador[jugadores["P1"]]}-${marcador[jugadores["P2"]]}`);
+    }
+  }
+
+
+juegoDelTenis(['P1', 'P1', 'P1','P1'])
