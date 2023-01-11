3d84be8d8a264969f3edbb985268f9158e2a31d0
David Lombera <david.lomberlan@gmail.com>
2023-01-11 03:37:59 -0600

    Refactor to structs

--- Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/go/lombervid.go
+++ Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/go/lombervid.go
@@ -10,10 +10,137 @@ import (
 	"strings"
 )
 
+var scoreDescriptions = map[int]string{
+	0: "Love",
+	1: "15",
+	2: "30",
+	3: "40",
+}
+
+/******** player declaration ********/
+type player struct {
+	score int
+}
+
+func (p *player) Score() int {
+	return p.score
+}
+
+func (p *player) IncreaseScore() {
+	p.score++
+}
+
+func (p *player) ScoreDescription() string {
+	return scoreDescriptions[p.Score()]
+}
+
+/******** TennisMatch declaration ********/
+type TennisMatch struct {
+	player1      player
+	player2      player
+	isFinished   bool
+	scoreHistory []string
+}
+
+// Private methods
+func (tm *TennisMatch) addHistory(record string) {
+	tm.scoreHistory = append(tm.scoreHistory, record)
+}
+
+func (tm *TennisMatch) addScore() {
+	switch true {
+	case tm.IsTie() && (tm.player1.Score() >= 3):
+		tm.addHistory("Deuce")
+	case (tm.player1.Score() <= 3) && (tm.player2.Score() <= 3):
+		tm.addHistory(fmt.Sprintf("%s - %s", tm.player1.ScoreDescription(), tm.player2.ScoreDescription()))
+	case (tm.player1.Score() - tm.player2.Score()) > 1:
+		tm.addHistory("Ha ganado el Player 1")
+		tm.FinishGame()
+	case (tm.player2.Score() - tm.player1.Score()) > 1:
+		tm.addHistory("Ha ganado el Player 2")
+		tm.FinishGame()
+	case tm.player1.Score() > tm.player2.Score():
+		tm.addHistory("Ventaja Player 1")
+	case tm.player2.Score() > tm.player1.Score():
+		tm.addHistory("Ventaja Player 2")
+	}
+}
+
+// Public methods
+func (tm *TennisMatch) IsTie() bool {
+	return tm.player1.Score() == tm.player2.Score()
+}
+
+func (tm *TennisMatch) FinishGame() {
+	tm.isFinished = true
+}
+
+func (tm *TennisMatch) HasFinished() bool {
+	return tm.isFinished
+}
+
+func (tm *TennisMatch) Result() {
+	if len(tm.scoreHistory) == 0 {
+		return
+	}
+
+	center := func(s string, w int) string {
+		return fmt.Sprintf("%[1]*s", -w, fmt.Sprintf("%[1]*s", (w+len(s))/2, s))
+	}
+
+	fmt.Println(strings.Repeat("—", 30))
+	fmt.Println(center("Match Result", 30))
+	fmt.Println(strings.Repeat("—", 30))
+	fmt.Println("")
+
+	for _, score := range tm.scoreHistory {
+		width := 28
+		if strings.Contains(score, "Love") {
+			width = 30
+		}
+		fmt.Println(center(score, width))
+	}
+
+	fmt.Println("")
+	fmt.Println(strings.Repeat("—", 30))
+	fmt.Println("")
+}
+
+func (tm *TennisMatch) Start() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for !tm.HasFinished() {
+		clear()
+		tm.Result()
+
+		input := getInput(reader, "Insert winning player (P1/P2): ")
+		switch strings.ToUpper(input) {
+		case "P1":
+			tm.player1.IncreaseScore()
+			tm.addScore()
+		case "P2":
+			tm.player2.IncreaseScore()
+			tm.addScore()
+		default:
+			fmt.Printf("\nInvalid option %q, please select between P1/P2. ", input)
+			getInput(reader, "Press 'Enter' key to continue...")
+		}
+	}
+
+	clear()
+	tm.Result()
+}
+
+/******** TennisMatch constructor ********/
+func NewTennisMatch() *TennisMatch {
+	return &TennisMatch{}
+}
+
+/******** Normal functions ********/
 func getInput(r *bufio.Reader, msg string) string {
 	fmt.Print(msg)
-	input, err := r.ReadString('\n')
 
+	input, err := r.ReadString('\n')
 	if err != nil {
 		log.Fatal("An error has ocurred reading the input.")
 	}
@@ -36,89 +163,6 @@ func clear() {
 	}
 }
 
-func printResult(scores []string) {
-	if len(scores) == 0 {
-		return
-	}
-
-	center := func(s string, w int) string {
-		return fmt.Sprintf("%[1]*s", -w, fmt.Sprintf("%[1]*s", (w+len(s))/2, s))
-	}
-
-	fmt.Println(strings.Repeat("—", 30))
-	fmt.Println(center("Match Result", 30))
-	fmt.Println(strings.Repeat("—", 30))
-	fmt.Println("")
-
-	for _, score := range scores {
-		width := 28
-		if strings.Contains(score, "Love") {
-			width = 30
-		}
-		fmt.Println(center(score, width))
-	}
-
-	fmt.Println("")
-	fmt.Println(strings.Repeat("—", 30))
-	fmt.Println("")
-}
-
-func getScore(p1 int, p2 int) (score string, finished bool) {
-	scoreTerms := map[int]string{0: "Love", 1: "15", 2: "30", 3: "40"}
-	p1Score := scoreTerms[p1]
-	p2Score := scoreTerms[p2]
-
-	switch true {
-	case (p1 == p2) && (p1 >= 3):
-		score = "Deuce"
-	case (p1 <= 3) && (p2 <= 3):
-		score = fmt.Sprintf("%s - %s", p1Score, p2Score)
-	case (p1 - p2) > 1:
-		score = "Ha ganado P1"
-		finished = true
-	case (p2 - p1) > 1:
-		score = "Ha ganado P2"
-		finished = true
-	case p1 > p2:
-		score = "Ventaja P1"
-	case p2 > p1:
-		score = "Ventaja P2"
-	}
-
-	return score, finished
-}
-
-func collectScores() []string {
-	var p1, p2 int
-	var finished bool
-	scores := make([]string, 0)
-	reader := bufio.NewReader(os.Stdin)
-
-	for !finished {
-		var score string
-		printResult(scores)
-		input := getInput(reader, "Insert winning player (P1/P2): ")
-
-		switch strings.ToUpper(input) {
-		case "P1":
-			p1++
-			score, finished = getScore(p1, p2)
-			scores = append(scores, score)
-		case "P2":
-			p2++
-			score, finished = getScore(p1, p2)
-			scores = append(scores, score)
-		default:
-			fmt.Printf("\nInvalid option %q, please select between P1/P2. ", input)
-			getInput(reader, "Press 'Enter' key to continue...")
-		}
-		clear()
-	}
-
-	return scores
-}
-
 func main() {
-	scores := collectScores()
-	printResult(scores)
+	NewTennisMatch().Start()
 }
