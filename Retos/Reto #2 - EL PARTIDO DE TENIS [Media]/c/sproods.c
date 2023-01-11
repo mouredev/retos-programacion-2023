e4a71d6718e32c98a0bc77f71fc8a3a9731bbf4d
sproods <106177602+sproods@users.noreply.github.com>
2023-01-11 11:21:03 -0500

    funciones de manejo de los puntos y escritura de la tabla de anotaciones.

--- Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/c/sproods.c
+++ Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/c/sproods.c
@@ -1,6 +1,7 @@
 /* Reto #2 - EL PARTIDO DE TENIS */
 
 #include <stdio.h>
+#include <string.h>
 
 typedef struct
 {
@@ -8,21 +9,68 @@ typedef struct
     int puntaje;
 } jugador;
 
+void datos(jugador *);          // Prototipos de funciones
+void puntos(jugador *);
+void tabla_Puntuaciones(jugador *);
+
 void main(void)
 {
-    jugador nombre_puntos[2];
+    jugador nombre[2];
+    int i;
+
+    nombre[0].puntaje = 0;
+    nombre[1].puntaje = 0;
+
+    datos(nombre);
+
+    while (nombre[0].puntaje < 3 && nombre[1].puntaje < 3)
+    {
+        puntos(nombre);
+        tabla_Puntuaciones(nombre);
+    }
+}
+
+void datos(jugador name[2])
+{
     int i;
 
     for (i = 0; i < 2; i++)
     {
-        printf("\nDigite el nombre del jugador %d: ", i + 1);
-        gets(nombre_puntos[i].nombre);
-
-        printf("Digite el puntaje del jugador %d: ", i + 1);
-        scanf("%d", &nombre_puntos[i].puntaje);
+        printf("\nEscriba el nombre del jugador %d: ", i + 1);
+        gets(name[i].nombre);
         fflush(stdin);
     }
+}
 
-    for (i = 0; i < 2; i++)
-        printf("\nNombre_jugador%d:\t%s\nPuntos_jugador%d:\t%d\n", i + 1, nombre_puntos[i].nombre, i + 1, nombre_puntos[i].puntaje);
+void puntos(jugador name[2])
+{
+    int i;
+
+    do
+    {
+        printf("\nDigite quién hizo punto - (1. %s o 2. %s): ", name[0].nombre, name[1].nombre);
+        scanf("%d", &i);
+    }
+    while (i != 1 && i != 2);
+
+    name[i - 1].puntaje++;
+}
+
+void tabla_Puntuaciones(jugador name[2])
+{
+    int len1, len2, i;
+
+    len1 = strlen(name[0].nombre);
+    len2 = strlen(name[1].nombre);
+
+    printf("\n%s\t%s\n", name[0].nombre, name[1].nombre);
+    for (i = 0; i < len1 / 2; i++)
+        printf(" ");
+    printf("%d", name[0].puntaje);
+    for (i = 0; i < len1 / 2; i++)
+        printf(" ");
+    printf("\t");
+    for (i = 0; i < len2 / 2; i++)
+        printf(" ");
+    printf("%d", name[1].puntaje);
 }
