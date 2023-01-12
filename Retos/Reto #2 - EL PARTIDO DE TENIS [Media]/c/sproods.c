3b21dd2bd2b56ef00e72b05ae8981c662497ca9c
sproods <106177602+sproods@users.noreply.github.com>
2023-01-12 10:29:26 -0500

    condición de la estructura repetitiva e intento de nomenclatura

--- Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/c/sproods.c
+++ Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/c/sproods.c
@@ -2,31 +2,37 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 typedef struct
 {
     char nombre[30];
     int puntaje;
+    char nomen[5];
 } jugador;
 
 void datos(jugador *);          // Prototipos de funciones
 void puntos(jugador *);
+void nomenclatura(jugador *);
 void tabla_Puntuaciones(jugador *);
 
 void main(void)
 {
-    jugador nombre[2];
+    jugador player[2];
     int i;
 
-    nombre[0].puntaje = 0;
-    nombre[1].puntaje = 0;
+    player[0].puntaje = 0;
+    strcpy(player[0].nomen, "Love");
+    player[1].puntaje = 0;
+    strcpy(player[1].nomen, "Love");
 
-    datos(nombre);
+    datos(player);
 
-    while (nombre[0].puntaje < 3 && nombre[1].puntaje < 3)
+    while (((player[0].puntaje != 3 && player[1].puntaje != 3) && (player[0].puntaje <= 3 || player[1].puntaje <= 3)) || (fabs(player[0].puntaje - player[1].puntaje)) < 2)
     {
-        puntos(nombre);
-        tabla_Puntuaciones(nombre);
+        puntos(player);
+        nomenclatura(player);
+        tabla_Puntuaciones(player);
     }
 }
 
@@ -56,6 +62,30 @@ void puntos(jugador name[2])
     name[i - 1].puntaje++;
 }
 
+void nomenclatura(jugador name[2])
+{
+    int i;
+    char N[4][5] = {"Love", "15", "30", "40"};
+
+    while ((name[0].puntaje < 3 && name[1].puntaje <= 3) || (name[0].puntaje <= 3 && name[1].puntaje < 3))
+    {
+        for (i = 0; i < 2; i++)
+        {
+            if (name[i].puntaje == 0)
+                strcpy(name[i].nomen, "Love");
+            else
+                if (name[i].puntaje == 1)
+                    strcpy(name[i].nomen, "15");
+                else
+                    if (name[i].puntaje == 2)
+                        strcpy(name[i].nomen, "30");
+                    else
+                        if (name[i].puntaje == 3)
+                            strcpy(name[i].nomen, "40");
+        }
+    }
+}
+
 void tabla_Puntuaciones(jugador name[2])
 {
     int len1, len2, i;
@@ -66,11 +96,12 @@ void tabla_Puntuaciones(jugador name[2])
     printf("\n%s\t%s\n", name[0].nombre, name[1].nombre);
     for (i = 0; i < len1 / 2; i++)
         printf(" ");
-    printf("%d", name[0].puntaje);
+    printf("%s", name[0].nomen);
     for (i = 0; i < len1 / 2; i++)
         printf(" ");
     printf("\t");
     for (i = 0; i < len2 / 2; i++)
         printf(" ");
-    printf("%d", name[1].puntaje);
+    printf("%s", name[1].nomen);
+    printf("\n");
 }
