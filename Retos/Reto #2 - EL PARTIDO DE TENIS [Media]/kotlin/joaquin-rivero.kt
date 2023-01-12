499af68e530089aa15204c79a2340c5e4de2b98a
Joaquin Rivero <rivero.dev@gmail.com>
2023-01-12 18:38:56 +0100

    Reto #2 - Kotlin

--- Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/kotlin/joaquin-rivero.kt
+++ Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/kotlin/joaquin-rivero.kt
@@ -6,6 +6,7 @@ fun main() {
 }
 
 fun playTennis() {
+
     val score = mapOf(0 to "Love", 1 to "15", 2 to "30", 3 to "40")
     var playing = true
     var p1Score = 0
@@ -15,50 +16,53 @@ fun playTennis() {
 
     do {
         print("¿Qué jugador ha anotado, $PLAYER_ONE o $PLAYER_TWO?: ")
+
         val player = readln().uppercase()
 
         if (player != PLAYER_ONE && player != PLAYER_TWO) {
             println("Jugador $player no reconocido. Introduzca  $PLAYER_ONE o $PLAYER_TWO.\n")
-            continue
-        }
 
-        if (player == PLAYER_ONE) {
-            p1Score += 1
         } else {
-            p2Score += 1
-        }
-
-        // Si superan el máximo de puntos
-        if (p1Score > 3 || p2Score > 3) {
-            // Si hay empate
-            if (p1Score == p2Score) {
-                println("Deuce\n")
-                continue
-            }
-
-            // Si hay un ganador
-            if (p1Score - p2Score >= 2) {
-                println("Ha ganado el $PLAYER_ONE")
-                playing = false
-                continue
-            } else if (p2Score - p1Score >= 2) {
-                println("Ha ganado el $PLAYER_TWO")
-                playing = false
-                continue
-            }
-
-            // Si hay un jugador con ventaja
-            if (p1Score > p2Score) {
-                println("Ventaja $PLAYER_ONE\n")
-                continue
+            if (player == PLAYER_ONE) {
+                p1Score += 1
             } else {
-                println("Ventaja $PLAYER_TWO\n")
-                continue
+                p2Score += 1
             }
-        } else {
-            println("${score[p1Score]} - ${score[p2Score]}\n")
-            continue
+
+            if (p1Score > 3 || p2Score > 3) {
+                // Si hay empate
+                if (p1Score == p2Score) {
+                    println("Deuce\n")
+
+                } else {
+                    // Si hay un ganador
+                    if (p1Score - p2Score >= 2) {
+                        println("Ha ganado el $PLAYER_ONE")
+                        playing = false
+
+                    } else if (p2Score - p1Score >= 2) {
+                        println("Ha ganado el $PLAYER_TWO")
+                        playing = false
+
+                    } else {
+                        // Si hay un jugador con ventaja
+                        if (p1Score > p2Score) {
+                            println("Ventaja $PLAYER_ONE\n")
+
+                        } else {
+                            println("Ventaja $PLAYER_TWO\n")
+                        }
+                    }
+                }
+            } else {
+                if (p1Score == 3 && p2Score == 3) {
+                    println("Deuce\n")
+                } else {
+                    println("${score[p1Score]} - ${score[p2Score]}\n")
+                }
+            }
         }
+
     } while (playing)
 
     println("\n----- ----- -----")
