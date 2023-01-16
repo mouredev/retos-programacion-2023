6cd1d9c16460bfa7fb29497ec8021f72f9e19196
alevil8022 <111132742+alevil8022@users.noreply.github.com>
2023-01-16 19:35:46 +0000

    Reto #1 - bash

--- Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/bash/alevil8022.sh
+++ Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/bash/alevil8022.sh
@@ -1,76 +1,65 @@
+#!/usr/bin/bash
+
 #
 # Escribe un programa que reciba un texto y transforme lenguaje natural a
 # "lenguaje hacker" (conocido realmente como "leet" o "1337"). Este lenguaje
 #  se caracteriza por sustituir caracteres alfanuméricos.
-# - Utiliza esta tabla (https://www.gamehouse.com/blog/leet-speak-cheat-sheet/) 
+# - Utiliza esta tabla (https://www.gamehouse.com/blog/leet-speak-cheat-sheet/)
 #   con el alfabeto y los números en "leet".
 #   (Usa la primera opción de cada transformación. Por ejemplo "4" para la "a")
-#
-
-alfabeto = dict()
-
-alfabeto = {"a":"4", 
-            "b":"I3",
-            "c":"[",
-            "d":")",
-            "e":"3",
-            "f":"|=",
-            "g":"&",
-            "h":"#",
-            "i":"1",
-            "j":",_|",
-            "k":">|",
-            "l":"1",
-            "m":"'/\/\'",
-            "n":"^/",
-            "o":"0",
-            "p":"|*",
-            "q":"(_,)",
-            "r":"I2",
-            "s":"5",
-            "t":"7",
-            "u":"(_)",
-            "v":"\/",
-            "w":"\/\/",
-            "x":"><",
-            "y":"j",
-            "z":"2",
-            " ":" "
-            }
 
 
-def convertir_texto(oracion, new_oracion):
-    palabra=oracion.split(" ")
-    cant_palabras=len(palabra)
-    # print(palabra)
-    new_oracion = ""
-    numero=0
-    for caracter in palabra:
-        numero += 1
-        caracter_min = caracter.lower()
-        #print(caracter_min)
-        cant_caracter=len(caracter_min)
-        # print(cant_caracter)
-        cant = 0
-        new_palabra = ""
-        new_caracter = ""
-        while cant < cant_caracter:
-            #print(caracter_min[cant])
-            indice=caracter_min[cant]
-            new_caracter = alfabeto[indice]
-            new_palabra = new_palabra + new_caracter
-            cant +=1
-            #print(new_palabra)
-        if cant_palabras <= numero:
-            new_oracion = new_oracion + new_palabra
-        else:
-            new_oracion = new_oracion + new_palabra + " "
-            #print(new_oracion)
-    return new_oracion
+declare -A alfabeto
+alfabeto[a]='4'
+alfabeto[b]='I3'
+alfabeto[c]='['
+alfabeto[d]=')'
+alfabeto[e]='3'
+alfabeto[f]='|='
+alfabeto[g]='&'
+alfabeto[h]='#'
+alfabeto[i]='1'
+alfabeto[j]=',_|'
+alfabeto[k]='>|'
+alfabeto[l]='1'
+alfabeto[m]='/\/\'
+alfabeto[n]='^/'
+alfabeto[o]='0'
+alfabeto[p]='|*'
+alfabeto[q]='(_,)'
+alfabeto[r]='I2'
+alfabeto[s]='5'
+alfabeto[t]='7'
+alfabeto[u]='(_)'
+alfabeto[v]='\/'
+alfabeto[w]='\/\/'
+alfabeto[x]='><'
+alfabeto[y]='j'
+alfabeto[z]='2'
+alfabeto[1]='L'
+alfabeto[2]='R'
+alfabeto[3]='E'
+alfabeto[4]='A'
+alfabeto[5]='S'
+alfabeto[6]='b'
+alfabeto[7]='T'
+alfabeto[8]='B'
+alfabeto[9]='g'
+alfabeto[0]='0'
+alfabeto[ ]=' '
 
 
-oracion = input("Escriba la Palabra a traducir: ")
-new_oracion = str
-print("TEXTO ORIGINAL: ", oracion)
-oracion_traducida = convertir_texto(oracion, new_oracion)
-print("TEXTO TRADUCIDO: ", oracion_traducida)
+read -p "Indicar el texto que desea traducir: " texto
+# Cambiando todo el texto a minuscula
+
+	echo "${texto,,}" > file     # Cambiando todo el texto a minuscula
+	size=${#texto}
+	for ((j=1; j<=$size; j++))
+	do
+		caracter=`cut -c $j file`
+		new_texto+=${alfabeto[$caracter]}
+	done
+rm file
+echo "PALABRA ORIGINAL: ", $texto
+echo "PALABRA TRADUCIDA: ", $new_texto
+
