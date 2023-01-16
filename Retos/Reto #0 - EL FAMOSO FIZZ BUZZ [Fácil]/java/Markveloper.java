49725968a88a3cd538b6ac1350ded8105c9497c4
Markveloper <mellamomarkitos75@gmail.com>
2023-01-16 19:36:02 +0100

    Reto #0 - Java equal 100

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/java/Markveloper.java
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/java/Markveloper.java
@@ -1,6 +1,6 @@
 public class Markveloper {
     public static void main(String[] args) {
-        for (int i=0; i<100; i++){
+        for (int i=0; i<=100; i++){
             if(i%5==0 && i%3==0){
                 System.out.println("fizzbuzz");
             } else if (i%3==0) {
