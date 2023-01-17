41f7d63362cd5bd3980a3d394da0dc9c557294f3
Bárbara D. I <AlioncaDevelopment@gmail.com>
2023-01-17 10:44:11 +0100

    error code

--- Retos/Reto #1: EL "LENGUAJE HACKER"/python/AlioncaDevelopment.py
+++ Retos/Reto #1: EL "LENGUAJE HACKER"/python/AlioncaDevelopment.py
@@ -1,29 +1,12 @@
-def tennis_game(points: list):
-    scores = {0: "Love", 1: "15", 2: "30", 3: "40"}
-    p1_score = 0
-    p2_score = 0
-    for point in points:
-        if point == "P1":
-            p1_score += 1
-        elif point == "P2":
-            p2_score += 1
-        if p1_score < 4 and p2_score < 4:
-            print(scores[p1_score] + " - " + scores[p2_score])
-        elif p1_score == p2_score:
-            print("Deuce")
-        elif p1_score > p2_score:
-            if p1_score - p2_score == 1:
-                print("Ventaja P1")
-            else:
-                print("Ha ganado el P1")
-                return
-        else:
-            if p2_score - p1_score == 1:
-                print("Ventaja P2")
-            else:
-                print("Ha ganado el P2")
-                return
+def leet_speak(text):
+    leet_dict = {
+        'a': '4', 'b': '8', 'c': '(', 'd': 'd', 'e': '3', 'f': 'f', 'g': '9',
+        'h': '#', 'i': '1', 'j': 'j', 'k': 'k', 'l': '1', 'm': 'm', 'n': 'n',
+        'o': '0', 'p': 'p', 'q': 'q', 'r': '2', 's': '5', 't': '7', 'u': 'u',
+        'v': 'v', 'w': 'w', 'x': 'x', 'y': 'y', 'z': '2', '0': '0', '1': '1',
+        '2': '2', '3': '3', '4': '4', '5': '5', '6': '6', '7': '7', '8': '8',
+        '9': '9'
+    }
+    return "".join(leet_dict.get(c.lower(), c) for c in text)
 
-# Example usage
-points = ["P1", "P1", "P2", "P2", "P1", "P2", "P1", "P1"]
-tennis_game(points)
+print(leet_speak("Hello World"))  # H3110 W0r1d
