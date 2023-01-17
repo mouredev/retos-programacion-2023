04cefc748d043f96459e18bdac8c0b706c8e1e7a
nrdev <nelson_rojas_janda@hotmail.com>
2023-01-17 17:55:36 +0100

    Start Reto#3 with Typescript

---
+++ Retos/Reto #3 - EL GENERADOR DE CONTRASEÑAS [Media]/typescript/nrdevpy.ts
@@ -0,0 +1,9 @@
+/*
+ * Escribe un programa que sea capaz de generar contraseñas de forma aleatoria.
+ * Podrás configurar generar contraseñas con los siguientes parámetros:
+ * - Longitud: Entre 8 y 16.
+ * - Con o sin letras mayúsculas.
+ * - Con o sin números.
+ * - Con o sin símbolos.
+ * (Pudiendo combinar todos estos parámetros entre ellos)
+ */
