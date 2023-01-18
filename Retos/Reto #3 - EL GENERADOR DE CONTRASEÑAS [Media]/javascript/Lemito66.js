fed542803aa15bbe1e8653451dc055a6ca22147e
Emill <lemito66@gmail.com>
2023-01-17 22:53:48 -0500

    falta generar la contraseña

--- Retos/Reto #3 - EL GENERADOR DE CONTRASEÑAS [Media]/javascript/Lemito66.js
+++ Retos/Reto #3 - EL GENERADOR DE CONTRASEÑAS [Media]/javascript/Lemito66.js
@@ -7,3 +7,22 @@
 * - Con o sin símbolos.
 * (Pudiendo combinar todos estos parámetros entre ellos) 
  */
+
+
+const lengthOfPassword = () => {
+    lengthOfNumber = []
+    for (let number = 8; number <=16; number++) {
+        lengthOfNumber.push(number);
+    }
+    return lengthOfNumber[Math.floor(Math.random() * lengthOfNumber.length)];
+}
+
+const  completeList = () => {
+    var lowerCase = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    var numbers = '0123456789'.split('');
+    var punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'.split('');
+    return lowerCase.concat(upperCase,numbers,punctuation)
+}
+
+console.log(completeList());
