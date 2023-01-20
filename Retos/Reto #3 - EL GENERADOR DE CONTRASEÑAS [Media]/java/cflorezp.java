88b27ec13335062a6eb7e505bdd529cfce6b8ae7
carlosf <carlos@mail.com>
2023-01-20 10:07:00 -0500

    programa finalizado

--- Retos/Reto #3 - EL GENERADOR DE CONTRASEÑAS [Media]/java/cflorezp.java
+++ Retos/Reto #3 - EL GENERADOR DE CONTRASEÑAS [Media]/java/cflorezp.java
@@ -5,55 +5,7 @@ import java.util.Scanner;
 public class cflorezp {
 
     public static void main(String[] args) {
-        /*
-        System.out.println("***** GENERADOR DE CONTRASEÑAS *****\n");
-        int option = 1;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Desea incluir mayusculas? Y/N: ");
-        String value = input.nextLine();
-        if(value.equals("Y") || value.equals("y")) option++;
-
-        System.out.println("Desea incluir numeros? Y/N: ");
-        String value2 = input.nextLine();
-        if(value2.equals("Y") || value2.equals("y")) option++;
-
-        System.out.println("Desea incluir simbolos? Y/N: ");
-        String value3 = input.nextLine();
-        if(value3.equals("Y") || value3.equals("y")) option++;
-
-        System.out.println(option);
-        System.out.println(generaPasword(generaAletaorio8a16()));
-
-         */
-        /*
-        int[] data = new int[2];
-
-        int total = 9;
-        int num = 2;
-        total = total - num;
-        for(int i= num-1; i >=0;i--){
-            data[i]=1;
-            int ls = 0;
-            if(total>0){
-                if(i==0){
-                    data[i] +=total;
-                }else{
-                    int max = (int)(total/i);
-                    ls  = (int)(Math.random()*max+1);
-                    data[i] += ls;
-                }
-            }
-            total -= ls;
-        }
-        //var_dump($data);
-        for(int ele : data){
-            System.out.println(ele);
-        }
-    */
-        System.out.println(generaPassword());
-
-
-
+        System.out.println("\nLa contraseña generada es: " + generaPassword());
     }
 
     public static int generaAletaorio8a16() {
@@ -61,21 +13,6 @@ public class cflorezp {
         return numero;
     }
 
-    public static String generaPaswordold(int longitud) {
-        char[] caracteres = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-                'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-                '*', '/', '*', '.', '?', '¿', '!', '¡', '-', '_', '+'};
-
-        char[] result = new char[longitud];
-        for (int i = 0; i < longitud; i++) {
-            int position = (int) (Math.random() * caracteres.length);
-            result[i] = caracteres[position];
-        }
-        return new String(result);
-
-    }
-
-    // NUEVA IMPLEMENTACION
     public static String generaPassword(){
         System.out.println("***** GENERADOR DE CONTRASEÑAS *****\n");
         int opciones = 1;
@@ -92,20 +29,35 @@ public class cflorezp {
         String value4 = input.nextLine();
         if(value4.equals("Y") || value4.equals("y")) opciones++;
 
-
         int longitudPassword = generaAletaorio8a16();
         int[] combinaciones = posiblesCombinaciones(longitudPassword, opciones);
 
-        char[] password = generaCaracteresRandom(generaMinusculas(), combinaciones[0]);
+        char[] cadenaMinusculas = generaCaracteresRandom(generaMinusculas(), combinaciones[0]);
+        String password = new String(cadenaMinusculas);
 
-        return new String(password);
-
-
-        //System.out.println(option);
-        //System.out.println(generaPasword(generaAletaorio8a16()));
-
+        if(combinaciones.length > 1){
+            for(int i = 1; i < combinaciones.length; i++){
+                if(value2.equals("Y") || value2.equals("y")){
+                    char[] cadenaMayusculas = generaCaracteresRandom(generaMayusculas(), combinaciones[i]);
+                    value2 = "ok";
+                    password += new String(cadenaMayusculas);
+                    continue;
+                }
+                if(value3.equals("Y") || value3.equals("y")){
+                    char[] cadenaNumeros = generaCaracteresRandom(generaNumeros(), combinaciones[i]);
+                    value3 = "ok";
+                    password += new String(cadenaNumeros);
+                    continue;
+                }
+                if(value4.equals("Y") || value4.equals("y")){
+                    char[] cadenaSimbolos = generaCaracteresRandom(generaSimbolos(), combinaciones[i]);
+                    value4 = "ok";
+                    password += new String(cadenaSimbolos);
+                }
+            }
+        }
+        return desordenarString(password);
     }
-
 
     public static char[] generaCaracteresRandom(char[] caracteres, int longitud) {
         char[] result = new char[longitud];
@@ -136,6 +88,17 @@ public class cflorezp {
         return result;
     }
 
+    public static String desordenarString(String cadena){
+        int azar = 0;
+        String resultado = "";
+        for(int i = cadena.length(); i >= 2; i--){
+            azar = (int)(Math.random() * i + 1);
+            resultado = resultado + cadena.substring(azar - 1, azar);
+            cadena = cadena.substring(0, azar - 1) + cadena.substring(azar, i);
+        }
+        return resultado + cadena;
+    }
+
     public static char[] generaMinusculas() {
         char[] minusculas = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
                 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
@@ -157,6 +120,4 @@ public class cflorezp {
         char[] simbolos = {'*', '-', '_', ';', '.', ',', '?', '¿', '¡', '!', '$', '#'};
         return simbolos;
     }
-
-
 }
