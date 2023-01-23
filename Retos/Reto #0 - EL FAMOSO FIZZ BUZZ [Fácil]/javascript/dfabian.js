0cb54b92039afea8505df3726874fd2a8d363107
dfabian <dafaos90@gmail.com>
2023-01-23 23:21:25 +0100

    FizzBuzz

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/javascript/dfabian.js
@@ -0,0 +1,24 @@
+function fizzBuzz(number) {
+    const isFizz = number % 3 === 0;
+    const isBuzz = number % 5 === 0;
+    const isFizzBuzz = isFizz && isBuzz;
+
+    if (isFizzBuzz) {
+        return 'FizzBuzz';
+    }
+
+    if (isFizz) {
+        return 'Fizz';
+    }
+
+    if (isBuzz) {
+        return 'Buzz';
+    }
+
+    return number;
+}
+// Create range from 1 to 100
+const numbers = Array.from(Array(100).keys()).map(n => n + 1);
+// Iterate over list
+const fizzBuzzList = numbers.map(number => fizzBuzz(number));
+console.log(fizzBuzzList.join('\n'));
