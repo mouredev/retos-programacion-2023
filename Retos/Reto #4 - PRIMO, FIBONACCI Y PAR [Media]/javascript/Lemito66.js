50ed5bc9773fd354c1212ea0a78dea47731fb303
Emill <lemito66@gmail.com>
2023-01-24 17:09:51 -0500

    Iniciando

---
+++ Retos/Reto #4 - PRIMO, FIBONACCI Y PAR [Media]/javascript/Lemito66.js
@@ -0,0 +1,6 @@
+/*
+ * Escribe un programa que, dado un número, compruebe y muestre si es primo, fibonacci y par.
+ * Ejemplos:
+ * - Con el número 2, nos dirá: "2 es primo, fibonacci y es par"
+ * - Con el número 7, nos dirá: "7 es primo, no es fibonacci y es impar"
+ */
