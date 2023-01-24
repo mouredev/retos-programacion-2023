00edf750aa5c1837230f1c5c0ad6af8547ac6194
Emill <lemito66@gmail.com>
2023-01-24 17:22:19 -0500

    par o impar listo

--- Retos/Reto #4 - PRIMO, FIBONACCI Y PAR [Media]/javascript/Lemito66.js
+++ Retos/Reto #4 - PRIMO, FIBONACCI Y PAR [Media]/javascript/Lemito66.js
@@ -4,3 +4,14 @@
  * - Con el número 2, nos dirá: "2 es primo, fibonacci y es par"
  * - Con el número 7, nos dirá: "7 es primo, no es fibonacci y es impar"
  */
+
+const isEven = (number) => {
+    if (number % 2 ===0){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+console.log(isEven(5));
