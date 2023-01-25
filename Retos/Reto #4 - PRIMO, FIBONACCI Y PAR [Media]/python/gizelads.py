a2c2b22fb0d2333965412dc2ae93b26c179ce1c6
GizelaDS <74159414+gizelads@users.noreply.github.com>
2023-01-25 10:53:25 -0400

    create function es_par in gizelads.py

--- Retos/Reto #4 - PRIMO, FIBONACCI Y PAR [Media]/python/gizelads.py
+++ Retos/Reto #4 - PRIMO, FIBONACCI Y PAR [Media]/python/gizelads.py
@@ -2,3 +2,21 @@
 Ejemplos:
 - Con el número 2, nos dirá: "2 es primo, fibonacci y es par"
 - Con el número 7, nos dirá: "7 es primo, no es fibonacci y es impar" """
+
+def es_par(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+
+def run():
+    numero = int(input("Escribe un numero: "))
+    if es_par(numero):
+        print(numero, "es par.")
+    else:
+        print(numero, "es impar.")
+
+
+if __name__ == "__main__":
+    run()
