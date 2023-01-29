7f9cc4cd7dc65d7f055db686ace59c5e89d5b404
RiveroDev <Richard.Rivero.dev@gmail.com>
2023-01-29 13:01:54 -0400

    Correcionlogica y comprobaciones

--- Retos/Reto #4 - PRIMO, FIBONACCI Y PAR [Media]/python/RiveroDev.py
+++ Retos/Reto #4 - PRIMO, FIBONACCI Y PAR [Media]/python/RiveroDev.py
@@ -1,6 +1,5 @@
 #! bin/python3
 
-
 class fibo_primo_par():
 
     def __init__(self, numero):
@@ -8,10 +7,10 @@ class fibo_primo_par():
         self.numero = numero
 
     def num_primo(self) -> bool:
-        valor = False
+        valor = True
         for i in range(2,self.numero):
             if self.numero % i == 0 :
-                valor = True
+                valor = False
         return valor 
 
     def num_par(self) -> bool:
@@ -38,15 +37,24 @@ class fibo_primo_par():
 
         return valor
 
-    def que_es(self):
-        pass
+    def que_es(self) -> str:
+        pri = "no es" 
+        par = "impar"
+        fibo = "no es"
+        if self.num_primo() == True:
+            pri = "es"
+        if self.num_par() == True:
+            par = "par"
+        if self.num_fibo() == True:
+            fibo = ""
 
-
+        return (f"{self.numero} {pri} primo,{fibo} fibonacci y es {par}")
 
 
 if __name__=="__main__":
     
     fibo = fibo_primo_par(7)
-    #print(fibo.num_par())
-    #print(fibo.num_primo())
+    print(fibo.num_par())
+    print(fibo.num_primo())
     print(fibo.num_fibo())
+    print(fibo.que_es())
