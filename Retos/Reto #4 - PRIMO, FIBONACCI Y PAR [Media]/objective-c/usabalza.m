bbe4cef9db31961721e8b980c9351161d679b624
Uziel Sabalza <54079791+usabalza@users.noreply.github.com>
2023-02-01 12:22:21 -0400

    Delete Retos/Reto #4 - PRIMO, FIBONACCI Y PAR [Media]/objective-c directory

--- Retos/Reto #4 - PRIMO, FIBONACCI Y PAR [Media]/objective-c/usabalza.m
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hola mundo");
-    }
-    return 0;
-}
