2dcf031cf6660f97abe2eaa05820e8168f6fdb72
Emill <lemito66@gmail.com>
2023-02-01 20:11:38 -0500

    Listo

---
+++ Retos/Reto #5 - HOLA MUNDO [Fácil]/c#/Lemito66.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace HolaMundo {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hola mundo");
+            Console.ReadLine();
+        }
+    }
+}
