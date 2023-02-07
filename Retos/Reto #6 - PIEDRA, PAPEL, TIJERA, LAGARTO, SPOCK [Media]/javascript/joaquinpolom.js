ca79a1b4566c8937fa4cf68fdf4a4bdef052f335
joaquinpolom <joaquin.polo.m@outlook.es>
2023-02-07 19:50:28 +0000

    Update joaquinpolom.js

--- Retos/Reto #6 - PIEDRA, PAPEL, TIJERA, LAGARTO, SPOCK [Media]/javascript/joaquinpolom.js
+++ Retos/Reto #6 - PIEDRA, PAPEL, TIJERA, LAGARTO, SPOCK [Media]/javascript/joaquinpolom.js
@@ -18,6 +18,7 @@
   '🖖🏻': ['✂️', '🪨'],
  */
 
+// Table with the rules of the game
 const results = [
   '🪨': ['✂️', '🦎'],
   '📃': ['🪨', '🖖🏻'],
@@ -26,9 +27,59 @@ const results = [
   '🖖🏻': ['✂️', '🪨'],
 ];
 
+// Initialising points of the game
 let game = [
   Player1 = 0,
   Player2 = 0,
   Tie = 0,
   ];
   
+// Entry
+let game1 = [("🗿","✂️"), ("✂️","🗿"), ("📄","✂️")];
+
+function pptle(game) {
+  // Reset the results
+  game.Player1 = 0;
+  game.Player2 = 0;
+  game.Tie = 0;
+  // Check every result
+  for (const a of game) {
+    // Guest player1
+    gp1 = a[0];
+    console.log(gp1);
+    // Guest player2
+    gp2 = a[1];
+    console.log(gp2);
+    // if it is the same result then tie
+    if (gp1 === gp2) {
+      // Tie
+      game.Tie += 1;
+    } else {
+      // Who win that game
+      // Check table results
+      result1 = results[gp1];
+      console.log(result1);
+      // if guest player2 is in result1 Player1 win if not Player2 wins
+      if ((gp2 === result1[0]) or (gp2 === result1[1])) {
+        // wins player1
+        game.Player1 += 1;
+      } else {
+        // wins player2
+        game.Player2 += 1;
+      }
+    }
+  }
+  // Check the final result
+  if ((game.Tie > game.Player1) or (game.Tie > game.Player2) or (game.Player1 === game.Player2)) {
+    //Tie
+    return "Tie";
+  } else if (game.Player1 > game.Player2) {
+    // Win Player1
+    return "Player1";
+  } else {
+    // Win Player2
+    return "Player2";
+  }
+}
+
+console.log(pptle(game1));
