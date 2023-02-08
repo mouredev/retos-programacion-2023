1a4e06aab9cb32a5c148584334174ec7472fdb6d
Óscar <aronracso@gmail.com>
2023-02-08 18:02:13 +0100

    Creación de la clase

---
+++ Retos/Reto #6 - PIEDRA, PAPEL, TIJERA, LAGARTO, SPOCK [Media]/java/aronracso.java
@@ -0,0 +1,35 @@
+/**
+ * Reto #6: Piedra, Papel, Tijera, Lagarto, Spock
+ * Dificultad: Media | Publicación: 06/02/23 | Corrección: 13/02/23
+ * 
+ * Crea un programa que calcule quien gana más partidas al piedra,
+ * papel, tijera, lagarto, spock.
+ * - El resultado puede ser: "Player 1", "Player 2", "Tie" (empate)
+ * - La función recibe un listado que contiene pares, representando cada jugada.
+ * - El par puede contener combinaciones de "🗿" (piedra), "📄" (papel),
+ *   "✂️" (tijera), "🦎" (lagarto) o "🖖" (spock).
+ * - Ejemplo. Entrada: [("🗿","✂️"), ("✂️","🗿"), ("📄","✂️")]. Resultado: "Player 2".
+ * - Debes buscar información sobre cómo se juega con estas 5 posibilidades.
+ * 
+ * Información del juego:
+ * https://www.hobbyconsolas.com/reportajes/curiosa-historia-mitico-piedra-papel-tijeras-lagarto-spock-sheldon-big-bang-theory-707343
+ * 
+ * "Las reglas son muy sencillas, según Sheldon.
+ * - las tijeras cortan el papel
+ * - el papel envuelve la piedra
+ * - la piedra aplasta al lagarto
+ * - el lagarto envenena a Spock
+ * - Spock aplasta las tijeras
+ * - las tijeras decapitan al lagarto
+ * - el lagarto devora el papel
+ * - el papel desaprueba a Spock
+ * - Spock desintegra la piedra y, como siempre, 
+ * - la piedra aplasta las tijeras."
+ */
+public class aronracso
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
