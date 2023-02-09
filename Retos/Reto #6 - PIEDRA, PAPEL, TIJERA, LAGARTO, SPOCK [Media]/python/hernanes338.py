04fb2e2704621f09fcf6b90d8f722b71b65ee272
hernanes338 <auherlpe@gmail.com>
2023-02-09 16:56:27 +0100

    Reto 6 - Python

--- Retos/Reto #6 - PIEDRA, PAPEL, TIJERA, LAGARTO, SPOCK [Media]/python/hernanes338.py
+++ Retos/Reto #6 - PIEDRA, PAPEL, TIJERA, LAGARTO, SPOCK [Media]/python/hernanes338.py
@@ -22,38 +22,42 @@ def game_winner(rounds_list: list):
     
     emojis_dict = {"🗿":"rock", "📄":"paper", "✂️":"scissors", "🦎":"lizard", "🖖":"spock"}
     
+    rounds_to_play = 0
     player_1_choices_list: list = []
     player_2_choices_list: list = []
 
     for round in rounds_list:
-        player_1_choices_list.append(round[0])
-        player_2_choices_list.append(round[1])
+        if round[0] and round[1] in emojis_dict:
+            player_1_choices_list.append(round[0])
+            player_2_choices_list.append(round[1])
+            rounds_to_play += 1
     
     winner = ''
     player_1_score: int = 0
     player_2_score: int = 0
     
-    if len(player_1_choices_list) == len(player_2_choices_list):
-    
-        for i in range(len(player_1_choices_list)):
-            player_1_choice = emojis_dict[player_1_choices_list[i]]
-            player_2_choice = emojis_dict[player_2_choices_list[i]]
+    for i in range(rounds_to_play):
+        if (player_1_score or player_2_score) > rounds_to_play/2:
+            break
+
+        player_1_choice = emojis_dict[player_1_choices_list[i]]
+        player_2_choice = emojis_dict[player_2_choices_list[i]]
+        
+        if player_1_choice == player_2_choice:
+            continue
             
-            if player_1_choice == player_2_choice:
-                continue
-                
-            elif player_1_choice == "scissors" and (player_2_choice == "paper" or player_2_choice == "lizard"):
-                player_1_score += 1
-            elif player_1_choice == "paper" and (player_2_choice == "rock" or player_2_choice == "spock"):
-                player_1_score += 1
-            elif player_1_choice == "rock" and (player_2_choice == "lizard" or player_2_choice == "scissors"):
-                player_1_score += 1
-            elif player_1_choice == "lizard" and (player_2_choice == "spock" or player_2_choice == "paper"):
-                player_1_score += 1
-            elif player_1_choice == "spock" and (player_2_choice == "scissors" or player_2_choice == "rock"):
-                player_1_score += 1
-            else:
-                player_2_score += 1
+        elif player_1_choice == "scissors" and (player_2_choice == "paper" or player_2_choice == "lizard"):
+            player_1_score += 1
+        elif player_1_choice == "paper" and (player_2_choice == "rock" or player_2_choice == "spock"):
+            player_1_score += 1
+        elif player_1_choice == "rock" and (player_2_choice == "lizard" or player_2_choice == "scissors"):
+            player_1_score += 1
+        elif player_1_choice == "lizard" and (player_2_choice == "spock" or player_2_choice == "paper"):
+            player_1_score += 1
+        elif player_1_choice == "spock" and (player_2_choice == "scissors" or player_2_choice == "rock"):
+            player_1_score += 1
+        else:
+            player_2_score += 1
     
     if player_1_score > player_2_score:
         winner = 'Player 1'
@@ -64,4 +68,6 @@ def game_winner(rounds_list: list):
     
     return winner
     
-print(game_winner([("🗿","📄"), ("✂️","🗿"), ("🖖","✂️")]))
+# print(game_winner([("🗿","📄"), ("✂️","🦎"), ("🖖","✂️"), ("🖖","✂️")])) # player 1
+# print(game_winner([("📄","🗿"), ("🦎","✂️"), ("✂️","🖖"), ("🖖","✂️")])) # tie
+# print(game_winner([("🗿","📄"), ("🦎","✂️"), ("✂️","🖖"), ("🖖","✂️")])) # player 2
