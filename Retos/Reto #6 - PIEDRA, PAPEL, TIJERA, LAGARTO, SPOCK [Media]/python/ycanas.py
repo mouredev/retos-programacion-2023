f58a483b091ad515369ed30aa82e836cdcf410a6
ycanas <yairca3.7@gmail.com>
2023-02-10 12:38:41 -0500

    Reto #6 - Python

--- Retos/Reto #6 - PIEDRA, PAPEL, TIJERA, LAGARTO, SPOCK [Media]/python/ycanas.py
+++ Retos/Reto #6 - PIEDRA, PAPEL, TIJERA, LAGARTO, SPOCK [Media]/python/ycanas.py
@@ -3,29 +3,26 @@ def game(sequence):
     player2 = 0
 
     combinations = {
-        "lagartopapel":  "lagarto",
-        "lagartopiedra": "piedra",
-        "lagartospock":  "lagarto",
-        "lagartotijera": "tijera",
-        "papelpiedra":   "papel",
-        "papelspock":    "papel",
-        "papeltijera":   "tijera",
-        "piedraspock":   "spock",
-        "piedratijera":  "piedra",
-        "spocktijera":   "spock"
+        "🗿": "✂🦎",
+        "📄": "🗿🖖",
+        "✂":  "🦎📄",
+        "🦎": "📄🖖",
+        "🖖": "✂🗿"
     }
 
     for attempt in sequence:
-        key = sorted(attempt)[0] + sorted(attempt)[1]
-
-        if key in combinations:
-            if combinations[key] == attempt[0]:
-                player1 += 1
-
-            else:
-                player2 += 1
+        if attempt[0] not in combinations or attempt[1] not in combinations:
+            return "Jugada no valida"
+            
+        if attempt[1] in combinations[attempt[0]]:
+            player1 += 1
+        
+        elif attempt[0] in combinations[attempt[1]]:
+            player2 += 1
     
     return "Tie" if player1 == player2 else "Player 1" if player1 > player2 else "Player 2"
 
 
-print(game([["spock", "tijera"], ["spock", "papel"], ["piedra", "spock"]]))
+print(game([["🖖", "📄"], ["🖖", "📄"], ["🗿", "🦎"]]))
+print(game([["🗿", "✂"],  ["🖖", "✂"],  ["📄", "🦎"]]))
+print(game([["🖖", "🖖"], ["🖖", "🗿"], ["✂", "🖖"]]))
