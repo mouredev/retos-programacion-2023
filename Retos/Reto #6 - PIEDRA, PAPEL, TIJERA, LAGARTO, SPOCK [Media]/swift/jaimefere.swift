7d5c65e65209fd303abb77795703341916312689
Jaime Fernández <dev@jaimefere.es>
2023-02-12 21:54:55 +0100

    Jaimefere's solution #6 with Kotlin

--- Retos/Reto #6 - PIEDRA, PAPEL, TIJERA, LAGARTO, SPOCK [Media]/swift/jaimefere.swift
+++ Retos/Reto #6 - PIEDRA, PAPEL, TIJERA, LAGARTO, SPOCK [Media]/swift/jaimefere.swift
@@ -18,6 +18,13 @@ func playRockPaperScissorsAlligatorSpock(games: [(first: String, second: String)
         if(playerOneMove != playerTwoMove) {
             if((playerOneMove == Move.SCISSORS && playerTwoMove == Move.PAPER) ||
             (playerOneMove == Move.PAPER && playerTwoMove == Move.ROCK) ||
+            (playerOneMove == Move.ROCK && playerTwoMove == Move.ALLIGATOR) ||
+            (playerOneMove == Move.ALLIGATOR && playerTwoMove == Move.SPOCK) ||
+            (playerOneMove == Move.SPOCK && playerTwoMove == Move.SCISSORS) ||
+            (playerOneMove == Move.SCISSORS && playerTwoMove == Move.ALLIGATOR) ||
+            (playerOneMove == Move.ALLIGATOR && playerTwoMove == Move.PAPER) ||
+            (playerOneMove == Move.PAPER && playerTwoMove == Move.SPOCK) ||
+            (playerOneMove == Move.SPOCK && playerTwoMove == Move.ROCK) ||
             (playerOneMove == Move.ROCK && playerTwoMove == Move.SCISSORS)) {
                 playerOneWins += 1
             } else {
