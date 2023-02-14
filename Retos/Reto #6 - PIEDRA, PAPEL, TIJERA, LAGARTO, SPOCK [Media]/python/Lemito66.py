1c6b58169e7b67bbb03006095937592c566acdaf
SYSTEM-INTERN <pas_sistemas@hmetro.med.ec>
2023-02-14 11:17:31 -0500

    Listo

--- Retos/Reto #6 - PIEDRA, PAPEL, TIJERA, LAGARTO, SPOCK [Media]/python/Lemito66.py
+++ Retos/Reto #6 - PIEDRA, PAPEL, TIJERA, LAGARTO, SPOCK [Media]/python/Lemito66.py
@@ -10,3 +10,26 @@
 """
 
 
+def rock_paper_scissors_lizard_spock(games):
+    rules = {"🗿": ["✂️", "🦎"],
+             "📄": ["🗿", "🖖"],
+             "✂️": ["📄", "🦎"],
+             "🦎": ["🖖", "📄"],
+             "🖖": ["🗿", "✂️"]}
+
+    player_one = 0
+    player_two = 0
+
+    for game in games:
+        player_one_game = game[0]
+        player_two_game = game[1]
+        if player_one_game != player_two_game:
+            if player_one_game in rules[player_one_game]:
+                player_one += 1
+            else:
+                player_two += 1
+    return "Tie" if player_one == player_two else "Player 1" if player_one > player_two else "Player 2"
+
+
+
+print(rock_paper_scissors_lizard_spock([("✂️", "✂️")]))
