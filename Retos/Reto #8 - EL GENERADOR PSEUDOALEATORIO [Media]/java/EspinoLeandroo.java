b6901a45ed8c6021052d665e3716b6360c2da040
Leandro Espino <espinoleandroo@gmail.com>
2023-02-20 22:02:03 -0600

    Reto #8 - Java

--- Retos/Reto #8 - EL GENERADOR PSEUDOALEATORIO [Media]/java/EspinoLeandroo.java
+++ Retos/Reto #8 - EL GENERADOR PSEUDOALEATORIO [Media]/java/EspinoLeandroo.java
@@ -1,8 +1,18 @@
 public class EspinoLeandroo {
 
 
-    // Declarar las constantes necesarias para el generador de números pseudoaleatorios
-   
+    /*
+     * Este código utiliza una semilla inicial para el generador de números 
+     * aleatorios y la fórmula (1103515245 * semilla + 12345) % 2147483648
+     * para generar números aleatorios. La fórmula produce un número entero 
+     * de 32 bits, y se utiliza el operador módulo (%) para asegurarse de que
+     * el resultado está dentro del rango deseado de 0 a 100.
+     * 
+     * Es importante tener en cuenta que los generadores de números aleatorios 
+     * pseudoaleatorios no producen números realmente aleatorios, sino que 
+     * utilizan una secuencia determinística de valores para generar una 
+     * secuencia de números que parecen ser aleatorios
+     */
 
 
     public static void main(String[] args) {
