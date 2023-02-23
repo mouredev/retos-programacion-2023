ea0076f4909d412eae7b85793f19a97614e976bc
Majinka10 <camachopinto_@hotmail.com>
2023-02-23 17:43:14 -0500

    Reto #4 - Python

---
+++ Retos/Reto #4 - PRIMO, FIBONACCI Y PAR [Media]/python/majinka10.py
@@ -0,0 +1,28 @@
+import math
+
+def is_fibonacci(n):
+    x,y,fib=-1,1,0
+    while fib<n:
+        fib=x+y
+        x=y
+        y=fib
+    if fib==n:
+        return True
+    else: 
+        return False
+
+def is_prime(n):
+  for i in range(2,int(math.sqrt(n))+1):
+    if (n%i) == 0:
+      return False
+  return True
+
+def test_number(number):
+    result=''
+    result+= 'es primo, ' if is_prime(number) else 'no es primo, ' 
+    result+= 'fibonacci ' if is_fibonacci(number) else 'no es fibonacci '
+    result+= 'y es par' if number % 2 == 0 else 'y es impar'
+    print(number, result)
+
+test_number(2)
+test_number(7)
