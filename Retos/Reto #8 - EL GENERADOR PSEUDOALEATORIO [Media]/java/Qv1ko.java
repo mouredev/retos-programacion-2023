d2080fa17a9190c19917f1b815ebba601d3a3afc
Víctor García <95133973+Qv1ko@users.noreply.github.com>
2023-02-25 03:33:41 +0100

    Reto #8 - Java

--- Retos/Reto #8 - EL GENERADOR PSEUDOALEATORIO [Media]/java/Qv1ko.java
+++ Retos/Reto #8 - EL GENERADOR PSEUDOALEATORIO [Media]/java/Qv1ko.java
@@ -2,7 +2,7 @@ public class Qv1ko {
 
     public static void main(String[] args) {
         System.out.println(randomNumber());
-    }
+    }//main
 
     private static int randomNumber() {
         double num1=((double)System.currentTimeMillis())/1000;
