b76a816c8dcd3f2421752a92f4ccfac0d4c51bbe
Jose Juan <56514339+pepeju1995@users.noreply.github.com>
2023-02-26 12:34:11 +0100

    Update pepeju1995.php

--- Retos/Reto #3 - EL GENERADOR DE CONTRASEÑAS [Media]/php/pepeju1995.php
+++ Retos/Reto #3 - EL GENERADOR DE CONTRASEÑAS [Media]/php/pepeju1995.php
@@ -8,38 +8,36 @@ $values = [
 ];
 
 function getNextPassValue(
-	bool hasUpper,
-	bool hasNumbers,
-	bool hasSymbols
+	bool $hasUpper,
+	bool $hasNumbers,
+	bool $hasSymbols
 ) : string
 {
 	$value = '';
-	$options = [$values['lower']];
+	$options = [$GLOBALS['values']['lower']];
 	
-	if (hasUpper) array_push($options, $values['upper']);
-	if (hasNumbers) array_push($options, $values['number']);
-	if (hasSymbols) array_push($options, $values['symbol']);
+	if ($hasUpper) array_push($options, $GLOBALS['values']['upper']);
+	if ($hasNumbers) array_push($options, $GLOBALS['values']['number']);
+	if ($hasSymbols) array_push($options, $GLOBALS['values']['symbol']);
 	
 	$selected = array_rand($options);
-	$value .= rand($selecter[0], $selected[1]);
+	$value .= chr(rand($options[$selected][0], $options[$selected][1]));
 	return $value;
 }
 
 function passwordGenerator(
-	int passLength = 8,
-	bool hasUpper = false,
-	bool hasNumbers = false,
-	bool hasSymbols = false
+	int $passLength = 8,
+	bool $hasUpper = false,
+	bool $hasNumbers = false,
+	bool $hasSymbols = false
 ) : string
 {
 	$password = '';
 	$position = 0;
 	
-	while (position < passLength) {
-		$password .= getNextPassValue(hasUpper, hasNumbers, hasSymbols);
+	while ($position < $passLength) {
+		$password .= getNextPassValue($hasUpper, $hasNumbers, $hasSymbols);
 		$position++;
 	}
 	return $password;
 }
-
-passwordGenerator();
