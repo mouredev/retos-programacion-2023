4d178f4d2826eff1da02b7a47b7a974dd758c44d
omgslinux <omgs01@gmail.com>
2023-02-27 10:16:04 +0100

    Caso numero == 2, primo

--- Retos/Reto #4 - PRIMO, FIBONACCI Y PAR [Media]/php/omgslinux.php
+++ Retos/Reto #4 - PRIMO, FIBONACCI Y PAR [Media]/php/omgslinux.php
@@ -6,18 +6,20 @@
  * - Con el número 7, nos dirá: "7 es primo, no es fibonacci y es impar"
  */
 
-class fibonacci
+class Fibonacci
 {
     private $debug;
 
-    public function __construct($debug=false)
+    public function __construct($debug = false)
     {
         $this->debug = $debug;
     }
-    
+
     public function checkFibonacci($number): bool
     {
-        $prev = 0; $next = 1; $result=1;
+        $prev = 0;
+        $next = 1;
+        $result = 1;
         while ($result<=$number) {
             $result = $prev + $next;
             $prev = $next;
@@ -31,13 +33,13 @@ class fibonacci
     }
 }
 
-function isDivisible($number, $debug=false)
+function isDivisible($number, $debug = false)
 {
     $i = 2;
     $div = false;
     while ($i<($number**0.5) && (!$div)) {
         if ($debug) {
-            printf ("probando %d\n", $i);
+            printf("probando %d\n", $i);
         }
         if (($number % $i)==0) {
             $div = true;
@@ -48,11 +50,12 @@ function isDivisible($number, $debug=false)
     return $div;
 }
 
-$test = new fibonacci();
-$numbers = [5, 6, 7, 8, 11, 13, 20, 33, 34, 55];
-foreach($numbers as $number) {
-    $div = isDivisible($number);
-    $fibo = $test->checkFibonacci($number);
+$test = new Fibonacci();
+$numbers = [2, 3, 4, 5, 6, 7, 8, 11, 13, 20, 33, 34, 55];
+foreach ($numbers as $number) {
     $odd = $number % 2;
+    // El 2 es primo
+    $div = ($odd?isDivisible($number):$number!=2);
+    $fibo = $test->checkFibonacci($number);
     printf("%d:%s es primo,%s fibonacci y es %s\n", $number, ($div?' no':''), ($fibo?'':' no'), ($odd?'impar':'par'));
 }
