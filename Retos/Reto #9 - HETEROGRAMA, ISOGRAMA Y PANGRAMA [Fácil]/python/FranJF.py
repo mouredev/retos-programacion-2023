320b895aa8d92ccfb85d0cf54f2615ee7e7cf076
Francisco Javier <48967040+FranJF@users.noreply.github.com>
2023-03-01 20:33:08 +0100

    Update FranJF.py

--- Retos/Reto #9 - HETEROGRAMA, ISOGRAMA Y PANGRAMA [Fácil]/python/FranJF.py
+++ Retos/Reto #9 - HETEROGRAMA, ISOGRAMA Y PANGRAMA [Fácil]/python/FranJF.py
@@ -1,1 +1,22 @@
+"""
+/*
+ * Crea 3 funciones, cada una encargada de detectar si una cadena de
+ * texto es un heterograma, un isograma o un pangrama.
+ * - Debes buscar la definición de cada uno de estos términos.
+ */
+"""
 
+def heterograma(palabra:str):
+    if len(palabra) == set(palabra.split()):
+        return True
+    else:
+        return False
+
+def isograma(palabra:str):
+    pass
+
+def pangrama(palabra:str):
+    pass
+
+
+print(heterograma('Calumbrientos'))
