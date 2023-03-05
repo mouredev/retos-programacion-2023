8cf8597497be39441741f08d3e93dbd2e0bdd4fe
Joaquin Polo Molero <joaquin.polo.m@outlook.es>
2023-03-05 16:53:15 +0000

    Update joaquinpolom.js

--- Retos/Reto #9 - HETEROGRAMA, ISOGRAMA Y PANGRAMA [Fácil]/javascript/joaquinpolom.js
+++ Retos/Reto #9 - HETEROGRAMA, ISOGRAMA Y PANGRAMA [Fácil]/javascript/joaquinpolom.js
@@ -8,69 +8,115 @@
 // Un pangrama es una frase en la que aparecen todas las letras del abecedario.
 // Ejemplos
 // Heterogramas: yuxtaponer (10), centrifugado (12), luteranismo (11), adulterinos (11), hiperblanduzcos (15)...
-// Isogramas con una repetición o de segundo orden: acondicionar (11), escritura (9), intestinos (10), papelera (8)...
+// Isogramas con una repetición o de segundo orden: abdomen, escritura (9), intestinos (10), papelera (8)...
 // Pangrama: Benjamín pidió una bebida de kiwi y fresa. Noé, sin vergüenza, la más exquisita champaña del menú.
 
 function isHeterograma(word) {
+  console.log("isHeterograma word: " + word);
+  //console.log("isHeterograma word length: " + word.length);
   var letters = [];
-  for (let i = 0; i < word.Length; i++) {
+  for (let i = 0; i < word.length; i++) {
     let letter = word[i];
-    console.log(letter);
+    //console.log("isHeterograma letter: " + letter);
     if (isInArray(letter,letters)) {
       // Is not Heterograma
       console.log("No es un Heterograma");
-      return FALSE;
+      return false;
     } else {
       // Add the leter to the array
       letters = letters + letter;
-      console.log(letters);
+      //console.log(letters);
     }
   }
   // Es un Heterograma
   console.log("Es un Heterograma");
-  return TRUE;
+  return true;
 }
 
 function isIsograma(word) {
+  console.log("isIsograma word: " + word);
   var letters = new Map();
-  for (let i = 0; i < word.Length; i++) {
+  for (let i = 0; i < word.length; i++) {
     let letter = word[i];
-    console.log(letter);
+    //console.log("isIsograma letter: " + letter);
+    //console.log("isIsograma word letters.has(letter): " + letters.has(letter));
     if (letters.has(letter)) {
       // If the letter has already appeared, increase the account
       letters.set(letter, (letters.get(letter) + 1));
+      //console.log("isIsograma letters[letter]: " + letters.get(letter));
     } else {
       // if the letter has not appeared include in the array
       letters.set(letter, 1);
+      //console.log("isIsograma letters[letter]: " + letters.get(letter));
     }
   }
+  //console.log("isIsograma letters: " + letters);
   // Check if letters appears the same qty
-  const iterator = letters.values();
-  let a = iterator.next().value;
-  console.log("isIsograma a: " + a);
-  for (iterator) {
-    console.log("isIsograma iterator: " + iterator);
-    if (iterator.value !== a) {
+  let a = 0;
+  //console.log("isIsograma a: " + a);
+  for (const value of letters.values()) {
+    //console.log("isIsograma value:" + value);
+    if (a == 0) {
+      // Is the first value
+      //console.log("a==0");
+      a = value;
+      //console.log("isIsograma a: " + a);
+    }
+    //console.log("a: " + a + " value: " + value);
+    if (a != value) {
       // Is diferent. Is not Isograma
+      //console.log("a != value");
       console.log("No es un Isograma");
-      return FALSE
-    let a = iterator.value;
-    console.log("isIsograma a: " + a);
-  }
+      return false;
+    }
+  } 
   // Is a Isograma
+  console.log("Es un Isograma");
+  return true;
 }
 
 function isPangrama(word) {
-  var letters = [];
+  console.log("isPangrama word: " + word);
+  var letters = new Map();
+  // Count the letters
+  for (let i = 0; i < word.length; i++) {
+    let letter = word[i];
+    //console.log("isPangrama letter: " + letter);
+    //console.log("isPangrama word letters.has(letter): " + letters.has(letter));
+    if (letters.has(letter)) {
+      // If the letter has already appeared, increase the account
+      letters.set(letter, (letters.get(letter) + 1));
+      //console.log("isPangrama letters[letter]: " + letters.get(letter));
+    } else {
+      // if the letter has not appeared include in the array
+      letters.set(letter, 1);
+      //console.log("isPangrama letters[letter]: " + letters.get(letter));
+    }
+  }
+  // Alphabet
+  const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  // Check if all the letter of the alphabet are in the map with the letters of the word
+  for (const l of alphabet) {
+    // if l is in letters
+    //console.log("isPangrama l: " + l);
+    if (!letters.get(l)) {
+      console.log("No es un Pangrama");
+      return false
+    }
+  }
+  // is a Pangrama
+  console.log("Es un pangrama");
+  return true;
 }
 
 function isInArray(element, array) {
   for (let i = 0; i < array.length; i++) {
-    if (element === array[i]) return TRUE;
+    if (element === array[i]) return true;
   }
-  return FALSE;
+  return false
 }
-  
+
+
 isHeterograma("yuxtaponer");
 isIsograma("yuxtaponer");
 isPangrama("yuxtaponer");
@@ -81,11 +127,11 @@ isHeterograma("luteranismo");
 isIsograma("luteranismo");
 isPangrama("luteranismo");
 isHeterograma("acondicionar");
-isIsograma("acondicionar");
+isIsograma("abdomen");
 isPangrama("acondicionar");
 isHeterograma("escritura");
 isIsograma("escritura");
 isPangrama("escritura");
-isHeterograma("Benjamín pidió una bebida de kiwi y fresa");
-isIsograma("Benjamín pidió una bebida de kiwi y fresa");
-isPangrama("Benjamín pidió una bebida de kiwi y fresa");
+isHeterograma("Benjamín pidió una bebida de kiwi y fresa. Noé, sin vergüenza, la más exquisita champaña del menú.");
+isIsograma("Benjamín pidió una bebida de kiwi y fresa. Noé, sin vergüenza, la más exquisita champaña del menú.");
+isPangrama("Benjamín pidió una bebida de kiwi y fresa. Noé, sin vergüenza, la más exquisita champaña del menú.");
