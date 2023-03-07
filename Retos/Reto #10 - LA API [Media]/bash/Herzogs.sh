6a267c5766ae694a2430503e8418a65180c0d239
Cristian Feldman <cris@d4rk.ml>
2023-03-06 23:22:15 -0300

    add bash solution

---
+++ Retos/Reto #10 - LA API [Media]/bash/Herzogs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -s -k -X GET https://www.fruityvice.com/api/fruit/banana
---
+++ Retos/Reto #8 - EL GENERADOR PSEUDOALEATORIO [Media]/c/Herzogs.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+typedef struct {
+    int sem = 3;
+    int const_mult = 2;
+    int resto = 32;
+    int const_adit=5;
+} Confiugracion;
+
+int main (const int nargs, char ** vargs){
+    
+}
