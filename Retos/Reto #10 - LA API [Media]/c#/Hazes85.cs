75c2595e2ca947a74f7d520c4e99b03614951131
Hazes85 <64097578+Hazes85@users.noreply.github.com>
2023-03-08 23:55:15 +0100

    Solucion reto 10

--- Retos/Reto #10 - LA API [Media]/c#/Hazes85.cs
+++ Retos/Reto #10 - LA API [Media]/c#/Hazes85.cs
@@ -1,28 +1,26 @@
 using System.Xml.Linq;
 
-namespace prueba
+
+public class Program
 {
-    internal class prueba
+    static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
+        using (HttpClient httpClient = new HttpClient())
         {
-            using (HttpClient httpClient = new HttpClient())
+            HttpResponseMessage response = await httpClient.GetAsync("https://api.geekdo.com/xmlapi/search?search=arkham horror");
+
+            if (response.IsSuccessStatusCode)
             {
-                HttpResponseMessage response = await httpClient.GetAsync("https://api.geekdo.com/xmlapi/search?search=arkham horror");
+                var stream = response.Content.ReadAsStream();
+                StreamReader sr = new StreamReader(stream, System.Text.Encoding.UTF8);
+                var result = sr.ReadToEnd();
 
-                if (response.IsSuccessStatusCode)
+                List<XElement> boardGames = XElement.Parse(result).Elements().ToList();
+
+                boardGames.ForEach(boardGame =>
                 {
-                    var stream = response.Content.ReadAsStream();
-                    StreamReader sr = new StreamReader(stream, System.Text.Encoding.UTF8);
-                    var result = sr.ReadToEnd();
-
-                    List<XElement> boardGames = XElement.Parse(result).Elements().ToList();
-
-                    boardGames.ForEach(boardGame =>
-                    {
-                        Console.WriteLine(boardGame.Elements().Where(e => e.Name.LocalName == "name").Single().Value);
-                    });
-                }
+                    Console.WriteLine(boardGame.Elements().Where(e => e.Name.LocalName == "name").Single().Value);
+                });
             }
         }
     }
