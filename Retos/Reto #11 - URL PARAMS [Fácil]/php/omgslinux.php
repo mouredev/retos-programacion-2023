b80f741430c3438cbb763f3290af2b3ff805c7af
omgslinux <omgs01@gmail.com>
2023-03-13 23:01:18 +0100

    Modificaciones a reto #11 - PHP

--- Retos/Reto #11 - URL PARAMS [Fácil]/php/omgslinux.php
+++ Retos/Reto #11 - URL PARAMS [Fácil]/php/omgslinux.php
@@ -8,12 +8,27 @@
  * los parámetros serían ["2023", "0"]
  */
 
-$URL = "https://retosdeprogramacion.com?year=2023&challenge=0";
 
-$strip = explode('?', $URL);
-$args = explode('&', $strip[1]);
+$URLS = [
+    'https://retosdeprogramacion.com?year=2023&challenge=0',
+    'https://www.youtube.com/watch?v=TedBu1kt5kk',
+    'https://www.palabreto.com/'
+];
 
-foreach ($args as $string) {
-    $param = explode('=', $string);
-    printf("clave: %s, valor: %s\n", $param[0], $param[1]);
+$contador = 0;
+foreach ($URLS as $URL) {
+    $contador++;
+    printf("URL %d: %s\n", $contador, $URL);
+    $strip = explode('?', $URL);
+    if (count($strip)>1) {
+        $args = explode('&', $strip[1]);
+        $c = 0;
+        foreach ($args as $string) {
+            $c++;
+            $param = explode('=', $string);
+            printf("argumento %d: %s, valor: %s\n", $c, $param[0], $param[1]);
+        }
+    } else {
+        printf("La URL %s no tiene argumentos\n", $URL);
+    }
 }
