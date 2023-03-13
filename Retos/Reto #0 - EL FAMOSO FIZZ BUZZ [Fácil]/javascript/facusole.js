8d194953755c6a1dc2ac1e200479ec3c2549e73c
Facundo <facundojsole@gmail.com>
2023-03-13 16:02:08 -0300

    "Reto #0 - Javascript"

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/javascript/facusole.js
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/javascript/facusole.js
@@ -12,6 +12,6 @@ const fizzBuzz = () => {
         if( i % 5 === 0 && i % 3 === 0 ) console.log('fizzbuzz')
         else if ( i % 3 === 0 ) console.log('fizz')
         else if ( i % 5 === 0 ) console.log('buzz')
-        else console.log(i)
+        else console.log(i)      
     }
 }
