a50fa91cf91e05a3cdd019cca933c73fcc795adc
Emill <lemito66@gmail.com>
2023-03-13 17:40:01 -0500

    listo isograma

--- Retos/Reto #9 - HETEROGRAMA, ISOGRAMA Y PANGRAMA [Fácil]/python/Lemito66.py
+++ Retos/Reto #9 - HETEROGRAMA, ISOGRAMA Y PANGRAMA [Fácil]/python/Lemito66.py
@@ -5,6 +5,9 @@
 """
 
 def heterograma(words: str):
+    '''
+    Un heterograma es una palabra o frase que no contiene ninguna letra repetida.
+    '''
     diccionary_of_words = {}
     response = ''
     lower_word = words.lower()
@@ -16,6 +19,33 @@ def heterograma(words: str):
             diccionary_of_words[lower_word[i]] = 1
     response += 'Es un heterograma'
     return response
+
+def isograma(words: str):
+    '''
+    Un isograma es una palabra o frase en la que cada letra aparece el mismo número de veces.
+    '''
+    dictionary_of_words = {}
+    response = ''
+    lower_word = words.lower()
+    for i in range(len(lower_word)):
+        if lower_word[i] in dictionary_of_words:
+            dictionary_of_words[lower_word[i]] += 1
+        else:
+            dictionary_of_words[lower_word[i]] = 1
+            
+    for value in dictionary_of_words.values():
+        if value > 1:
+            response += 'No es un isograma'
+            return response
+    response += 'Es un isograma'
+    return response
+
+print(isograma("Víctima"))
+print(isograma("Políglota"))
+print(isograma("Abstemio"))
+print(isograma("Desoxirribonucleico"))
+print(isograma("Hipopótamo"))
+print(isograma("Benzodiacepina"))
 
 print(heterograma("Víctima"))
 print(heterograma("Wágner"))
