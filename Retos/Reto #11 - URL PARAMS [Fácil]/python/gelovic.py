a241949f8db50ef9db302d3fe072b436501fbcfb
Ángel de Fez <angeldefez@gmail.com>
2023-03-15 20:59:03 +0100

    versión simplificada

--- Retos/Reto #11 - URL PARAMS [Fácil]/python/gelovic.py
+++ Retos/Reto #11 - URL PARAMS [Fácil]/python/gelovic.py
@@ -1,12 +1,5 @@
 # obtenemos una lista con los valores de los parámetros de la URL
 def obtener_valores(url):
-	try:
-		parametros = url.split('?')[1].split('&') # obtenemos una lista con los parámetros
-		parametros = dict([p.split('=') for p in parametros]) # creamos un diccionario clave/valor
-		return [v for v in parametros.values()] # devolvemos una lista con los valores
-	except:
-		return False # si no hay parámetros o la URL está mal formada, devolvemos False
+	return [p.split('=')[1] for p in url.split('?')[1].split('&')]
 
-url = 'https://retosdeprogramacion.com?year=2023&challenge=0'
-valores = obtener_valores(url)
-print(valores)
+print(obtener_valores('https://retosdeprogramacion.com?year=2023&challenge=0'))
