4ca4cb9fef697bf2fff217c5d8c5419c58301536
Rascasse80 <127511073+Rascasse80@users.noreply.github.com>
2023-03-16 19:39:02 +0100

    Create Rascasse80.py

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/python/Rascasse80.py
@@ -0,0 +1,11 @@
+x = 1
+while (x<=100):
+    if (x % 3 == 0):
+          print ("fizz \n")
+    elif (x % 5 == 0):
+          print ("buzz \n")
+    elif (x % 15 == 0):
+          print ("fizzbuzz \n")
+    else:
+          print (x,"\n")
+    x = x+1
