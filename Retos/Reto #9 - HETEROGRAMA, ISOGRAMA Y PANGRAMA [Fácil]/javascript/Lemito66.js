df20b2afbeb34eaed9449a051e6163c0df8a6f52
Emill <lemito66@gmail.com>
2023-03-17 11:33:00 -0500

    listo Pangrama

--- Retos/Reto #9 - HETEROGRAMA, ISOGRAMA Y PANGRAMA [Fácil]/javascript/Lemito66.js
+++ Retos/Reto #9 - HETEROGRAMA, ISOGRAMA Y PANGRAMA [Fácil]/javascript/Lemito66.js
@@ -1,7 +1,7 @@
 /*
 
  * Crea 3 funciones, cada una encargada de detectar si una cadena de
- * texto es un isHeterogram, un isograma o un pangrama.
+ * texto es un isHeterogram, un isHeterogram o un pangrama.
  * - Debes buscar la definición de cada uno de estos términos.
 
 */
@@ -55,11 +55,36 @@ const isIsogram = (word) => {
     }
     for (const letter in objectOfLetters) {
         if (objectOfLetters[letter] > 1) {
-            response += "No es un isograma"
+            response += "No es un isHeterogram"
             return response
         }
     }
-    response += "Es un isograma"
+    response += "Es un isHeterogram"
+    return response
+}
+
+const isPangram = (word) => {
+    word = replaceAccents(word).toLowerCase();
+    alphabet = {
+        'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 
+        'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 
+        'm': 0, 'n': 0, 'ñ': 0, 'o': 0, 'p': 0, 'q': 0, 
+        'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 
+        'x': 0, 'y': 0, 'z': 0
+    }
+    response = ""
+    for (let index = 0; index < word.length; index++) {
+        if (word[index] in alphabet) {
+            alphabet[word[index]] += 1
+        }
+    }
+    for (const letter in alphabet) {
+        if (alphabet[letter] == 0) {
+            response += "No es un pangrama"
+            return response
+        }
+    }
+    response += "Es un pangrama"
     return response
 }
 
@@ -70,3 +95,16 @@ console.log(isHeterogram("Wágner"))
 console.log(isHeterogram("Queso"))
 console.log(isHeterogram("Néctar"))
 console.log(isHeterogram("Lánguido"))
+
+
+console.log(isHeterogram("Víctima"))
+console.log(isHeterogram("Políglota"))
+console.log(isHeterogram("Abstemio"))
+console.log(isHeterogram("Desoxirribonucleico"))
+console.log(isHeterogram("Hipopótamo"))
+console.log(isHeterogram("Benzodiacepina"))
+
+
+print(pangrama("El veloz murciélago hindú comía feliz cardillo y kiwi. La cigüeña tocaba el saxofón detrás del palenque de paja"))
+print(pangrama("Jovencillo emponzoñado de whisky, qué figurota exhibe. Cadáveres de ñus, paz y asombro, ¿qué más añadir?"))
+print(pangrama("El pingüino Wenceslao hizo kilómetros bajo exhaustiva lluvia y frío, añoraba a su querido cachorro."))
