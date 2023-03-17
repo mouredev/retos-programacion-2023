46f10b046248f1c38cde8aafb8c185e9172c52fe
Emill <lemito66@gmail.com>
2023-03-17 12:46:45 -0500

    iniciando

---
+++ Retos/Reto #11 - URL PARAMS [Fácil]/python/Lemito66.py
@@ -0,0 +1,7 @@
+"""
+ * Dada una URL con parámetros, crea una función que obtenga sus valores.
+ * No se pueden usar operaciones del lenguaje que realicen esta tarea directamente.
+ *
+ * Ejemplo: En la url https://retosdeprogramacion.com?year=2023&challenge=0
+ * los parámetros serían ["2023", "0"] 
+"""
