bc4381456e6bc4704d2c49d8db2374decd1f2a78
hernanes338 <auherlpe@gmail.com>
2023-03-21 11:55:10 +0100

    Reto #12 - Python

--- Retos/Reto #12 - VIERNES 13 [Fácil]/python/hernanes338.py
+++ Retos/Reto #12 - VIERNES 13 [Fácil]/python/hernanes338.py
@@ -4,9 +4,14 @@ def isFriday13(month: int, year: int):
     days_of_month = calendar.monthcalendar(year, month)
     fridays_of_month = list(days_of_month[i][4] for i in range(len(days_of_month)))
     if 13 in fridays_of_month:
-        return True
+        return f"There is a Friday 13 on {month}/{year}"
     else:
-        return False
+        return f"There is no Friday 13 on {month}/{year}"
 
-print(isFriday13(3,2023)) # False
-print(isFriday13(3,2020)) # True
+
+## Start program execution ##
+
+print(isFriday13(3,2023)) # There is no Friday 13 on 3/2023
+print(isFriday13(3,2020)) # There is a Friday 13 on 3/2020
+
+## End program execution ##
