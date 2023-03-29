037ceffd3d6b94189bd0e57f76170b66dd753ea7
Emill <lemito66@gmail.com>
2023-03-29 11:01:29 -0500

    listo

--- Retos/Reto #11 - URL PARAMS [Fácil]/python/Lemito66.py
+++ Retos/Reto #11 - URL PARAMS [Fácil]/python/Lemito66.py
@@ -5,3 +5,15 @@
  * Ejemplo: En la url https://retosdeprogramacion.com?year=2023&challenge=0
  * los parámetros serían ["2023", "0"] 
 """
+
+def detect_parameters(url):
+    parameters = []
+    separate_ampersands = url.split("&") # Split a string into a list where each word is a list item:
+    for separate_ampersand in separate_ampersands:
+        if "=" in separate_ampersand:
+            parameter = separate_ampersand.split("=")
+            if len(parameter) == 2 and parameter[1] != "":
+                parameters.append(parameter[1])
+    return parameters
+
+print(detect_parameters("https://retosdeprogramacion.com?year=2023&challenge=0&Lemito66=Fantastic"))
