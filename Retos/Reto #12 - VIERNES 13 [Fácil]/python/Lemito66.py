f27e37dbcfeb3f2fd79c864680842f8edd625a8b
Emill <lemito66@gmail.com>
2023-03-29 11:35:57 -0500

    Iniciando

---
+++ Retos/Reto #12 - VIERNES 13 [Fácil]/python/Lemito66.py
@@ -0,0 +1,4 @@
+""" 
+ * Crea una función que sea capaz de detectar si existe un viernes 13 en el mes y el año indicados.
+ * - La función recibirá el mes y el año y retornará verdadero o falso.
+"""
