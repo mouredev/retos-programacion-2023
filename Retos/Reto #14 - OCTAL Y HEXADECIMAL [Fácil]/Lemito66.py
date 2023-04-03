3cd6b3cdec93dabeddf6202df3f8eb5626a329ee
Emill <lemito66@gmail.com>
2023-04-03 17:06:40 -0500

    Iniciando

---
+++ Retos/Reto #14 - OCTAL Y HEXADECIMAL [Fácil]/Lemito66.py
@@ -0,0 +1,6 @@
+"""
+* Crea una función que reciba un número decimal y lo trasforme a Octal
+ * y Hexadecimal.
+ * - No está permitido usar funciones propias del lenguaje de programación que
+ * realicen esas operaciones directamente. 
+"""
