ad85bd2c8a03c85d21825efbd0ace3a279417df6
Emill <lemito66@gmail.com>
2023-04-03 17:56:58 -0500

    casi listo

--- Retos/Reto #14 - OCTAL Y HEXADECIMAL [Fácil]/Lemito66.py
+++ Retos/Reto #14 - OCTAL Y HEXADECIMAL [Fácil]/Lemito66.py
@@ -4,3 +4,13 @@
  * - No está permitido usar funciones propias del lenguaje de programación que
  * realicen esas operaciones directamente. 
 """
+
+def to_hexadecimal(number: int):
+    response = []
+    number_hexadecimal = []
+    while number > 0:
+        response.append(number % 16)
+        number = number // 16
+    return number_hexadecimal
+    #return response[::-1]
+print(to_hexadecimal(500))
