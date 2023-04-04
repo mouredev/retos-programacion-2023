4295689e7d0ea53300db6de094eb8f1f311cd637
Emill <lemito66@gmail.com>
2023-04-04 11:51:54 -0500

    Correción de error en hexadecimal y octal

--- Retos/Reto #14 - OCTAL Y HEXADECIMAL [Fácil]/python/Lemito66.py
+++ Retos/Reto #14 - OCTAL Y HEXADECIMAL [Fácil]/python/Lemito66.py
@@ -11,9 +11,10 @@ def to_hexadecimal(number: int):
     while number > 0:
         list_response.append(number % 16)
         number = number // 16
-    for i in list_response:
+    for i in list_response[::-1]:
         response += str(i)
-    return int(response[::-1])
+    return int(response)
+    
 
 def to_octal(number: int):
     list_response = []
@@ -21,12 +22,12 @@ def to_octal(number: int):
     while number > 0:
         list_response.append(number % 8)
         number = number // 8
-    for i in list_response:
+    for i in list_response[::-1]:
         response += str(i)
-    return int(response[::-1])
+    return int(response)
 
 def to_hexadecimal_and_to_octal(number: int):
     return f'Hexadecimal: {to_hexadecimal(number)}, Octal: {to_octal(number)}'
 
 
-print(to_hexadecimal_and_to_octal(500))
+print(to_hexadecimal_and_to_octal(4321))
