3e9f0e1e38d25947ab1eccda90e78f4fe8da0b11
Emill <lemito66@gmail.com>
2023-04-04 17:00:56 -0500

    iniciando

---
+++ Retos/Reto #13 - ADIVINA LA PALABRA [Media]/python/Lemito66.py
@@ -0,0 +1,17 @@
+"""
+ * Crea un pequeño juego que consista en adivinar palabras en un número máximo de intentos:
+ * - El juego comienza proponiendo una palabra aleatoria incompleta
+ *   - Por ejemplo "m_ur_d_v", y el número de intentos que le quedan
+ * - El usuario puede introducir únicamente una letra o una palabra (de la misma longitud que
+ *   la palabra a adivinar)
+ *   - Si escribe una letra y acierta, se muestra esa letra en la palabra. Si falla, se resta
+ *     uno al número de intentos
+ *   - Si escribe una resolución y acierta, finaliza el juego, en caso contrario, se resta uno
+ *     al número de intentos
+ *   - Si el contador de intentos llega a 0, el jugador pierde
+ * - La palabra debe ocultar de forma aleatoria letras, y nunca puede comenzar ocultando más del 60%
+ * - Puedes utilizar las palabras que quieras y el número de intentos que consideres 
+"""
+
+import random
+
