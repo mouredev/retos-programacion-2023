47a24c7a2f8b1c2585f0e2ed1fb2b7d7d9af4445
Emill <lemito66@gmail.com>
2023-04-04 13:29:30 -0500

    solución de error en to_hexadecimal

--- Retos/Reto #14 - OCTAL Y HEXADECIMAL [Fácil]/python/Lemito66.py
+++ Retos/Reto #14 - OCTAL Y HEXADECIMAL [Fácil]/python/Lemito66.py
@@ -8,12 +8,31 @@
 def to_hexadecimal(number: int):
     list_response = []
     response = ''
+    convertions = {
+        0: '0',
+        1: '1',
+        2: '2',
+        3: '3',
+        4: '4',
+        5: '5',
+        6: '6',
+        7: '7',
+        8: '8',
+        9: '9',
+        10: 'A',
+        11: 'B',
+        12: 'C',
+        13: 'D',
+        14: 'E',
+        15: 'F'
+    }
     while number > 0:
         list_response.append(number % 16)
         number = number // 16
     for i in list_response[::-1]:
-        response += str(i)
-    return int(response)
+        if i in convertions:
+            response += convertions[i]
+    return (response)
     
 
 def to_octal(number: int):
@@ -30,4 +49,5 @@ def to_hexadecimal_and_to_octal(number: int):
     return f'Hexadecimal: {to_hexadecimal(number)}, Octal: {to_octal(number)}'
 
 
-print(to_hexadecimal_and_to_octal(4321))
+#print(to_hexadecimal_and_to_octal(4321))
+print(to_hexadecimal(1500))
