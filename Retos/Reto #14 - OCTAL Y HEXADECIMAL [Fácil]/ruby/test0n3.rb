543f515d0cf9a4718e683582387e77aaf415f7fb
Carlos A <cayala.w@gmail.com>
2023-04-05 21:39:00 -0500

    reto #14 - ruby

--- Retos/Reto #14 - OCTAL Y HEXADECIMAL [Fácil]/ruby/test0n3.rb
+++ Retos/Reto #14 - OCTAL Y HEXADECIMAL [Fácil]/ruby/test0n3.rb
@@ -5,30 +5,30 @@ def transform_to_octal_hex(number)
 end
 
 def transform_to_octal(number)
-  octal = []
-  cocient = number
-
   return '0' if number.zero? || !number.is_a?(Integer)
 
-  # num = number
-  while cocient != 0
-    remain = cocient % 8
-    cocient = cocient.div(8)
+  octal = []
+  quotient = number
+
+  while quotient != 0
+    quotient, remain = quotient.divmod(8)
     octal.unshift(remain.to_s)
   end
   octal.join('')
 end
 
 def transform_to_hex(number)
-  hex = []
-  cocient = number
+  hex_values = { 0 => '0', 1 => '1', 2 => '2', 3 => '3', 4 => '4', 5 => '5', 6 => '6', 7 => '7',
+                 8 => '8', 9 => '9', 10 => 'A', 11 => 'B', 12 => 'C', 13 => 'D', 14 => 'E', 15 => 'F' }
 
   return '0' if number.zero? || !number.is_a?(Integer)
 
-  while cocient != 0
-    remain = cocient % 16
-    cocient = cocient.div(16)
-    hex.unshift(remain.to_s)
+  hex = []
+  quotient = number
+
+  while quotient != 0
+    quotient, remain = quotient.divmod(16)
+    hex.unshift(hex_values[remain])
   end
   hex.join('')
 end
