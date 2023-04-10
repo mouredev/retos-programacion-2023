4eb610ab0b1245abb29ce1847be773e1be8c0460
Qv1ko <vicgarmur947@gmail.com>
2023-04-10 23:11:12 +0200

    Reto #14 - Java

--- Retos/Reto #14 - OCTAL Y HEXADECIMAL [Fácil]/java/Qv1ko.java
+++ Retos/Reto #14 - OCTAL Y HEXADECIMAL [Fácil]/java/Qv1ko.java
@@ -8,7 +8,7 @@ public class Qv1ko {
         int number=decimal;
         String octal="";
         String hex="";
-        char[] hexCharacters={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        char[] hexValues={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         while(number>0) {
             octal=number%8+octal;
             number/=8;
@@ -17,7 +17,7 @@ public class Qv1ko {
         System.out.println(decimal+" in octal is "+octal);
         number=decimal;
         while(number>0) {
-            hex=hexCharacters[number%16]+hex;
+            hex=hexValues[number%16]+hex;
             number/=16;
         }
         hex=(hex.equals(""))? "0":hex;
