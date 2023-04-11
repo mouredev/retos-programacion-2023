af4ef370c705a705862d0816a64aa9c0d367e19d
Emill <lemito66@gmail.com>
2023-04-11 09:24:25 -0500

    Iniciando

---
+++ Retos/Reto #15 - AUREBESH [Fácil]/python/Lemito66.py
@@ -0,0 +1,10 @@
+""" 
+* Crea una función que sea capaz de transformar Español al lenguaje básico del universo
+ * Star Wars: el "Aurebesh".
+ * - Puedes dejar sin transformar los caracteres que no existan en "Aurebesh".
+ * - También tiene que ser capaz de traducir en sentido contrario.
+ *  
+ * ¿Lo has conseguido? Nómbrame en twitter.com/mouredev y escríbeme algo en Aurebesh.
+ *
+ * ¡Que la fuerza os acompañe!
+"""
