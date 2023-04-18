28b6c67ecd21ed588c3cccf6016e5fd7c4995820
Emill <lemito66@gmail.com>
2023-04-18 17:55:58 -0500

    iniciando

---
+++ Retos/Reto #16 - LA ESCALERA [Media]/javascript/Lemito66.js
@@ -0,0 +1,14 @@
+/*
+ * Crea una función que dibuje una escalera según su número de escalones.
+ * - Si el número es positivo, será ascendente de izquiera a derecha.
+ * - Si el número es negativo, será descendente de izquiera a derecha.
+ * - Si el número es cero, se dibujarán dos guiones bajos (__).
+ *
+ * Ejemplo: 4
+ *         _
+ *       _|
+ *     _|
+ *   _|
+ * _|
+ *
+ */
