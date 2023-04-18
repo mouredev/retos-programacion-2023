9caacfb38510501bd79000bd8224dfbba9a6bd8a
Samuel Arandia <samuelarandia@gmial.com>
2023-04-18 12:59:24 -0400

    Reto #01 - JavaScript

--- Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/javascript/samuelArandia.js
+++ Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/javascript/samuelArandia.js
@@ -10,56 +10,60 @@
 ```
 
 const hackerAlphabet = {
-    A: "4",
-    B: "I3",
-    C: "[",
-    D: ")",
-    E: "3",
-    F: "|=",
-    G: "&",
-    H: "#",
-    I: "1",
-    J: ",_|",
-    K: ">|",
-    L: "1",
-    M: "/\\/\\",
-    N: "^/",
-    O: "0",
-    P: "|*",
-    Q: "(_,)",
-    R: "I2",
-    S: "5",
-    T: "7",
-    U: "(_)",
-    V: "\\/",
-    W: "\\/\\/",
-    X: "><",
-    Y: "j",
-    Z: "2",
-    1: "L",
-    2: "R",
-    3: "E",
-    4: "A",
-    5: "S",
-    6: "b",
-    7: "T",
-    8: "B",
-    9: "g",
-    0: "o",
+  A: "4",
+  B: "I3",
+  C: "[",
+  D: ")",
+  E: "3",
+  F: "|=",
+  G: "&",
+  H: "#",
+  I: "1",
+  J: ",_|",
+  K: ">|",
+  L: "1",
+  M: "/\\/\\",
+  N: "^/",
+  O: "0",
+  P: "|*",
+  Q: "(_,)",
+  R: "I2",
+  S: "5",
+  T: "7",
+  U: "(_)",
+  V: "\\/",
+  W: "\\/\\/",
+  X: "><",
+  Y: "j",
+  Z: "2",
+  1: "L",
+  2: "R",
+  3: "E",
+  4: "A",
+  5: "S",
+  6: "b",
+  7: "T",
+  8: "B",
+  9: "g",
+  0: "o",
 };
 
 
 function traductor(normalText) {
 
-    let newLeetText = "";
-    for (let i = 0; i < normalText.length; i++) { 
-        if (normalText.charAt(i) === " ") {
-            newLeetText += " ";
-        } else {
-            newLeetText += hackerAlphabet[`${normalText.charAt(i).toLowerCase()}`];
-        }
-        return newLeetText;
+  let newLeetText = "";
+  for (let i = 0; i < normalText.length; i++) {
+    if (normalText.charAt(i) === " ") {
+      console.log("1", normalText.charAt(i));
+      newLeetText += " ";
+      console.log("2", newLeetText);
+    } else {
+      console.log("3", normalText.charAt(i).toUpperCase());
+      newLeetText += hackerAlphabet[`${normalText.charAt(i).toUpperCase()}`];
+      console.log("4", newLeetText);
     }
+  }
+  console.log(newLeetText);
 }
 
-console.log(traductor("hi my birthday is October 21"));
+traductor("leet speak is fun");
