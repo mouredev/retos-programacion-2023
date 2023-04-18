72a83deb72c58b29dbb36e39ad518fa13c7f693f
Emill <lemito66@gmail.com>
2023-04-18 15:57:04 -0500

    refactorizando

--- Retos/Reto #16 - LA ESCALERA [Media]/python/Lemito66.py
+++ Retos/Reto #16 - LA ESCALERA [Media]/python/Lemito66.py
@@ -17,22 +17,30 @@
 
 
 def ladder(number: int) -> str:
-    up_to_0 = ''
-    down_to_0 = ''
+    """The objective of the ladder function is to draw a ladder according to the number of steps provided as input. The ladder can be ascending or descending, depending on whether the input number is positive or negative, respectively. If the input number is zero, the function will return two underscores.
+
+    Args:
+        number (int):  number: an integer representing the number of steps in the ladder.
+
+    Returns:
+        str: ladder: a string representing the ladder.
+    """
+    greather_than_0 = ''
+    less_than_0 = ''
     if number == 0:
         return '__'
     elif number > 0:
-        up_to_0 += ' '*(number+1) + '_\n'
+        greather_than_0 += f"{' '*(number+2)}_\n"
         for i in range(number):
-            up_to_0 += ' '*(number-i-1) + '_|\n'
-        return up_to_0
+            greather_than_0 += ' '*(number-i) + '_|\n'
+        return greather_than_0
     else:
+
         for i in range(abs(number)):
-            down_to_0 += ' '*i + '|_\n'
-            # print(' '*i + '|_')
-        down_to_0 += ' '*(i+2) + "|"
-        return down_to_0
+            less_than_0 += ' '*i + '|_\n'
+        less_than_0 += f"{' '*(abs(number)+1)}|"
+        return less_than_0
 
 
 print(ladder(-3))
-
+print(ladder(3))
