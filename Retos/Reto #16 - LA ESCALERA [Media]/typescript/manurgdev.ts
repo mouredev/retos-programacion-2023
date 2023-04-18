07846bb4c1b3f3b4b12af6d8efdc8541697d484f
ManuRGDev <manurgdev@gmail.com>
2023-04-18 21:36:39 +0200

    style: remove statement

--- Retos/Reto #16 - LA ESCALERA [Media]/typescript/manurgdev.ts
+++ Retos/Reto #16 - LA ESCALERA [Media]/typescript/manurgdev.ts
@@ -1,18 +1,3 @@
-/*
- * Crea una función que dibuje una escalera según su número de escalones.
- * - Si el número es positivo, será ascendente de izquiera a derecha.
- * - Si el número es negativo, será descendente de izquiera a derecha.
- * - Si el número es cero, se dibujarán dos guiones bajos (__).
- * 
- * Ejemplo: 4
- *         _
- *       _|       
- *     _|
- *   _|
- * _|
- * 
- */
-
 const createStairs = (n: number): string => {
     if (n === 0) return '__';
 
