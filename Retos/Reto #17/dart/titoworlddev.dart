aff6e987ddd55a20e1a464fd7a5e2132cf070f32
Cristian Arias Mejuto <titocristian.dev@gmail.com>
2023-04-29 18:36:04 +0200

    Finalizado Reto #17

--- Retos/Reto #17/dart/titoworlddev.dart
+++ Retos/Reto #17/dart/titoworlddev.dart
@@ -20,15 +20,42 @@
  * 
  */
 
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
+import 'dart:io';
 import 'dart:convert';
 
 const URL = 'https://api.github.com';
 const USER = 'mouredev';
 const REPO = 'retos-programacion-2023';
 
-void main() async {
-  print(await http
-      .get(Uri.parse('$URL/repos/$USER/$REPO/commits?per_page=10'))
-      .then((res) => jsonDecode(res.body)));
+void main() {
+  printCommits(10);
+}
+
+void printCommits(int commitsLength) async {
+  // final data = await http
+  //     .get(Uri.parse('$URL/repos/$USER/$REPO/commits'))
+  //     .then((res) => jsonDecode(res.body));
+
+  /* Aunque es mas facil con http, dependiendo de como y donde lo ejecutes se 
+    puede hacer la peticion con dart:io ya que http necesita 
+    un proyecto de dart para instalar las dependencias y poder usarlas 
+    pero dart:io no lo necesita */
+  final data = await HttpClient()
+      .getUrl(Uri.parse('$URL/repos/$USER/$REPO/commits'))
+      .then((req) => req.close())
+      .then((res) => res.transform(utf8.decoder).join())
+      .then((res) => jsonDecode(res));
+
+  for (int i = 1; i <= commitsLength; i++) {
+    final hash = data[i]['sha'].substring(0, 7).toUpperCase();
+    final commit = data[i]['commit'];
+    final name = commit['author']['name'];
+    final message = commit['message'];
+    final date = DateTime.parse(commit['author']['date']);
+    final dateFormated =
+        '${date.day}/${date.month}/${date.year} ${date.hour}:${date.minute}';
+
+    print('Commit $i | $hash | $name | $message | $dateFormated\n\n');
+  }
 }
