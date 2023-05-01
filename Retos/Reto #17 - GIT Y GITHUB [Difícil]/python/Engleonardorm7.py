619097444aedf49aee21ef2c00634f4d9dafb789
Leonardo Rodriguez <leonardorm7@hotmail.com>
2023-05-01 11:41:59 -0500

    Reto #17 - Python

--- Retos/Reto #17 - GIT Y GITHUB [Difícil]/python/Engleonardorm7.py
+++ Retos/Reto #17 - GIT Y GITHUB [Difícil]/python/Engleonardorm7.py
@@ -1,13 +1,10 @@
 from github import Github
 
-# Access the repository
-g = Github("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")
+g = Github("pon tu access token aqui...")
 repo = g.get_repo("mouredev/retos-programacion-2023")
 
-# Get the last 10 commits
 commits = repo.get_commits()[:10]
 
-# Display commit information
 for i, commit in enumerate(commits):
     sha = commit.sha[:7]
     author = commit.commit.author.name
