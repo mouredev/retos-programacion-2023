bc182eb1174271ef8ac2273939d6925a094d5d2b
Santos Alarcón Asensio <santosalarcon86@gmail.com>
2023-05-17 11:30:22 +0200

    Update SantosAlarcon.js
    Modify: quitar espacio en la primera línea

--- Retos/Reto #20 - LA TRIFUERZA [Media]/javascript/SantosAlarcon.js
+++ Retos/Reto #20 - LA TRIFUERZA [Media]/javascript/SantosAlarcon.js
@@ -5,7 +5,6 @@ const dibujarTrifuerza = (n) => {
 
     for (let f = 0; f < n; f++) {
         triangulo += '  '.repeat(n-f-1) + arriba + (' ' + arriba).repeat(f) + '\n';
-      if (f === 0) triangulo = triangulo.slice(1);
         triangulo += '  '.repeat(n-f-1) + abajo + (' ' + abajo).repeat(f) + '\n';
     }
 
