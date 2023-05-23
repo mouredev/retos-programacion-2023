efc516f57de3a66fe8e4a25da69103971846c4b7
Santos Alarcón <santosalarcon86@gmail.com>
2023-05-23 14:45:16 +0200

    Añadido Reto de JavaScript

--- Retos/Reto #20 - LA TRIFUERZA [Media]/javascript/SantosAlarcon.js
+++ Retos/Reto #20 - LA TRIFUERZA [Media]/javascript/SantosAlarcon.js
@@ -1,15 +1,37 @@
 const dibujarTrifuerza = (n) => {
-    let arriba = ' * ';
-    let abajo = '***'
-    let triangulo = "";
+    let espacio;
+    let espacio_mitad;
+    let asterisco_fila;
+    let fila_actual;
 
-    for (let f = 0; f < n; f++) {
-        triangulo += '  '.repeat(n-f-1) + arriba + (' ' + arriba).repeat(f) + '\n';
-        triangulo += '  '.repeat(n-f-1) + abajo + (' ' + abajo).repeat(f) + '\n';
+    // Se controla que el número de filas sea un entero positivo.
+    if (n > 0 && typeof n === "number") {
+
+        // Se dibujará un triángulo con el doble de las filas que se han introducido.
+        for (let fila = 0; fila < n * 2; fila++) {
+
+            // La mitad superior dibujará un triángulo de X filas con espacios a los
+            // lados.
+            if (fila < n) {
+                espacio = " ".repeat(((2 * n) - 1) - fila)
+                asterisco_fila = "*".repeat((2 * (fila + 1)) - 1);
+                console.log(espacio + asterisco_fila + espacio);
+            }
+            // La mitad inferior dibujarán los espacios de los lados y el espacio del
+            // medio.
+            else {
+                fila_actual = fila - n;
+                espacio = " ".repeat((n - fila_actual) - 1);
+                espacio_mitad = " ".repeat((2 * (n - fila_actual)) - 1)
+                asterisco_fila = "*".repeat((2 * (fila_actual + 1)) - 1);
+                console.log(espacio + asterisco_fila + espacio_mitad + asterisco_fila);
+            }
+        }
+        // Se muestra un mensaje de error si se introduce una cadena de texto o un
+        // número negativo.
+    } else {
+        console.error("¡ERROR! Sólo puedes introducir números positivos para dibujar la Trifuerza.");
     }
+}
 
-    return triangulo;
-};
-
-console.log(dibujarTrifuerza(10));
-
+dibujarTrifuerza(10);
