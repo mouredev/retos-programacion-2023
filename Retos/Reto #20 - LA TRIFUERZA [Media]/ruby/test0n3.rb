c0061f60e8bb51a08ea485b5025dc1d86ff6d6e8
Carlos A <cayala.w@gmail.com>
2023-05-23 00:48:09 -0500

    Corrección código para cumplir con lo requerido

--- Retos/Reto #20 - LA TRIFUERZA [Media]/ruby/test0n3.rb
+++ Retos/Reto #20 - LA TRIFUERZA [Media]/ruby/test0n3.rb
@@ -2,40 +2,78 @@
 
 # class TriForce
 class TriForce
-  TRIFORCE = { head: '*', body: '***' }.freeze
+  # def initialize(input)
+  #   @nro_row = input
+  #   @padding = (@nro_row * 4 - 1) / 2
+  #   @iter = 1
+  #   @triangle = ''
+  # end
+
+  # def draw_triangles
+  #   @nro_row.times do
+  #     print_heads
+  #     print_tails
+  #   end
+  #   @triangle
+  # end
+
+  # def print_heads
+  #   @triangle += ' ' * @padding
+  #   @iter.times do
+  #     @triangle += '*   '
+  #   end
+  #   @triangle += "\n"
+  #   @padding -= 1
+  # end
+
+  # def print_tails
+  #   @triangle += ' ' * @padding
+  #   @iter.times do
+  #     @triangle += '*** '
+  #   end
+  #   @triangle += "\n"
+  #   @iter += 1
+  #   @padding -= 1
+  # end
+
   def initialize(input)
-    @nro_row = input
-    @padding = (@nro_row * 4 - 1) / 2
-    @iter = 1
+    @nro_rows = input
+    @element_width = 2 * @nro_rows - 1
+    @max_length = @element_width * 2 + 1
+    @external_padding = @max_length / 2
     @triforce = ''
   end
 
   def draw_triforce
-    @nro_row.times do
-      print_heads
-      print_tails
-    end
+    print_head
+    print_base
     @triforce
   end
 
-  def print_heads
-    @triforce += ' ' * @padding
-    @iter.times do
-      @triforce += '*   '
+  def print_head
+    @nro_rows.times do |i|
+      @triforce += "#{' ' * @external_padding}#{gen_stars(i)}\n"
+      @external_padding -= 1
     end
-    @triforce += "\n"
-    @padding -= 1
   end
 
-  def print_tails
-    @triforce += ' ' * @padding
-    @iter.times do
-      @triforce += '*** '
+  def print_base
+    @nro_rows.times do |i|
+      @triforce += "#{' ' * @external_padding}#{gen_stars(i)}#{' ' * @element_width}#{gen_stars(i)}\n"
+      @external_padding -= 1
+      @element_width -= 2
     end
-    @triforce += "\n"
-    @iter += 1
-    @padding -= 1
+  end
+
+  private
+
+  def gen_stars(row)
+    '*' * (row * 2 + 1)
   end
 end
 
+# puts TriForce.new(1).draw_triforce
 # puts TriForce.new(2).draw_triforce
+# puts TriForce.new(3).draw_triforce
+# puts TriForce.new(4).draw_triforce
+# puts TriForce.new(5).draw_triforce
