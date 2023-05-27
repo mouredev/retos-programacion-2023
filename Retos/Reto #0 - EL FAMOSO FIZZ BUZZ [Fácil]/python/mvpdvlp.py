5e99f6ba9bbcc3bf4a6e850259d6cfaed173ffff
mvpdvlp <mvpdvlp@gmail.com>
2023-05-26 20:51:25 -0400

    Reto #0 - python

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/python/mvpdvlp.py
@@ -0,0 +1,29 @@
+def main():
+    for n in range(0,101):
+        print(f"{n} {is_multiple_and_fizz_buzz(n)}")
+
+def is_multiple_and_fizz_buzz(n:int)->str:
+    output = ''
+    if n % 3 ==0:
+        output +='Fizz '
+    
+    if n % 5 == 0:
+        output+='Buzz'
+
+    return output
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    # if n%3 ==0 and n%5 ==0:
+    #     return 'Fizz Buzz'
+    # elif n%3 ==0:
+    #     return 'Fizz'
+    # elif n%5==0:
+    #     return'Buzz'
+    # else:
+    #     return ''
