7c6b97183a8a0e10953d65bae442983e824eb2a3
mvpsdvlpr <65572029+mvpsdvlpr@users.noreply.github.com>
2023-05-26 21:53:26 -0400

    Update mvpdvlp.py
    Clean comments on file

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/python/mvpdvlp.py
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/python/mvpdvlp.py
@@ -14,16 +14,3 @@ def is_multiple_and_fizz_buzz(n:int)->str:
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-    # if n%3 ==0 and n%5 ==0:
-    #     return 'Fizz Buzz'
-    # elif n%3 ==0:
-    #     return 'Fizz'
-    # elif n%5==0:
-    #     return'Buzz'
-    # else:
-    #     return ''
