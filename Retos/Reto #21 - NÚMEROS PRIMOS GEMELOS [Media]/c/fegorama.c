caa62ba17e9d9db6ed6d3b5ccb5d7a04f03f0b0f
Fernando González (Fëgôr) <fegor@fegor.com>
2023-05-30 20:49:15 +0200

    Reto 21 - C

--- Retos/Reto #21 - NÚMEROS PRIMOS GEMELOS [Media]/c/fegorama.c
+++ Retos/Reto #21 - NÚMEROS PRIMOS GEMELOS [Media]/c/fegorama.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+/*
+ * Crea un programa que encuentre y muestre todos los pares de números primos
+ * gemelos en un rango concreto.
+ * El programa recibirá el rango máximo como número entero positivo.
+ * 
+ * - Un par de números primos se considera gemelo si la diferencia entre
+ *   ellos es exactamente 2. Por ejemplo (3, 5), (11, 13)
+ *
+ * - Ejemplo: Rango 14
+ *   (3, 5), (5, 7), (11, 13)
+ */
 
 int isPrime(int n)
 {
     if (n < 3 || n == 4)
         return 0;
 
-    for (int i = 2; i < (n / 2); i++)
+    int max = sqrt(n) + 1;
+
+    for (int i = 2; i < max; i++)
         if (n % i == 0)
             return 0;
 
@@ -19,25 +34,20 @@ int main(int args, char **argv)
 
     if (args != 2)
     {
-        printf("Debe introducir el rango superior.");
+        printf("Debe introducir el rango máximo.");
         return 1;
     }
 
     rg = atoi(argv[1]);
 
-    if (rg > 4)
-        printf("(3, 5)");
-    else
-        return 0;
-
-    for (int i = 4; i <= rg; i++)
+    for (int i = 3; i <= rg; i = i + 2)
     {
         if (isPrime(i))
         {
             diff = i - ant;
-            
+
             if (diff == 2)
-                printf(", (%d, %d)", ant, i);
+                printf("%s(%d, %d)", (i != 5 ? ", " : ""), ant, i);
 
             ant = i;
         }
@@ -45,5 +55,3 @@ int main(int args, char **argv)
 
     return 0;
 }
-
-
