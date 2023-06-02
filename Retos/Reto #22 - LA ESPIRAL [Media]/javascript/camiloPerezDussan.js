11aab5fe3afa77afa80e5eeac389524a6308f809
Camilo Perez <caperez@bancolombia.com.co>
2023-06-02 16:46:17 -0500

    reto #22

--- Retos/Reto #22 - LA ESPIRAL [Media]/javascript/camiloPerezDussan.js
+++ Retos/Reto #22 - LA ESPIRAL [Media]/javascript/camiloPerezDussan.js
@@ -1,3 +1,16 @@
+/*
+ * Crea una función que dibuje una espiral como la del ejemplo.
+ * - Únicamente se indica de forma dinámica el tamaño del lado.
+ * - Símbolos permitidos: ═ ║ ╗ ╔ ╝ ╚
+ *
+ * Ejemplo espiral de lado 5 (5 filas y 5 columnas):
+ * ════╗
+ * ╔══╗║
+ * ║╔╗║║
+ * ║╚═╝║
+ * ╚═══╝
+ */
+
 let espiral = size => {
     let espiral = '═'.repeat(size - 1) + '╗'
     for (let row = 1; row < size; row++) {
@@ -11,6 +24,6 @@ let espiral = size => {
 espiral(1)
 espiral(3)
 espiral(5)
-espiral(7)
+espiral(8)
 espiral(10)
 espiral(20)
