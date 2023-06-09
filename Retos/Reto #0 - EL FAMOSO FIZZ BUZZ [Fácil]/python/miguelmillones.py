63150048ee669574b4d8d8635bf5f90488a52806
Miguel Millones <miguelmillones22@gmail.com>
2023-06-09 16:27:21 -0500

    añadiendo mi archivo miguelmillones.py

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/python/miguelmillones.py
@@ -0,0 +1,16 @@
+### Reto #0: EL FAMOSO "FIZZ BUZZ"
+#### Dificultad: Fácil | Publicación: 26/12/22 | Corrección: 02/01/23
+
+# * Escribe un programa que muestre por consola (con un print) los
+# * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+# * cada impresión), sustituyendo los siguientes:
+# * - Múltiplos de 3 por la palabra "fizz".
+# * - Múltiplos de 5 por la palabra "buzz".
+# * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+# */
+
+numero_inicial=0
+numero_final=100
+
+lista=range(numero_inicial,numero_final,1)
+print(lista)
