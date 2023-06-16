b7cf402d9eccdf5b4bdac8191213e491f9f31524
Leonardo Rodriguez <leonardorm7@hotmail.com>
2023-06-16 11:33:13 -0500

    Reto #24 - Python

---
+++ Retos/Reto #23 - LA BASE DE DATOS [Media]/python/Engleonardorm7.py
@@ -0,0 +1,14 @@
+# /*
+#  * Realiza una conexión desde el lenguaje que hayas seleccionado a la siguiente
+#  * base de datos MySQL:
+#  * - Host: mysql-5707.dinaserver.com
+#  * - Port: 3306
+#  * - User: mouredev_read
+#  * - Password: mouredev_pass
+#  * - Database: moure_test
+#  * 
+#  * Una vez realices la conexión, lanza la siguiente consulta e imprime el resultado:
+#  * - SELECT * FROM `challenges`
+#  *
+#  * Se pueden usar librerías para realizar la lógica de conexión a la base de datos.
+#  */
