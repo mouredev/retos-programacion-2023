65836418949a89c9f400e2e54e99f73674c4c9e9
hogan26 <nelson.gonzalezguerra@gmail.com>
2023-06-25 22:03:23 -0400

    correccion de situacion de quiebre del match de tenis

--- Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/python/hogan26.py
+++ Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/python/hogan26.py
@@ -13,7 +13,13 @@ def match(secuencia):
 
         if p1 == 3 and p2 == 3: print("Deuce")
         elif p1 == 4 and p2 == 3: print("Ventaja P1")
-        elif p1 == 3 and p2 == 4: print("Ventaje P2")            
+        elif p1 == 4 and p2 < 3: 
+            print("Gana P1")
+            quit()
+        elif p1 == 3 and p2 == 4: print("Ventaja P2")            
+        elif p1 < 3 and p2 == 4:
+            print("Gana P2")
+            quit()
         elif p1 == 4 and p2 == 4: 
             p1-=1
             p2-=1
@@ -27,4 +33,4 @@ def match(secuencia):
         else:
             print(diccionario[p1]+" - "+diccionario[p2])    
 
-match(['P1','P1','P2','P2','P1','P2','P1','P1'])
+match(['P1','P1','P2','P2','P1','P2','P1','P2','P2','P2'])
