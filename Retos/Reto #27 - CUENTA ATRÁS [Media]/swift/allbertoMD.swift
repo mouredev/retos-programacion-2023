55d2f9a95035b6bcd191eaccbdd443aa5c715171
allbertoMD <aldevto@gmail.com>
2023-07-04 20:23:17 +0200

    Reto #27 - Swift

---
+++ Retos/Reto #27 - CUENTA ATRÁS [Media]/swift/allbertoMD.swift
@@ -0,0 +1,17 @@
+
+import Foundation
+
+func countdown(from: UInt32, withIntervals intervals: UInt32) {
+    
+    for number in (0..<from).reversed() {
+        print(number)
+        if number == 0 {
+            break
+        }
+        sleep(intervals)
+    }
+    print("El programa terminó")
+}
+
+countdown(from: 3, withIntervals: 4)
+
