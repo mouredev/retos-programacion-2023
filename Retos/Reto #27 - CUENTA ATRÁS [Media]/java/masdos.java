0f9fd1da6bb2c0fe6ea1cdd4fcd21660ea44f6d1
Marcos Ayllón Sánchez <marcosaysa92@gmail.com>
2023-07-10 20:33:05 +0200

    Reto #27 - Java

---
+++ Retos/Reto #27 - CUENTA ATRÁS [Media]/java/masdos.java
@@ -0,0 +1,18 @@
+import java.time.LocalDateTime;
+
+public class masdos {
+
+  public static void main(String[] args){
+    countDown(10, 1);
+  }
+
+  private static void countDown(int startNumber, int secondsBetweenNumbers) {
+    LocalDateTime timeValid = LocalDateTime.now().plusSeconds(secondsBetweenNumbers);
+    while(startNumber > 0) {
+      if (LocalDateTime.now().isAfter(timeValid)) {
+        System.out.println(--startNumber);
+        timeValid = timeValid.plusSeconds(secondsBetweenNumbers);
+      }
+    }
+  }
+}
