5c7cc66a349ef010f366927459be1781ee1c9c1d
josevegas7 <gonzalezjosejuan28@gmail.com>
2023-07-11 01:32:54 -0430

    primeros retos por josevegas27

---
+++ Retos/Reto #8 - EL GENERADOR PSEUDOALEATORIO [Media]/python/josevegas27.py
@@ -0,0 +1,27 @@
+'''
+	Reto #8: El generador pseudoaleatorio
+/*
+ * Crea un generador de números pseudoaleatorios entre 0 y 100.
+ * - No puedes usar ninguna función "random" (o semejante) del lenguaje de programación seleccionado.
+ *
+ * Es más complicado de lo que parece...
+*/
+'''
+
+from datetime import datetime
+
+lista = []
+def pseudo_random(start=0,finish=100, size=1):
+	for i in range(0,size):
+		a = datetime.now()
+		lista.append(int(str(a)[-5:-3]))
+		a = 2**(1000000 + i)	# Para ralentizar calculos y hacer que avance el tiempo
+
+	print(lista)
+
+randoms = int(input('Introduzca cuantos numeros aleatorios quiere. \n--> '))
+pseudo_random(size=randoms)
+
+# usando hash
+#num = abs(hash('0123456789'))
+#print(num % 101)
