2eeddb5128a2837b6825de2befca628da415aa98
Jose Aliaga <jose.aliaga@externos-ar.cencosud.com>
2023-07-15 10:01:00 -0300

    feat: add parenthesis support

--- Retos/Reto #28 - EXPRESIÓN MATEMÁTICA [Media]/python/jaliagag.py
+++ Retos/Reto #28 - EXPRESIÓN MATEMÁTICA [Media]/python/jaliagag.py
@@ -12,10 +12,24 @@ op = ['+','-','*','/','%']
 
 args = sys.argv[1]
 inp = args.split()
+no_paren = []
+
+for i in inp:
+    if i == '(' or i == ')':
+        pass
+    else:
+        if '(' in i:
+            b = i.replace('(','')
+            no_paren.append(b)
+        elif ')' in i:
+            b = i.replace(')','')
+            no_paren.append(b)
+        else:
+            no_paren.append(i)
 
 
 def check_string(sth):
-    for idx, i in enumerate(inp):
+    for idx, i in enumerate(sth):
         if idx % 2 == 0:
             try:
                 a = int(i)
@@ -32,7 +46,7 @@ def check_string(sth):
 
 
 if __name__ == '__main__':
-    if check_string(inp):
+    if check_string(no_paren):
         print('true')
     else:
         print('false')
