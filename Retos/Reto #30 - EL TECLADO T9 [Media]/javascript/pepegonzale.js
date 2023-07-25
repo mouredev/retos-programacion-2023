e953b0d73ebebfadfedabbe754fff7bcd708c36b
Pepe <94927052+PepeGonzale@users.noreply.github.com>
2023-07-25 08:25:22 +0200

    Reto #30 - Javascript

--- Retos/Reto #30 - EL TECLADO T9 [Media]/javascript/pepegonzale.js
+++ Retos/Reto #30 - EL TECLADO T9 [Media]/javascript/pepegonzale.js
@@ -1,3 +1,19 @@
+/*
+ * Los primeros dispositivos móviles tenían un teclado llamado T9
+ * con el que se podía escribir texto utilizando únicamente su
+ * teclado numérico (del 0 al 9).
+ *
+ * Crea una función que transforme las pulsaciones del T9 a su
+ * representación con letras.
+ * - Debes buscar cuál era su correspondencia original.
+ * - Cada bloque de pulsaciones va separado por un guión.
+ * - Si un bloque tiene más de un número, debe ser siempre el mismo.
+ * - Ejemplo:
+ *     Entrada: 6-666-88-777-33-3-33-888
+ *     Salida: MOUREDEV
+ */
+
+
 const teclado_t9 = (bloque_pulsaciones) => {
     const dictionary = [
       [".", ",", "?", "!"],
@@ -11,11 +27,14 @@ const teclado_t9 = (bloque_pulsaciones) => {
       ["W", "X", "Y", "Z"]
     ]
     const seccion_numeros = bloque_pulsaciones.split("-")
+    
+    
     for (var i = 0; i < seccion_numeros.length; i++) {
       let num = seccion_numeros[i]
-      let howMany = num.length
-      console.log(howMany)
-      console.log(dictionary[howMany][0])
+      let zone = num[0] -1 
+      let howMany = num.length - 1
+      console.log(zone)
+      console.log(dictionary[zone][howMany])
       
     }
   }
