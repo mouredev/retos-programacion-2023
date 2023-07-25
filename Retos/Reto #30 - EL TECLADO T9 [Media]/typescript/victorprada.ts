62fb682d4d54c851281c5dbfe98666abcb30b738
vprada25 <v.prada@udla.edu.co>
2023-07-24 21:00:01 -0500

    feat(victorPrada): keyboard challenge solution

---
+++ Retos/Reto #30 - EL TECLADO T9 [Media]/typescript/victorprada.ts
@@ -0,0 +1,17 @@
+const t9ToText = keystrokes => {
+  const t9Mappings = {
+    '2': 'ABC',
+    '3': 'DEF',
+    '4': 'GHI',
+    '5': 'JKL',
+    '6': 'MNO',
+    '7': 'PQRS',
+    '8': 'TUV',
+    '9': 'WXYZ',
+  }
+
+  return keystrokes
+    .split('-')
+    .map(block => t9Mappings[block[0]][block.length - 1])
+    .join('')
+}
