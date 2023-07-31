61b225b92cf00911ad453df0f103c9613a73fb7e
Fernando Marín <89979472+FernandooMarinn@users.noreply.github.com>
2023-07-31 23:12:10 +0200

    Add files via upload

---
+++ Retos/Reto #31 - EL ÁBACO [Fácil]/fernandoomarinn.py
@@ -0,0 +1,23 @@
+ejemplo = ["O---OOOOOOOO",
+         "OOO---OOOOOO",
+         "---OOOOOOOOO",
+         "OO---OOOOOOO",
+         "OOOOOOO---OO",
+         "OOOOOOOOO---",
+         "---OOOOOOOOO"]
+
+
+def convertir(abaco):
+    multiplicador = 1_000_000
+    num = 0
+
+    for linea in abaco:
+        separados = linea.split("---")
+
+        unidades = len(separados[0])
+
+        num += unidades * multiplicador
+
+        multiplicador /= 10
+
+    return num
