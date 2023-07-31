27097aebd799bd61b626199bdd6e1c58cb0683b3
ShinMugenNoKabe <rufisercay@gmail.com>
2023-07-31 19:34:26 +0200

    Reto #16 - Python

--- Retos/Reto #16 - LA ESCALERA [Media]/python/ShinMugenNoKabe.py
+++ Retos/Reto #16 - LA ESCALERA [Media]/python/ShinMugenNoKabe.py
@@ -3,21 +3,21 @@ def draw_ladder(steps: int):
         print('__')
         return
 
-    left_to_right = steps > 0
+    left_to_right: bool = steps > 0
 
-    # Imprimir último escalón
     if left_to_right:
+        # Imprimir último escalón
         print(f"{'_' : >{(steps * 2) + 1}}")
-    else:
-        print('_')
-
-    if left_to_right:
+        
         for i in range(steps, 0, -1):
             print(f"{'_|' : >{i * 2}}")
     else:
+        # Imprimir primer escalón
+        print('_')
+
         for i in range(0, abs(steps), 1):
             print(f"{'|_' : >{((i * 2) + 3)}}")
-
+        
 
 if __name__ == "__main__":
     draw_ladder(0)
