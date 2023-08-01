ef125116abecbfd5758dcb7d1f589e118ec595c6
Akihiro93 <krystof2ms.sale24@gmail.com>
2023-08-01 12:19:32 -0500

    Reto #27 -python

---
+++ Retos/Reto #27 - CUENTA ATRÁS [Media]/python/Akihiro93.py
@@ -0,0 +1,21 @@
+import time
+
+
+def count (start:  int, delay: int):
+    while start > 0:
+        print(start)
+        time.sleep(delay)
+        start -= 1
+
+while True:
+    try:
+        number = int(input("En numero de empiezo: "))
+        delay = int(input("Tiempo de retrazo: "))
+    except ValueError as e:
+        print(f"Error: {e}")
+    else:
+        break
+
+count(number, delay)
+
+
