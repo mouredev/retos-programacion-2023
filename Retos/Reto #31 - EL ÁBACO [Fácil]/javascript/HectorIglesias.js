5881415fd4d897f31b058687c65a7f2d77555692
Hector <Hector.Iglesias.1A@gmail.com>
2023-08-01 16:14:53 +0200

    Reto #31 - Javascript

---
+++ Retos/Reto #31 - EL ÁBACO [Fácil]/javascript/HectorIglesias.js
@@ -0,0 +1,38 @@
+/*
+ * Crea una función que sea capaz de leer el número representado por el ábaco.
+ * - El ábaco se representa por un array con 7 elementos.
+ * - Cada elemento tendrá 9 "O" (aunque habitualmente tiene 10 para realizar operaciones)
+ *   para las cuentas y una secuencia de "---" para el alambre.
+ * - El primer elemento del array representa los millones, y el último las unidades.
+ * - El número en cada elemento se representa por las cuentas que están a la izquierda del alambre.
+ *
+ * Ejemplo de array y resultado:
+ * ["O---OOOOOOOO",
+ *  "OOO---OOOOOO",
+ *  "---OOOOOOOOO",
+ *  "OO---OOOOOOO",
+ *  "OOOOOOO---OO",
+ *  "OOOOOOOOO---",
+ *  "---OOOOOOOOO"]
+ *  
+ *  Resultado: 1.302.790
+ */
+
+function abaco(array){
+    let numero= ""
+    let cad_split= ""
+    for(let i=0; i<array.length; i++){
+        cad_split= array[i].split("---")
+        
+    }
+    
+    return numero
+}
+
+console.log(abaco(["O---OOOOOOOO",
+                   "OOO---OOOOOO",
+                   "---OOOOOOOOO",
+                   "OO---OOOOOOO",
+                   "OOOOOOO---OO",
+                   "OOOOOOOOO---",
+                   "---OOOOOOOOO"]))
