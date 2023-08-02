dc4abdc521590f4adbb4813ec42f89a9b80e8ebb
Josep Monclus <monclusj@aia.es>
2023-08-02 11:49:03 +0200

    Solución reto 24

--- Retos/Reto #24 - CIFRADO CÉSAR [Fácil]/python/josepmonclus.py
+++ Retos/Reto #24 - CIFRADO CÉSAR [Fácil]/python/josepmonclus.py
@@ -1,4 +1,11 @@
+'''
+ Crea un programa que realize el cifrado César de un texto y lo imprima.
+ También debe ser capaz de descifrarlo cuando así se lo indiquemos.
 
+ Te recomiendo que busques información para conocer en profundidad cómo
+ realizar el cifrado. Esto también forma parte del reto.
+'''
+ 
 def ceasar_cypher(phrase: str, shift: int, crypt: bool):
     alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 
                 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 
