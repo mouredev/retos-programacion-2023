9fdbd5a4f77329de8d818d117b3b6bc567384192
KevinED11 <mendez_kael@hotmail.com>
2023-08-02 08:31:43 -0700

    reto #31 - python

--- Retos/Reto #31 - EL ÁBACO [Fácil]/python/KevinED11.py
+++ Retos/Reto #31 - EL ÁBACO [Fácil]/python/KevinED11.py
@@ -1,48 +1,72 @@
 from typing import Protocol, Iterable
-
+from functools import cached_property
 
 class Counter(Protocol):
+    """
+    Protocol defining the interface for a counter
+
+    A Counter is an object that provides a method 'start' which returns a string.
+    Classes implementing this protocol should define the 'start' method to provide
+    a custom implementation for the counter's functionality.
+
+    Example:
+        class MyCounter:
+            def start(self) -> str:
+                # Custom implementation for the counter's start method
+                return "Counting has started!"
+
+    """
     def start(self) -> str:
         ...
 
 
-class AbacoCounter:
+class AbacusCounter:
+    """
+    An implementation of the Counter interface
+    """
+    
     def __init__(self, secuence: Iterable[str]) -> None:
         self.__secuence = secuence
     
+    def __post_init__(self) -> None:
+        if not isinstance(self.__secuence, (list, tuple, set)):
+            raise TypeError("secuence must be a list, set or a tuple of strings")
+        
+        if not all(isinstance(s, str) for s in self.__secuence):
+            raise TypeError("secuence must contain only strings")
+    
     def __format_result(self, result: str) -> str:
-        result = self.__get_result()
         return f"{float(result):,.0f}".replace(",", ".")
 
     def __choice_number(self, block_numbers: str) -> int:
-        if block_numbers[0] == "-":
-           return 0
-        
         number = 0
         for n in block_numbers:
             if n == "-":
                 break
             number += 1
-                        
-        return number
-    
-    def __get_result(self) -> str:   
-        number = ""
-        for block_numbers in self.__secuence:
-          number += str(self.__choice_number(block_numbers=block_numbers))
 
         return number
     
+    @cached_property
+    def __get_result(self) -> str:   
+        return "".join([str(self.__choice_number(block_numbers=block_numbers)) 
+                        for block_numbers in self.__secuence])
+            
     def start(self) -> str:
-        result = self.__get_result()
+        result = self.__get_result
         output_formatted = self.__format_result(result=result)
 
         return output_formatted
 
 
 def main(counter: Counter) -> None:
+    """ 
+    Entry point of the application
+    """
     result = counter.start()
+    resul2 = counter.start()
     print(result)
+    print(resul2)
 
 
 if __name__ == "__main__":
@@ -56,5 +80,6 @@ if __name__ == "__main__":
         "---OOOOOOOOO",
     ]
 
-    counter = AbacoCounter(secuence=secuence)
+    counter = AbacusCounter(secuence=secuence)
     main(counter=counter)
+    
