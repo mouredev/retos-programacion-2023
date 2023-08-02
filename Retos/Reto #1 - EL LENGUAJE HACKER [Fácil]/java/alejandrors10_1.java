7c0e0dc3b78793172f1da528b43bfcb796afa6fe
M. Alejandro Ruiz Sánchez <123484881+alejandrors10@users.noreply.github.com>
2023-08-02 17:16:26 +0200

    Reto #1 - Java

---
+++ Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/java/alejandrors10_1.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.util.Hashtable;
+public class alejandrors10_1 {
+    public static void main(String[] args) {
+
+        String texto = JOptionPane.showInputDialog("Mensaje a encriptar:").toUpperCase();
+
+        Hashtable diccionario = new Hashtable();
+        diccionario.put("A", "4");diccionario.put("B", "8");diccionario.put("C", "{");
+        diccionario.put("D", "?");diccionario.put("E", "€");diccionario.put("F", "/=");
+        diccionario.put("G", "&");diccionario.put("H", "#");diccionario.put("I", "1");
+        diccionario.put("J", "]");diccionario.put("K", "|<");diccionario.put("L", "£");
+        diccionario.put("M", "|V|");diccionario.put("N", "И");diccionario.put("Ñ", "И~");
+        diccionario.put("O", "Ø");diccionario.put("P", "9");diccionario.put("Q", "?");
+        diccionario.put("R", "Я");diccionario.put("S", "5");diccionario.put("T", "7");
+        diccionario.put("U", "µ");diccionario.put("V", "|/");diccionario.put("W", "uu");
+        diccionario.put("X", "><");diccionario.put("Y", "Ч"); diccionario.put("Z", "2");
+        diccionario.put(" ", " ");
+
+        for(int i=0; i<texto.length(); i++){
+            System.out.print(diccionario.get(texto.substring(i, i+1)));
+        }
+    }
+}
