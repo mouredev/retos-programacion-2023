91e32fd24dc4bfca9678c95473a073d87b3a25bb
Jesus Matilla León <jesusway69@gmail.com>
2023-08-03 23:05:56 +0200

    Reto#31 - java

---
+++ Retos/Reto #31 - EL ÁBACO [Fácil]/java/jesusWay69.java
@@ -0,0 +1,30 @@
+
+/**
+ *
+ * @author jesus
+ */
+public class jesusWay {
+
+    public static void main(String[] args) {
+
+        String frase = "Hola que tal";
+        //int letras = frase.charAt(0);
+
+        for (int i = 0; i < frase.length(); i++) {
+            System.out.print(" ");
+            System.out.print(frase.charAt(i) + 3);
+        
+
+        }
+        String frase2 = frase.toString();
+            System.out.println (" ");
+        for (int i = 0; i < frase2.length(); i++) {
+            System.out.print(" ");
+String j = frase2.toString();
+            System.out.print(j);
+
+        }
+    System.out.println (" ");
+    }
+
+}
