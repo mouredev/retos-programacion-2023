21b4619c20f2c4a78ae1d3a9ca3e41743d1ab1f6
Jesus Matilla León <jesusway69@gmail.com>
2023-08-03 23:09:02 +0200

    Reto#31 - java

--- Retos/Reto #31 - EL ÁBACO [Fácil]/java/jesusWay69.java
+++ Retos/Reto #31 - EL ÁBACO [Fácil]/java/jesusWay69.java
@@ -1,30 +1,57 @@
+package reto_31;
+
+import java.text.NumberFormat;
 
 /**
+ * Crea una función que sea capaz de leer el número representado por el ábaco. -
+ * El ábaco se representa por un array con 7 elementos. - Cada elemento tendrá 9
+ * "O" (aunque habitualmente tiene 10 para realizar operaciones) para las
+ * cuentas y una secuencia de "---" para el alambre. - El primer elemento del
+ * array representa los millones, y el último las unidades. - El número en cada
+ * elemento se representa por las cuentas que están a la izquierda del alambre.
+ *
+ * Ejemplo de array y resultado: ["O---OOOOOOOO", "OOO---OOOOOO",
+ * "---OOOOOOOOO", "OO---OOOOOOO", "OOOOOOO---OO", "OOOOOOOOO---",
+ * "---OOOOOOOOO"]
+ *
+ * Resultado: 1.302.790
  *
  * @author jesus
  */
-public class jesusWay {
+public class jesusWay69 {
 
     public static void main(String[] args) {
+        String[] abacus = {
+            "O---OOOOOOOO",
+            "OOO---OOOOOO",
+            "---OOOOOOOOO",
+            "OO---OOOOOOO",
+            "OOOOOOO---OO",
+            "OOOOOOOOO---",
+            "---OOOOOOOOO"};
+        translator(abacus);
+    }
 
-        String frase = "Hola que tal";
-        //int letras = frase.charAt(0);
+    private static void translator(String[] abacus) {
+        StringBuilder sum = new StringBuilder();
+        NumberFormat num = NumberFormat.getInstance();
+        for (String row : abacus) {
 
-        for (int i = 0; i < frase.length(); i++) {
-            System.out.print(" ");
-            System.out.print(frase.charAt(i) + 3);
-        
+            for (int index = 0; index < row.length(); index += 1) {
+
+                if (row.charAt(index) == '-') {
+
+                    sum.append(index);
+                    break;
+                    
+                } if (row.startsWith("---")) 
+                    sum.append(index);
+              
+            }
 
         }
-        String frase2 = frase.toString();
-            System.out.println (" ");
-        for (int i = 0; i < frase2.length(); i++) {
-            System.out.print(" ");
-String j = frase2.toString();
-            System.out.print(j);
 
-        }
-    System.out.println (" ");
+        System.out.println(num.format(Integer.valueOf(sum.toString())));
     }
 
 }
