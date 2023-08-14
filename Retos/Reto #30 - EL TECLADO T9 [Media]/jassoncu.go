4b112838fb029787d7724570d386142d4cbf9ee7
Jasson Cu <85533418+JassonCu@users.noreply.github.com>
2023-08-14 03:02:43 +0000

    Reto #30 - Go

--- Retos/Reto #30 - EL TECLADO T9 [Media]/jassoncu.go
+++ Retos/Reto #30 - EL TECLADO T9 [Media]/jassoncu.go
@@ -13,61 +13,62 @@
  *     Salida: MOUREDEV
  */
 
-package main
+ package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func T9ToLetters(input string) string {
-	t9Mapping := map[rune]string{
-		'2': "ABC",
-		'3': "DEF",
-		'4': "GHI",
-		'5': "JKL",
-		'6': "MNO",
-		'7': "PQRS",
-		'8': "TUV",
-		'9': "WXYZ",
-		'0': " ",
-	}
-
-	var result strings.Builder
-	prevKey := rune(' ')
-	keyCount := 0
-
-	for _, char := range input {
-		if char == '-' {
-			if keyCount > 0 {
-				letters := t9Mapping[prevKey]
-				result.WriteString(string(letters[keyCount-1]))
-			}
-			prevKey = ' '
-			keyCount = 0
-		} else if val, exists := t9Mapping[char]; exists {
-			if prevKey != char {
-				if keyCount > 0 {
-					letters := t9Mapping[prevKey]
-					result.WriteString(string(letters[keyCount-1]))
-				}
-				prevKey = char
-				keyCount = 0
-			}
-			keyCount = (keyCount + 1) % (len(val) + 1)
-		}
-	}
-
-	if keyCount > 0 {
-		letters := t9Mapping[prevKey]
-		result.WriteString(string(letters[keyCount-1]))
-	}
-
-	return result.String()
-}
-
-func main() {
-	input := "6-666-88-777-33-3-33-888"
-	output := T9ToLetters(input)
-	fmt.Println("Salida:", output) // Debería imprimir "Salida: MOUREDEV"
-}
+ import (
+	 "fmt"
+	 "strings"
+ )
+ 
+ func T9ToLetters(input string) string {
+	 t9Mapping := map[rune]string{
+		 '2': "ABC",
+		 '3': "DEF",
+		 '4': "GHI",
+		 '5': "JKL",
+		 '6': "MNO",
+		 '7': "PQRS",
+		 '8': "TUV",
+		 '9': "WXYZ",
+		 '0': " ",
+	 }
+ 
+	 var result strings.Builder
+	 prevKey := rune(' ')
+	 keyCount := 0
+ 
+	 for _, char := range input {
+		 if char == '-' {
+			 if keyCount > 0 {
+				 letters := t9Mapping[prevKey]
+				 result.WriteString(string(letters[keyCount-1]))
+			 }
+			 prevKey = ' '
+			 keyCount = 0
+		 } else if val, exists := t9Mapping[char]; exists {
+			 if prevKey != char {
+				 if keyCount > 0 {
+					 letters := t9Mapping[prevKey]
+					 result.WriteString(string(letters[keyCount-1]))
+				 }
+				 prevKey = char
+				 keyCount = 0
+			 }
+			 keyCount = (keyCount + 1) % (len(val) + 1)
+		 }
+	 }
+ 
+	 if keyCount > 0 {
+		 letters := t9Mapping[prevKey]
+		 result.WriteString(string(letters[keyCount-1]))
+	 }
+ 
+	 return result.String()
+ }
+ 
+ func main() {
+	 input := "5-555-22-7777-666-66"
+	 output := T9ToLetters(input)
+	 fmt.Println("Salida:", output) // Debería imprimir "Salida: JASSON"
+ }
+ 
