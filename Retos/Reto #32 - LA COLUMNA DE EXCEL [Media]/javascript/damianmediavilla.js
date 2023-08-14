e2cc9a3fadf5a8a87c5a1acef6d57453b9c4b732
Damian Mediavilla <damianmediavilla@hotmail.com>
2023-08-14 18:15:56 +0200

    refactor numberColumn

--- Retos/Reto #32 - LA COLUMNA DE EXCEL [Media]/javascript/damianmediavilla.js
+++ Retos/Reto #32 - LA COLUMNA DE EXCEL [Media]/javascript/damianmediavilla.js
@@ -1,15 +1,12 @@
-
- function numberColumn(columnName){
-    var num=0
+function numberColumn(columnName){
+    var colNum=0
     columnName=columnName.split("").reverse().join("");
     for (i=0;i<columnName.length;i++){
-        cn =parseInt(columnName[i],36)-9
-        digit=((26**i)*cn)
-        i==0?sum=cn : sum=digit;
-        num += sum
-        //console.log(`letra: ${columnName[i]} valorLetra ${cn} pos ${i} digit ${digit} sumaremos: ${sum}`)
+        charValue =parseInt(columnName[i],36)-9
+        toAdd=((26**i)*charValue)
+        colNum += toAdd
     }
-    return num
+    return colNum
 }
 
 console.log(numberColumn("A"))
