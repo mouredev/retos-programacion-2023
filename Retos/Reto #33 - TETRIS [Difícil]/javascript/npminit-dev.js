5263beb98b17e18c5efbdd4536f202d45d0d7843
npminit-dev <jorge_zetagraff@hotmail.com>
2023-08-19 19:36:00 -0300

    Eliminado de archivo HTML, pasado el codigo al archivo .js

--- Retos/Reto #33 - TETRIS [Difícil]/javascript/npminit-dev.js
+++ Retos/Reto #33 - TETRIS [Difícil]/javascript/npminit-dev.js
@@ -50,3 +50,17 @@ window.addEventListener('keydown', (e) => {
     printBoard()
   }
 })
+
+/* HTML */
+/* <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+  <script src="npminit-dev.js"></script>
+</body>
+</html> */
