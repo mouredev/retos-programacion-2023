b1493459e547115bd4be834c8cacd3b27a1def78
raulG91 <rgarciapedrosa@gmail.com>
2023-08-26 12:47:16 +0200

    Reto #34 javascript

--- Retos/Reto #34 - EL TXT [Media]/javascript/raulG91.js
+++ Retos/Reto #34 - EL TXT [Media]/javascript/raulG91.js
@@ -1,46 +1,69 @@
 const fs = require('fs');
 
-const prompt = require("prompt-sync")(); 
+const prompt = require("prompt-sync")();
 
-let text = "This is a new line";
+let text = "";
 
 let exist = false;
-let input ="";
+let input = "";
+let path = "/home/raul/Documentos/text_file.txt";
 
-
-if(fs.existsSync("C:\\Users\\RaulGarciaPedrosa\\test_file.txt")){
+if (fs.existsSync(path)) {
     exist = true
 
 }
 
-if(exist){
+if (exist) {
 
     console.log("File already exist, which action you would like to perform");
-    console.log("Enter A for append at the en of the file " + "\n " + " Enter D for deleting the file");
+    console.log("Enter A for append at the en of the file ");
+    console.log("Enter D for deleting the file");
+
     input = prompt()
     input = input.toUpperCase();
 
-    if(input == 'D'){
-
-        fs.unlink("C:\\Users\\RaulGarciaPedrosa\\test_file.txt")
+    if (input == 'D') {
+        try {
+            fs.unlink(path, (error) => {
+                if (error) {
+                    throw err;
+                }
+            })
+        } catch (err) {
+            console.log("error deleting the file");
+        }
+        finally{
+            console.log("File deleted");
+        }
     }
 }
 
-try{
-    fs.writeFile("C:\\Users\\RaulGarciaPedrosa\\test_file.txt",text,(err)=>{
 
-        if(err){
+try {
+
+    console.log("Adding data to the file press END to finish");
+
+    do{
+        input = prompt()
+
+        if(input != "END"){
+            text = text + input +"\n" ;
+        }
+    }while(input !="END")
+
+   fs.appendFile(path, text, (err) => {
+
+        if (err) {
             throw err;
-        } 
-    
-    
-    } )
+        }
 
-}catch(err){
+
+    })
+
+} catch (err) {
 
     console.log("There is an error:  " + err.text)
 
 
 }
 
-fs.readFile
