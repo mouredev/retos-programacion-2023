79c53813678e536b760d5c5311982d835027963d
elkin-dev <112489597+elkin-dev@users.noreply.github.com>
2023-09-12 16:50:53 -0500

    Reto #4 - Kotlin

---
+++ Retos/Reto #4 - PRIMO, FIBONACCI Y PAR [Media]/kotlin/ElkinDev.kt
@@ -0,0 +1,10 @@
+/*
+ * Escribe un programa que, dado un número, compruebe y muestre si es primo, fibonacci y par.
+ * Ejemplos:
+ * - Con el número 2, nos dirá: "2 es primo, fibonacci y es par"
+ * - Con el número 7, nos dirá: "7 es primo, no es fibonacci y es impar"
+ */
+
+ fun main(){
+    println("Hola mundo desde kotlin")
+ }
