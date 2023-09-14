88e6fa0f68317e3c06d00e0c722b196bb0a3f43a
Michael Da Rosa <micheldarosazxc@gmail.com>
2023-09-14 20:10:59 -0300

    testeado del funcionamiento.

--- Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/python/Michelzxc.py
+++ Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/python/Michelzxc.py
@@ -7,6 +7,8 @@
  *   (Usa la primera opción de cada transformación. Por ejemplo "4" para la "a")
 '''
 
+import unittest
+
 
 def traduce(texto: str) -> str:
     '''Traduce el argumento "texto" desde Lenguaje Natural a Lenguaje Hack.'''
@@ -22,3 +24,24 @@ def traduce(texto: str) -> str:
     texto_normalizado = texto.upper()
     texto_hack = texto_normalizado.translate(str.maketrans(MAPA_DE_CARACTERES))
     return texto_hack
+
+
+class TestLenguajeHack(unittest.TestCase):
+    def test_traduce(self):
+        self.assertEqual(traduce('leet'), '1337')
+        self.assertEqual(
+            traduce('soy un texto para probar.'),
+            '50j (_)^/ 73><70 |*4l24 |*l20l34l2.'
+        )
+        self.assertEqual(
+            traduce('esto es un unittest de python. Se esta comprobando el funcionamiento.'),
+            '3570 35 (_)^/ (_)^/177357 )3 |*j7#0^/. 53 3574 [0/\\/\\|*l20l34^/)0 31 |=(_)^/[10^/4/\\/\\13^/70.'
+        )
+        self.assertEqual(
+            traduce('3 millones, 82, 0.5 y otros.'),
+            'E /\\/\\1110^/35, BR, o.S j 07l205.'
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
