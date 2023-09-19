e6d06629ef631907d58e28da8ed212bea9015fc4
jcdm60 <jcdiezdemedina@gmail.com>
2023-09-18 22:42:19 -0400

    Reto #37 - Python

---
+++ Retos/Reto #37 - COLORES HEX Y RGB [Media]/python/jcdm60.py
@@ -0,0 +1,13 @@
+# Reto #37: Colores HEX y RGB
+#### Dificultad: Media | Publicación: 18/09/23 | Corrección: 25/09/23
+
+## Enunciado
+
+
+#
+# Crea las funciones capaces de transformar colores HEX
+# a RGB y viceversa.
+# Ejemplos:
+# RGB a HEX: r: 0, g: 0, b: 0 -> #000000
+# HEX a RGB: hex: #000000 -> (r: 0, g: 0, b: 0)
+#
