05bae38eb3e8cb084d9f2f794c92b82dcb7cf5e7
Aaron <aaronperezwindows10@gmail.com>
2023-09-19 15:50:47 -0600

    Add solution Reto #37

---
+++ Retos/Reto #37 - COLORES HEX Y RGB [Media]/javascript/Binary-Nekko.js
@@ -0,0 +1,23 @@
+const RGBToHEX = (r, g, b) => {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b)
+}
+
+const componentToHex = (str) => {
+    let hexa = str.toString(16)
+
+    return hexa.lenght == 1 ? "0" + hexa : hexa
+}
+
+const hexToRgb = (hexa) => {
+    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hexa)
+
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null
+}
+
+console.log(RGBToHEX(153, 18, 66)) // output: '#991242'
+console.log(hexToRgb("#0033ff")) // { r: 0, g: 51, b: 255 }
+
