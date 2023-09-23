9d0ed6127c307be95b804d814d2c9f97b14eceae
Victor Molero <vmolero89@uoc.edu>
2023-09-23 21:26:22 +0200

    first commit -> file created

---
+++ Retos/Reto #37 - COLORES HEX Y RGB [Media]/java/vmolero89.java
@@ -0,0 +1,20 @@
+
+
+            /*
+             * Crea las funciones capaces de transformar colores HEX
+             * a RGB y viceversa.
+             * Ejemplos:
+             * RGB a HEX: r: 0, g: 0, b: 0 -> #000000
+             * HEX a RGB: hex: #000000 -> (r: 0, g: 0, b: 0)
+             */
+
+public class Colores {
+
+    public String rgbToHex(int r, g, b){
+        String colorHex;
+
+
+        return
+    }
+
+}
