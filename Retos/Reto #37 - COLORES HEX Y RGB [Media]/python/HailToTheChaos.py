096f135a9297f06f101d26cdb30bbf086030ab9e
Jimmy <jaimedelafuente25@outlook.es>
2023-09-26 13:23:48 +0200

    Corrección reto #37

--- Retos/Reto #37 - COLORES HEX Y RGB [Media]/python/HailToTheChaos.py
+++ Retos/Reto #37 - COLORES HEX Y RGB [Media]/python/HailToTheChaos.py
@@ -6,36 +6,38 @@
  HEX a RGB: hex: #000000 -> (r: 0, g: 0, b: 0)
  '''
 
-def hex2rgb(hex:str) -> dict:
+
+def hex2rgb(hex: str) -> tuple[int, int, int]:
     '''La función `hex2rgb` toma un código de color hexadecimal como entrada y devuelve un diccionario que
     contiene los valores RGB correspondientes.
-    
+
     Parametros
     ----------
     hex : str
         El parámetro "hex" es una cadena que representa un código de color hexadecimal.
-    
+
     Returns
     -------
         La función `hex2rgb` devuelve un diccionario con las claves 'r', 'g' y 'b' que representan los
     valores rojo, verde y azul respectivamente. Si hay un ValueError que indica un valor hexadecimal no
     válido, la función devuelve Ninguno.
-    
+
     '''
     try:
         r = int(hex[1:3], 16)
         g = int(hex[3:5], 16)
         b = int(hex[5:8], 16)
 
-        return {'r':r, 'g':g, 'b':b}
-    
+        return (r, g, b)
+
     except ValueError as error:
         print(f'Error: {error}')
-        return None
+        return (0, 0, 0)
 
-def rgb2hex(r,g,b) -> str:
+
+def rgb2hex(r, g, b) -> str:
     '''La función `rgb2hex` convierte valores RGB a un código de color hexadecimal.
-    
+
     Parametros
     ----------
     r
@@ -44,15 +46,16 @@ def rgb2hex(r,g,b) -> str:
         El parámetro `g` representa el componente verde del color RGB.
     b
         El parámetro "b" representa el componente azul del color RGB.
-    
+
     Returns
     -------
         La función `rgb2hex` devuelve una cadena que representa el código de color hexadecimal en el
     formato "#RRGGBB", donde RR, GG y BB son los valores hexadecimales de los componentes rojo, verde y
     azul respectivamente.
-    
+
     '''
-    return f"#{hex(r)[2:]}{hex(g)[2:]}{hex(b)[2:]}".upper()
+    return f"#{r:02x}{g:02x}{b:02x}".upper()
+
 
 def main():
     opcion = int(input('1 - HEX to RGB, 2 - RGB to HEX: '))
@@ -70,6 +73,7 @@ def main():
 
             hex_value = rgb2hex(r, g, b)
             print(f'(r: {r}, g: {g}, {b}: 0) -> {hex_value}')
+
 
 if __name__ == "__main__":
     main()
