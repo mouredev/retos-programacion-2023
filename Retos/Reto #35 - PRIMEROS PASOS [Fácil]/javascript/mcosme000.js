3f5c414e0714d2a97709110e16ceab863b746cde
Maria Cosmen <mcosme00@gmail.com>
2023-10-01 22:41:55 +0900

    Reto #35 - JavaScript

--- Retos/Reto #35 - PRIMEROS PASOS [Fácil]/javascript/mcosme000.js
+++ Retos/Reto #35 - PRIMEROS PASOS [Fácil]/javascript/mcosme000.js
@@ -1,22 +1,3 @@
-// En un fichero, haz lo siguiente (si el lenguaje lo soporta),
-// y comenta cada bloque para identificar con qué se corresponde:
-// - Haz un "Hola, mundo!"
-// - Crea variables de tipo String, numéricas (enteras y decimales)
-//   y Booleanas (o cualquier tipo de dato primitivo).
-// - Crea una constante.
-// - Usa un if, else if y else.
-// - Crea estructuras como un array, lista, tupla, set y diccionario.
-// - Usa un for, foreach y un while.
-// - Crea diferentes funciones (con/sin parámetros y con/sin retorno).
-// - Crea una clase.
-// - Muestra el control de excepciones.
-//
-// Así, cualquier persona podrá consultar rápidamente diferentes ejemplos
-// de sintaxis básica de muchos lenguajes.
-//
-// ¡Muchas gracias!
-// /
-
 console.log("Hola, mundo!");
 
 let str = "María";
