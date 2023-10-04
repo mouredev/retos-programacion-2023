ce1c03c7443732f8e3fac5696c3a345bf4fff78d
jlcontini <126628342+jlcontini@users.noreply.github.com>
2023-10-04 20:29:14 +0000

    Correccion string linea 4 "FizzBuzz"

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/python/k3nvd.py
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/python/k3nvd.py
@@ -1,7 +1,7 @@
 n= int(input("Type a number: "))
 for x in range(1,n):
     if x% 3==0 and x%5==0:
-        print(FizzBuzz)
+        print("FizzBuzz")
     elif x% 3==0:
         print("Fizz")
     elif x % 5==0:
