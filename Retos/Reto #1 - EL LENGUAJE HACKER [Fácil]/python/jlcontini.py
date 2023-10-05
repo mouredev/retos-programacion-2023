462d268d839df0552c1d854448e285dad9c568e3
jlcontini <jlcontini80@gmail.com>
2023-10-04 23:02:10 -0300

    Reto #01 - primer commit

---
+++ Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/python/jlcontini.py
@@ -0,0 +1,39 @@
+"""
+# Reto #1: EL "LENGUAJE HACKER"
+#### Dificultad: Fácil | Publicación: 02/01/23 | Corrección: 09/01/23
+
+## Enunciado
+
+```
+/*
+ * Escribe un programa que reciba un texto y transforme lenguaje natural a
+ * "lenguaje hacker" (conocido realmente como "leet" o "1337"). Este lenguaje
+ *  se caracteriza por sustituir caracteres alfanuméricos.
+ * - Utiliza esta tabla (https://www.gamehouse.com/blog/leet-speak-cheat-sheet/) 
+ *   con el alfabeto y los números en "leet".
+ *   (Usa la primera opción de cada transformación. Por ejemplo "4" para la "a")
+ */
+```
+#### Tienes toda la información extendida sobre los retos de programación semanales en **[retosdeprogramacion.com/semanales2023](https://retosdeprogramacion.com/semanales2023)**.
+
+Sigue las **[instrucciones](../../README.md)**, consulta las correcciones y aporta la tuya propia utilizando el lenguaje de programación que quieras.
+
+> Recuerda que cada semana se publica un nuevo ejercicio y se corrige el de la semana anterior en directo desde **[Twitch](https://twitch.tv/mouredev)**. Tienes el horario en la sección "eventos" del servidor de **[Discord](https://discord.gg/mouredev)**.
+"""
+
+
+def get_string() -> str:
+  original_string = str(input("Ingresa el texto que quieras transformar a 'lenguaje hacker': "))
+  return original_string
+
+
+def transform_string_to_leet(original_string):
+  
+
+original_string = get_string()
+leet_string = transform_string_to_leet(original_string)
+print(leet_string)
+
+
+leet_dictionary: list = {"A": "4", "B": "I3", "C": "[", "D": ")", "E": "3", "F": "|=", "G": "&", "H": "#", "I": "", "J": "", "K": "", "L": "", "M": "", "N": "", "O": "", "P": "", "Q": "", "R": "", "S": "", "T": "", "U": "", "V": "", "W": "", "X": "", "Y": "", "Z": ""}
+
