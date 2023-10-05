ec3b16b4d53195c41463c450e842565245eb9287
jlcontini <jlcontini80@gmail.com>
2023-10-05 16:53:29 -0300

    Reto #1 - file jlcontini.py

--- Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/python/jlcontini.py
+++ Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/python/jlcontini.py
@@ -22,18 +22,33 @@ Sigue las **[instrucciones](../../README.md)**, consulta las correcciones y apor
 """
 
 
+leet_dict: dict = {"A": "4", "B": "I3", "C": "[", "D": ")", "E": "3", "F": "|=", "G": "&", "H": "#", "I": "1", "J": ",_|", "K": ">|", "L": "1", "M": "/\/\\", "N": "^/", "O": "0", "P": "|*", "Q": "(_,)", "R": "I2", "S": "5", "T": "7", "U": "(_)", "V": "\/", "W": "\/\/", "X": "><", "Y": "j", "Z": "2"}
+
+
+def change_to_leet(original_string: str) -> str:
+  leet_string = ""
+  upper_case_string = original_string.upper()
+  
+  for character in upper_case_string:
+    if character in leet_dict:
+      leet_string += leet_dict[character]
+    else:
+      leet_string += character
+  
+  return leet_string
+
+
 def get_string() -> str:
   original_string = str(input("Ingresa el texto que quieras transformar a 'lenguaje hacker': "))
   return original_string
 
 
-def transform_string_to_leet(original_string):
-  
-
-original_string = get_string()
-leet_string = transform_string_to_leet(original_string)
-print(leet_string)
+def main():
+  original_string = get_string()
+  leet_string = change_to_leet(original_string)
+  print(leet_string)
 
 
-leet_dictionary: list = {"A": "4", "B": "I3", "C": "[", "D": ")", "E": "3", "F": "|=", "G": "&", "H": "#", "I": "1", "J": ",_|", "K": ">|", "L": "1", "M": "/\/\\", "N": "^/", "O": "0", "P": "|*", "Q": "(_,)", "R": "I2", "S": "5", "T": "7", "U": "(_)", "V": "\/", "W": "\/\/", "X": "><", "Y": "j", "Z": "2"}
 
+if __name__ == "__main__":
+  main()
