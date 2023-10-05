60f409d9c3df76a8f1021d137a239fb1e0e71d49
jlcontini <jlcontini80@gmail.com>
2023-10-05 17:35:12 -0300

    Reto #01 - file jlcontini.py - modificacion

--- Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/python/jlcontini.py
+++ Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/python/jlcontini.py
@@ -1,31 +1,12 @@
-"""
-# Reto #1: EL "LENGUAJE HACKER"
-#### Dificultad: Fácil | Publicación: 02/01/23 | Corrección: 09/01/23
-
-## Enunciado
-
-```
-/*
- * Escribe un programa que reciba un texto y transforme lenguaje natural a
- * "lenguaje hacker" (conocido realmente como "leet" o "1337"). Este lenguaje
- *  se caracteriza por sustituir caracteres alfanuméricos.
- * - Utiliza esta tabla (https://www.gamehouse.com/blog/leet-speak-cheat-sheet/) 
- *   con el alfabeto y los números en "leet".
- *   (Usa la primera opción de cada transformación. Por ejemplo "4" para la "a")
- */
-```
-#### Tienes toda la información extendida sobre los retos de programación semanales en **[retosdeprogramacion.com/semanales2023](https://retosdeprogramacion.com/semanales2023)**.
-
-Sigue las **[instrucciones](../../README.md)**, consulta las correcciones y aporta la tuya propia utilizando el lenguaje de programación que quieras.
-
-> Recuerda que cada semana se publica un nuevo ejercicio y se corrige el de la semana anterior en directo desde **[Twitch](https://twitch.tv/mouredev)**. Tienes el horario en la sección "eventos" del servidor de **[Discord](https://discord.gg/mouredev)**.
-"""
-
-
-leet_dict: dict = {"A": "4", "B": "I3", "C": "[", "D": ")", "E": "3", "F": "|=", "G": "&", "H": "#", "I": "1", "J": ",_|", "K": ">|", "L": "1", "M": "/\/\\", "N": "^/", "O": "0", "P": "|*", "Q": "(_,)", "R": "I2", "S": "5", "T": "7", "U": "(_)", "V": "\/", "W": "\/\/", "X": "><", "Y": "j", "Z": "2"}
-
+# Reto #1 - Leet translator
 
 def change_to_leet(original_string: str) -> str:
+  
+  leet_dict: dict = {"A": "4", "B": "I3", "C": "[", "D": ")", "E": "3", "F": "|=", "G": "&", "H": "#",
+                     "I": "1", "J": ",_|", "K": ">|", "L": "1", "M": "/\/\\", "N": "^/", "O": "0",
+                     "P": "|*", "Q": "(_,)", "R": "I2", "S": "5", "T": "7", "U": "(_)", "V": "\/",
+                     "W": "\/\/", "X": "><", "Y": "j", "Z": "2"}
+  
   leet_string = ""
   upper_case_string = original_string.upper()
   
@@ -39,14 +20,30 @@ def change_to_leet(original_string: str) -> str:
 
 
 def get_string() -> str:
-  original_string = str(input("Ingresa el texto que quieras transformar a 'lenguaje hacker': "))
+  original_string = str(input("Ingresa el texto que quieras transformar a 'lenguaje hacker': \n"))
   return original_string
 
 
+def show_results(original_string, leet_string):
+  print(f"""
+        Texto original ingresado:
+        {original_string}
+        
+        Texto en formato leet:
+        {leet_string}
+        
+        """)
+
+
 def main():
+  print("\n\nLEET: esto es una prueba")
+  print(change_to_leet("LEET: esto es una prueba\n\n"))
+  
   original_string = get_string()
   leet_string = change_to_leet(original_string)
-  print(leet_string)
+  show_results(original_string, leet_string)
+  
+  print(f'\nResultado de la transformacion: \n{leet_string}\n')
 
 
 
