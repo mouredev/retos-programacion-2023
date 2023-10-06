33aa94e0d725b0fd4593774aa02398fec8dc89db
camilo180601 <camilo06180401@gmail.com>
2023-10-06 15:58:17 -0500

    Reto mouredev#38 - javascript

---
+++ Retos/Reto #38 - LAS SUMAS [Media]/javascript/camilo06180401.js
@@ -0,0 +1,53 @@
+const range = (start, end, step = 1) => {
+    if (arguments.length === 1) {
+        end = start;
+        start = 0;
+    }
+
+    const res = [];
+
+    if (step > 0) {
+        for (let i = start; i < end; i += step) {
+            res.push(i);
+        }
+    } else if (step < 0) {
+        for (let i = start; i > end; i += step) {
+            res.push(i);
+        }
+    }
+
+    return res;
+}
+
+const find_sums = (numeros, objetivo) => {
+
+    const find_sum = (start, objetivo, combination) => {
+
+        if (objetivo == 0) {
+            result.push([...combination])
+            return
+        }
+
+        if (objetivo < 0 || start == numeros.length) {
+            return
+        }
+
+        for (let key = start; key < numeros.length; key++) {
+
+            if (numeros[key] == numeros[key - 1]) {
+                continue
+            }
+
+            combination.push(numeros[key])
+            find_sum(key + 1, objetivo - numeros[key], combination)
+            combination.pop()
+        }
+    }
+
+    numeros.sort((a, b) => a - b)
+    result = []
+    find_sum(0, objetivo, [])
+    console.log(result)
+}
+
+find_sums([2,1,5,3,4], 3)
