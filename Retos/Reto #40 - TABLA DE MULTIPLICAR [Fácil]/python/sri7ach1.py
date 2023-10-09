ac1c0ec76fc5e2d00ef91c41046617b2c3389766
Aridane Mirabal <mirabal16@gmail.com>
2023-10-09 21:28:23 +0100

    Reto #40

---
+++ Retos/Reto #40 - TABLA DE MULTIPLICAR [Fácil]/python/sri7ach1.py
@@ -0,0 +1,15 @@
+'''
+ * Crea un programa que sea capaz de solicitarte un número y se
+ * encargue de imprimir su tabla de multiplicar entre el 1 y el 10.
+ * - Debe visualizarse qué operación se realiza y su resultado.
+ *   Ej: 1 x 1 = 1
+ *       1 x 2 = 2
+ *       1 x 3 = 3
+ *       ... 
+'''
+
+def tabla(n):
+    for i in range(1,11):
+        print(f"{n} x {i} = {n * i}")
+
+tabla(9)
