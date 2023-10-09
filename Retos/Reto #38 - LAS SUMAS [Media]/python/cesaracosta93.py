26b85d0fbc861e14392ec45bbd7d9102c169adec
Cesar <cesaracostabazan@gmail.com>
2023-10-09 16:47:31 -0600

    Muy buen reto me ayudo a comprender mejor el comportamiento de las funciones recursivas

--- Retos/Reto #38 - LAS SUMAS [Media]/python/cesaracosta93.py
+++ Retos/Reto #38 - LAS SUMAS [Media]/python/cesaracosta93.py
@@ -5,16 +5,13 @@ def find_sum(lista, target):
     suma=0
     lista.sort()
     for i, v in enumerate(lista):
-        listaCopy=lista.copy()
-        listaCopy.pop(i)#A la copia de la lisa le quita el elemento que esta verificando
         suma=sum(lista)#Suma toda la lista
-
-        combinaciones.append(lista) if suma==target else None  #si la suma de la lista es igual al target la agrega a las combinaciones
-        
-        encontrado=find_sum(listaCopy,target)#Al volver a llamar la funcion sumara la lista completa sin el elemento que esta verificando
-        
-        #Aqui se detiene la funcion hasta que termine la recursion
-
+        combinaciones.append(lista) if suma==target else None  #si la suma de la lista es igual al target la agrega a las combinaciones        
+#----------------------------------------------------------------------------------------------------------------------------------
+        listaCopy=lista.copy()
+        listaCopy.pop(i)#A la copia de la lista le quitamos el elemento para ir reduciendo la lista y seguir con otra combinacion
+        encontrado=find_sum(listaCopy,target)#Al volver a llamar la funcion sumara la lista estara reducida para comprobar otra combinacion
+#Aqui se detiene la funcion hasta que termine la recursion--------------------------------------------------------------------------------------
         if encontrado != []:#si el resultado no esta vacio
             for elemento in encontrado:#Por cada elemento encontrado y
                 if elemento not in combinaciones:#si no esta en la combinaciones
