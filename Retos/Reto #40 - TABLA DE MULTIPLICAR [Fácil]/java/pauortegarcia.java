233a7bff9ee9f6d95042891a60dbfb6aeac63be6
Pau Ortega Garcia <paucursosformacion@gmail.com>
2023-10-11 21:29:43 +0200

    Actualización de optimización de variables -  pauortegarcia.java

--- Retos/Reto #40 - TABLA DE MULTIPLICAR [Fácil]/java/pauortegarcia.java
+++ Retos/Reto #40 - TABLA DE MULTIPLICAR [Fácil]/java/pauortegarcia.java
@@ -3,13 +3,9 @@ public class Program
 {
     public static void main(String[] args) {
         Scanner numero = new Scanner(System.in);
-    int num = numero.nextInt();
-    int total = 0;
-    String text= "";
-        for(int i =1; i<=10; i++){
-            total = num * i; 
-        text =  num+" x "+i + " = "+total;
-        System.out.println(text);    
+    int num = numero.nextInt();    
+        for(int i =1; i<=10; i++){        
+        System.out.println(num+" x "+ i+" = "+num * i);
         }
 	}
 }
