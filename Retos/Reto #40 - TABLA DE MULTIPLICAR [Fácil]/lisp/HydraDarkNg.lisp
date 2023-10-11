e6e972874f8cebafbb329e24c1110f526ad6adb3
HydraDarkNg <HydraDarkNg@localhost.localdomain>
2023-10-10 21:56:49 -0600

    Reto #40 - Common lisp

---
+++ Retos/Reto #40 - TABLA DE MULTIPLICAR [Fácil]/lisp/HydraDarkNg.lisp
@@ -0,0 +1,16 @@
+;;; 
+;;; Crea un programa que sea capaz de solicitarte un número y se
+;;; encargue de imprimir su tabla de multiplicar entre el 1 y el 10.
+;;; - Debe visualizarse qué operación se realiza y su resultado.
+;;;   Ej: 1 x 1 = 1
+;;;       1 x 2 = 2
+;;;       1 x 3 = 3
+;;;       ... 
+;;;
+
+(defun main ()
+  (let ((number (parse-integer (read-line))))
+    (loop for i from 1 to 10 do
+      (format t "~a x ~2,' d = ~a~%" number i (* number i)))))
+
+(main)
