33255632715d1279c1ea82176d6a8cd316d555f6
Mathias Hidalgo Caja <mejhc2307lbc@gmail.com>
2023-10-11 15:29:52 -0400

    Reto #40 - Javascript

--- Retos/Reto #40 - TABLA DE MULTIPLICAR [Fácil]/javascript/mathbucks.js
+++ Retos/Reto #40 - TABLA DE MULTIPLICAR [Fácil]/javascript/mathbucks.js
@@ -1,8 +1,11 @@
+// Creo una funcion para tomar tanto el numero que quieres saber la tabla (n), como la cantidad de numberos los cuales se multiplica (t)
 function XTable(n, t){
+  // se utiliza un loop para poder interactuar con la cantidad de la tabla 
   for(let i=1; i<= t; i++){
-    const res = i * n;
-    console.log(`${n} x ${i} = ${res}`);
+    const res = i * n; // se almacena el resultado como respuesta para terminar la fila de la multiplicacion
+    console.log(`${n} x ${i} = ${res}`); // se printea cada resultado como se requiere en el problema
   }
 }
 
 XTable(5, 10)
+XTable(2, 10)
