5badeda360125e99a3c0140e71035e6ad0088b0d
FabianTorres <programacioncursos2022@gmail.com>
2023-10-14 15:51:02 -0500

    New Solution: Reto #5 - Java

---
+++ Retos/Reto #5 - HOLA MUNDO [Fácil]/java/nftorres.java
@@ -0,0 +1,8 @@
+/*
+This script shows the solution to the exercise "Reto #5: HOLA MUNDO" from Brais Moure's official website.
+*/
+public class nftorres {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+    }
+}
