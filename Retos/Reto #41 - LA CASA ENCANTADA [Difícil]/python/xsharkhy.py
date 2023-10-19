0f78d30548865fa01e411c3804d7f2a7abf5e615
xSharkhy <sharkhyacc@gmail.com>
2023-10-20 00:29:24 +0200

    chore: add types

--- Retos/Reto #41 - LA CASA ENCANTADA [Difícil]/python/xsharkhy.py
+++ Retos/Reto #41 - LA CASA ENCANTADA [Difícil]/python/xsharkhy.py
@@ -1,8 +1,9 @@
 from random import randint, choice
+from typing import List, Dict, Tuple
 
 
-def init():
-    maps = {
+def init() -> None:
+    maps: Dict[int, List[List[str]]] = {
         1: [
             ["🚪", "⬜️", "⬜️", "⬜️"],
             ["⬜️", "👻", "⬜️", "⬜️"],
@@ -73,13 +74,13 @@ def init():
         + "La partida acabará cuando llegues a la sala de los dulces.\n"
     )
 
-    game_map = maps[randint(1, len(maps))]
+    game_map: List[List[str]] = maps[randint(1, len(maps))]
     main(game_map)
 
 
-def main(game_map: list[list]):
-    def print_map(player_position):
-        display_map = [
+def main(game_map: List[List[str]]) -> None:
+    def print_map(player_position: Dict[str, int]) -> None:
+        display_map: List[List[str]] = [
             ["🚪" if game_map[i][j] == "🚪" else "⬜️" for j in range(map_size)]
             for i in range(map_size)
         ]
@@ -98,7 +99,7 @@ def main(game_map: list[list]):
             print("".join(map(str, row)))
         print("")
 
-    def ask_question():
+    def ask_question() -> None:
         question, choices = choice(list(QUESTIONS.items()))
 
         print(f"ENIGMA! {question}")
@@ -121,31 +122,31 @@ def main(game_map: list[list]):
                 ask_question()
 
     def find_item_in_matrix(
-        matrix: list[list[str]], target_item: str
-    ) -> tuple[int, int]:
+        matrix: List[List[str]], target_item: str
+    ) -> Tuple[int, int]:
         for i, row in enumerate(matrix):
             for j, item in enumerate(row):
                 if item == target_item:
                     return i, j
 
-    def get_valid_player_moves(player_position: dict) -> list:
-        movement_options = {
+    def get_valid_player_moves(player_position: Dict[str, int]) -> List[str]:
+        movement_options: Dict[str, Tuple[int, int]] = {
             "W": (-1, 0),
             "S": (1, 0),
             "A": (0, -1),
             "D": (0, 1),
         }
 
-        valid_moves = []
+        valid_moves: List[str] = []
         for move, vector in movement_options.items():
-            new_position = {
+            new_position: Dict[str, int] = {
                 "x": player_position["x"] + vector[0],
                 "y": player_position["y"] + vector[1],
             }
             if 0 <= new_position["x"] < map_size and 0 <= new_position["y"] < map_size:
                 valid_moves.append(move)
 
-        chosen_move = ""
+        chosen_move: str = ""
         while chosen_move not in valid_moves:
             chosen_move = input(
                 "Hacia dónde quieres moverte? (Usa las teclas WASD): "
@@ -153,23 +154,27 @@ def main(game_map: list[list]):
             if chosen_move not in valid_moves:
                 print("Movimiento inválido. Por favor, elige una dirección válida.")
 
-        movement_vector = movement_options[chosen_move]
+        movement_vector: Tuple[int, int] = movement_options[chosen_move]
         return movement_vector
 
-    def update_player_position(movement_vector, player_position, seen_map):
+    def update_player_position(
+        movement_vector: Tuple[int, int],
+        player_position: Dict[str, int],
+        seen_map: List[List[int]],
+    ) -> None:
         player_position["x"] += movement_vector[0]
         player_position["y"] += movement_vector[1]
 
         seen_map[player_position["x"]][player_position["y"]] = 1
 
-    def check_win(pos: dict, map: list) -> bool:
-        win_coords = find_item_in_matrix(map, "🍭")
-        player_coords = list(pos.values())
+    def check_win(pos: Dict[str, int], map: List[List[str]]) -> bool:
+        win_coords: Tuple[int, int] = find_item_in_matrix(map, "🍭")
+        player_coords: List[int] = list(pos.values())
 
         if win_coords == player_coords:
             return True
 
-    QUESTIONS = {
+    QUESTIONS: Dict[str, Dict[str, int]] = {
         "What is the capital of France?": {
             "Paris": 1,
             "London": 0,
@@ -197,27 +202,24 @@ def main(game_map: list[list]):
         },
     }
 
-    map_size = len(game_map)
-    player_position = None
-    seen_map = [[0] * map_size for _ in range(map_size)]
+    map_size: int = len(game_map)
+    player_position: Dict[str, int] = {}
+    seen_map: List[List[int]] = [[0] * map_size for _ in range(map_size)]
 
     print_map(player_position)
 
-    confirm = input("¿Quieres entrar? (s/n): ").lower() == "s"
+    confirm: bool = input("¿Quieres entrar? (s/n): ").lower() == "s"
     if confirm:
-        door_position = find_item_in_matrix(game_map, "🚪")
+        door_position: Tuple[int, int] = find_item_in_matrix(game_map, "🚪")
         player_position = {"x": door_position[0], "y": door_position[1]}
         seen_map[door_position[0]][door_position[1]] = 1
 
-        win_flag = False
+        win_flag: bool = False
 
         while not win_flag:
             print_map(player_position)
-            if game_map[player_position["x"]][player_position["y"]] == "👻":
-                ask_question()
-            else:
-                ask_question()
-            move = get_valid_player_moves(player_position)
+            ask_question()
+            move: Tuple[int, int] = get_valid_player_moves(player_position)
             update_player_position(move, player_position, seen_map)
             win_flag = check_win(player_position, game_map)
 
