703149680227d61d883b3d4035d1d8dd657a1a0b
Gabriel Flores <eldalai@gmail.com>
2023-10-22 19:43:29 -0300

    House init

---
+++ Retos/Reto #41 - LA CASA ENCANTADA [Difícil]/python/eldalai.py
@@ -0,0 +1,21 @@
+import unittest
+
+class House:
+    def __init__(self):
+        self.cells = [
+            [
+                None for _ in range(4)
+            ]
+            for _ in range(4)
+        ]
+
+class TestHouse(unittest.TestCase):
+
+    def test_init(self):
+        house = House()
+        self.assertEqual(len(house.cells), 4)
+        for room_index in range(4):
+            self.assertEqual(len(house.cells[room_index]), 4)
+
+if __name__ == '__main__':
+    unittest.main()
