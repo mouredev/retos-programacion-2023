1773f8171a6b2805ff31a77c8bfa1f32a862093e
Gabriel Flores <eldalai@gmail.com>
2023-10-22 20:18:24 -0300

    Door pos in House

--- Retos/Reto #41 - LA CASA ENCANTADA [Difícil]/python/eldalai.py
+++ Retos/Reto #41 - LA CASA ENCANTADA [Difícil]/python/eldalai.py
@@ -1,4 +1,7 @@
 import unittest
+from unittest.mock import patch
+import random
+
 
 class Cell:
     ...
@@ -10,12 +13,20 @@ class Door(Cell):
 
 class House:
     def __init__(self):
+        door_pos_row = random.randint(0, 3)
+        door_pos_col = random.randint(0, 3)
         self.cells = [
             [
-                None for _ in range(4)
+                (
+                    Door()
+                    if row_index == door_pos_row and col_index == door_pos_col
+                    else None
+                )
+                for col_index in range(4)
             ]
-            for _ in range(4)
+            for row_index in range(4)
         ]
+
 
 class TestDoor(unittest.TestCase):
     def test_init(self):
@@ -36,6 +47,15 @@ class TestHouse(unittest.TestCase):
         self.assertEqual(len(house.cells), 4)
         for room_index in range(4):
             self.assertEqual(len(house.cells[room_index]), 4)
+
+    @patch('random.randint', side_effect=[2, 3])
+    def test_door_pos(self, randint_patched):
+        house = House()
+        self.assertIsInstance(
+            house.cells[2][3],
+            Door,
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
