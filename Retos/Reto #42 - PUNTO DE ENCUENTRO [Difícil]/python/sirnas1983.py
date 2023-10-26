c7fc10db20edeb4fb02beb43d5e43098cf6b0348
DHiebl <108873454+sirnas1983@users.noreply.github.com>
2023-10-26 10:44:51 -0300

    Correccion reto #42 - python
    Se modifico nombre de funcion
    Se corrigio casos de trayectorias coplanares y colineales ya que algunas pruebas unitarias no pasaron los tests
    Se sacaron algunas redundacias

--- Retos/Reto #42 - PUNTO DE ENCUENTRO [Difícil]/python/sirnas1983.py
+++ Retos/Reto #42 - PUNTO DE ENCUENTRO [Difícil]/python/sirnas1983.py
@@ -1,135 +1,113 @@
 from matplotlib import pyplot as plt
 
-
-def interseccion(coor1 : tuple, coor2 : tuple, vel1 : tuple, vel2 : tuple, plot_graph : bool = False):
+def detectar_colision(coor1, coor2, vel1, vel2, plot_graph=True):
     '''
-    Funcion que imprime por pantalla si existe o no colision de 2 objetos en el plano
-    y retorna True en caso de existir y False en caso que no
-    
-    Deben ingresarse las coordenadas de partida de ambos objetos como tuplas y sus
-    velocidades en x e y tambien como tuplas. Para que imprima los graficos pase True como 5to argumento.
+    Determina si dos objetos colisionarán en el plano y muestra un gráfico si se especifica.
+
+    Parámetros:
+    coor1 (tuple): Coordenadas iniciales del objeto 1 (x1, y1).
+    coor2 (tuple): Coordenadas iniciales del objeto 2 (x2, y2).
+    vel1 (tuple): Velocidades del objeto 1 en (vx1, vy1).
+    vel2 (tuple): Velocidades del objeto 2 en (vx2, vy2).
+    plot_graph (bool, opcional): True si se debe mostrar un gráfico, False por defecto.
+
+    Retorna:
+    bool: True si los objetos colisionan, False si no.
     '''
     colision = False
     try:
         # Asigno el valor de las tuplas a variables individuales
-        vx1 = vel1[0]
-        vy1 = vel1[1]
-        vx2 = vel2[0]
-        vy2 = vel2[1]    
-        
-        x0 = coor1[0]
-        y0 = coor1[1]
-        x1 = coor2[0]
-        y1 = coor2[1]
-        
-        colision = False
-        
-        # Verifico que ambos objetos no esten en reposo
-        if (vx1 == vy1 == 0) and (vx2 == vy2 == 0):
-            print("Ambos objetos estan en reposo")
-        # Verifico la condicion de que no parten del mismo punto
-        elif (x0 == x1) and (y0 == y1):
-            print("Ambos objetos estan colisionados antes de iniciar su movimiento")
+        vx1, vy1 = vel1
+        vx2, vy2 = vel2
+
+        x1, y1 = coor1
+        x2, y2 = coor2
+
+        # Verifico la condición de que no parten del mismo punto
+        if (x1 == x2) and (y1 == y2):
+            print("Ambos objetos están colisionados antes de iniciar su movimiento.")
+            x = x1
+            y = y1
+            alpha = 0
             colision = True
         else:
             # Verifico que ambas trayectorias no sean coplanares
-            if (vx1*vy2 - vy1*vx2) != 0:
-                
+            if (vx1 * vy2 - vy1 * vx2) != 0:
                 # Calculo alpha y beta de sus ecuaciones vectoriales
-                beta = (vx1*(y0 - y1) + vy1*(x1 - x0)) / (vx1*vy2 - vy1*vx2)
+                beta = (vx1 * (y1 - y2) + vy1 * (x2 - x1)) / (vx1 * vy2 - vy1 * vx2)
                 if vy1 != 0:
-                    alpha = (y1 - y0 + beta * vy2) / vy1
+                    alpha = (y2 - y1 + beta * vy2) / vy1
                 elif vx1 != 0:
-                    alpha = (x1 - x0 + beta * vx2) / vx1
-                
-                x = x1 + beta * vx2
-                y = y1 + beta * vy2
+                    alpha = (x2 - x1 + beta * vx2) / vx1
 
-                # Si alpha y beta son iguales (tiempo) hay colision, sino solamente interseccion de trayectorias
+                x = x2 + beta * vx2
+                y = y2 + beta * vy2
+
+                # Si alpha y beta son iguales (tiempo) hay colisión, sino solamente intersección de trayectorias
                 if alpha == beta:
-                    print("Las objetos colisionan en", (x,y).__str__(),"transcurridos", alpha, "segundos")
+                    print(f"Los objetos colisionan en ({x:.2f}, {y:.2f}) transcurridos {alpha:.2f} segundos.")
                     colision = True
                 else:
-                    print(f"Si bien las trayectorias se intersectan en {(x,y).__str__()} no hay colision entre objetos.")
+                    print(f"Si bien las trayectorias se intersectan en ({x:.2f}, {y:.2f}), no hay colisión entre objetos.")
             else:
-                # Hago el analisis puntual de trayectorias coplanares
-                alpha1 = None
-                alpha2 = None
+                # Hago el análisis puntual de trayectorias coplanares
+                alpha = None
+                beta = None
                 # Para ello calculo la constante del vector
                 if vx1 - vx2 != 0:
-                    alpha1 = (x1 - x0) / (vx1 - vx2)
-                elif vy1 - vy2 != 0:
-                    alpha2 = (y1 - y0) / (vy1 - vy2)
-                else:
-                    print("Los objetos van en la misma direccion pero uno igual o mas lento que el otro")
-                    alpha2 = 1
-                    alpha1 = alpha2 + 1
-                # La constante del vector debe ser igual para ambos y mayor a 0 
-                if alpha1 == alpha2 > 0:
-                    x = x0 + alpha1 * vx1
-                    y = y0 + alpha1 * vy1
-                    print("Los objetos colisionan en", (x,y).__str__(),"transcurridos", alpha1, "segundos")
+                    alpha = (x2 - x1) / (vx1 - vx2)
+                if vy1 - vy2 != 0:
+                    beta = (y2 - y1) / (vy1 - vy2)
+                # La constante del vector debe ser igual para ambos y mayor a 0
+                if alpha is None and beta is None:
+                    print("Los objetos van a la misma velocidad, no habrá colisión.")
+                elif alpha == beta:
+                    if alpha > 0:
+                        x = x1 + alpha * vx1
+                        y = y1 + alpha * vy1
+                        print(f"Los objetos colisionan en ({x:.2f}, {y:.2f}) transcurridos {alpha:.2f} segundos.")
+                        colision = True
+                    else:
+                        print("Los objetos son colineales pero van separándose, no habrá colisión.")
+                elif (alpha is None and beta > 0) or (beta is None and alpha > 0):
+                    x = x1 + alpha * vx1
+                    y = y1 + alpha * vy1
+                    print(f"Los objetos colisionan en ({x:.2f}, {y:.2f}) transcurridos {alpha:.2f} segundos.")
                     colision = True
-                elif alpha1 < 0:
-                    print("Los objetos son colineales pero van en direcciones opuetas")
+                elif alpha != beta:
+                    print("Los objetos no colisionarán.")
                 elif (vx1 == vy1 == 0) or (vx2 == vy2 == 0):
-                    print("El objeto en movimento no intersecta el objeto en reposo")
-        # Si existe colision pregunto si quieren ver el grafico
+                    print("El objeto en movimiento no intersecta el objeto en reposo.")
+
         if colision and plot_graph:
             plt.axhline(0, color='black', linewidth=0.5)
             plt.axvline(0, color='black', linewidth=0.5)
-            plt.scatter(x, y, color='red', linewidths=10, label='Punto de encuentro')
-            plt.scatter(x0, y0, color='blue', linewidths=5, label='Salida objeto 1')
-            plt.scatter(x1, y1, color='green', linewidths=5, label='Salida objeto 2')
-            plt.plot([x0, x], [y0, y], color='blue', ls="-." )
-            plt.plot([x1, x], [y1, y], color='green', ls="-.")
+            plt.scatter(x, y, color='red', linewidths=10, label=f'COLISIÓN\nx: {x:.2f} - y: {y:.2f} - t: {alpha:.2f}')
+            plt.scatter(x1, y1, color='blue', linewidths=5, label=f'Salida objeto 1\nVx: {vx1} Vy: {vy1}')
+            plt.scatter(x2, y2, color='green', linewidths=5, label=f'Salida objeto 2\nVx: {vx2} Vy: {vy2}')
+            plt.plot([x1, x], [y1, y], color='blue', ls="-.")
+            plt.plot([x2, x], [y2, y], color='green', ls="-.")
             plt.legend()
             plt.xlabel('Eje X')
             plt.ylabel('Eje Y')
-            plt.title('Punto de Colision')
+            plt.title('Punto de Colisión')
             plt.grid(True)
             plt.show()
-        return colision  
-    except:
-        print("Debe ingresar valores de coordenadas y velocidades como tuplas")     
+
         return colision
 
-##### PRUEBAS ######
-  
-coor1 = (0,0)
-v1 = (-4,-4)       
-coor2 = (2,2)    
-v2 = (-6,-6)
-interseccion(coor1, coor2, v1, v2) 
+    except Exception as e:
+        print(f"Error: {e}")
+        return colision
 
-coor1 = (0,0)
-v1 = (0,0)       
-coor2 = (3,2)    
-v2 = (-6,-6)
-interseccion(coor1, coor2, v1, v2)
-    
-coor1 = (1,1)
-v1 = (-4,-4)       
-coor2 = (2,2)    
-v2 = (-6,-6)
-interseccion(coor1, coor2, v1, v2)   
-    
-coor1 = (10,8)
-v1 = (7,.5)       
-coor2 = (4,0)    
-v2 = (-0.5,0.5)
-interseccion(coor1, coor2, v1, v2)     
-    
-coor1 = (0,0)
-v1 = (4,4)       
-coor2 = (4,0)    
-v2 = (0,4)
-interseccion(coor1, coor2, v1, v2)  
-
-coor1 = (0,0)
-v1 = (4,4)       
-coor2 = (2,2)    
-v2 = (4,4)
-interseccion(coor1, coor2, v1, v2)  
-
-interseccion(4,3,1,6)
+#### PRUEBAS ####:
+detectar_colision((1, 1), (2, 1), (1, 6), (-1, 6))
+detectar_colision((0, 0), (1, 1), (1, 0), (0, 1))
+detectar_colision((0, 0), (0, 0), (0, 0), (0, 0))
+detectar_colision((0, 0), (2, 2), (1, 1), (1, 1))
+detectar_colision((0, 0), (2, 2), (1, 1), (1, 1))
+detectar_colision((0, 0), (3, 0), (1, 0), (0, 1))
+detectar_colision((0, 0), (3, 0), (1, 0), (2, 0))
+detectar_colision((1, 1), (1, 1), (1, 0), (2, 0))
+detectar_colision((1, 1), (2, 2), (1, 1), (1, 1))
+detectar_colision((0, 0), (1, 1), (1, 0), (2, 0))
