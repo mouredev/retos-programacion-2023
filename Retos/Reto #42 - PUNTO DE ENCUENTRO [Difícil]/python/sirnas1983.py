03fda81e364119f0e8b8919602b1bad570e4d4af
DHiebl <108873454+sirnas1983@users.noreply.github.com>
2023-10-26 17:29:56 -0300

    Reto #42 - python
    Correccion en calculos coplanares

--- Retos/Reto #42 - PUNTO DE ENCUENTRO [Difícil]/python/sirnas1983.py
+++ Retos/Reto #42 - PUNTO DE ENCUENTRO [Difícil]/python/sirnas1983.py
@@ -1,6 +1,8 @@
+import math
 from matplotlib import pyplot as plt
+import numpy
 
-def detectar_colision(coor1, coor2, vel1, vel2, plot_graph=True):
+def detectar_colision(coor1, coor2, vel1, vel2, plot_graph=False):
     '''
     Determina si dos objetos colisionarán en el plano y muestra un gráfico si se especifica.
 
@@ -44,44 +46,49 @@ def detectar_colision(coor1, coor2, vel1, vel2, plot_graph=True):
                 y = y2 + beta * vy2
 
                 # Si alpha y beta son iguales (tiempo) hay colisión, sino solamente intersección de trayectorias
-                if alpha == beta:
+                if alpha == beta and alpha > 0:
                     print(f"Los objetos colisionan en ({x:.2f}, {y:.2f}) transcurridos {alpha:.2f} segundos.")
                     colision = True
                 else:
                     print(f"Si bien las trayectorias se intersectan en ({x:.2f}, {y:.2f}), no hay colisión entre objetos.")
             else:
                 # Hago el análisis puntual de trayectorias coplanares
-                alpha = None
-                beta = None
-                # Para ello calculo la constante del vector
-                if vx1 - vx2 != 0:
-                    alpha = (x2 - x1) / (vx1 - vx2)
-                if vy1 - vy2 != 0:
-                    beta = (y2 - y1) / (vy1 - vy2)
-                # La constante del vector debe ser igual para ambos y mayor a 0
-                if alpha is None and beta is None:
-                    print("Los objetos van a la misma velocidad, no habrá colisión.")
-                elif alpha == beta:
-                    if alpha > 0:
+                vect1 = complex(vx1, vy1)
+                vect12 = complex(x2-x1, y2-y1)
+                vect21 = complex(x1-x2, y1-y2)
+                
+                ang_vect1 = numpy.angle(vect1)
+                ang_vect12 = numpy.angle(vect12)
+                ang_vect21 = numpy.angle(vect21)
+
+                # Determino la colinealidad mediante compara el vector entre P1 y P2 y una de las velocidades
+                if ang_vect1 == ang_vect12 or ang_vect1 == ang_vect21:
+                    alpha = -math.inf
+                    beta = -math.inf    
+                    # Para ello calculo la constante del vector
+                    if vx1 - vx2 != 0:
+                        alpha = (x2 - x1) / (vx1 - vx2)
+                    if vy1 - vy2 != 0:
+                        beta = (y2 - y1) / (vy1 - vy2)
+                    # Si la velocidad en ambos ejes son igualesquiere decir que van a la misma velocidad
+                    if alpha == -math.inf and beta == -math.inf:
+                        print("Los objetos van a la misma velocidad, no habrá colisión.")
+                    elif (alpha == -math.inf and beta > 0) or (beta == -math.inf and alpha > 0) or (beta == alpha > 0):
+                        if alpha <= 0:
+                            alpha = beta
                         x = x1 + alpha * vx1
                         y = y1 + alpha * vy1
                         print(f"Los objetos colisionan en ({x:.2f}, {y:.2f}) transcurridos {alpha:.2f} segundos.")
                         colision = True
                     else:
                         print("Los objetos son colineales pero van separándose, no habrá colisión.")
-                elif (alpha is None and beta > 0) or (beta is None and alpha > 0):
-                    x = x1 + alpha * vx1
-                    y = y1 + alpha * vy1
-                    print(f"Los objetos colisionan en ({x:.2f}, {y:.2f}) transcurridos {alpha:.2f} segundos.")
-                    colision = True
-                elif alpha != beta:
-                    print("Los objetos no colisionarán.")
-                elif (vx1 == vy1 == 0) or (vx2 == vy2 == 0):
-                    print("El objeto en movimiento no intersecta el objeto en reposo.")
 
+                else:
+                    print("Las trayectorias son paralelas, no habrá colisión.")                       
+                
         if colision and plot_graph:
-            plt.axhline(0, color='black', linewidth=0.5)
-            plt.axvline(0, color='black', linewidth=0.5)
+            plt.axhline(0, color='black', linewidth=0.65)
+            plt.axvline(0, color='black', linewidth=0.65)
             plt.scatter(x, y, color='red', linewidths=10, label=f'COLISIÓN\nx: {x:.2f} - y: {y:.2f} - t: {alpha:.2f}')
             plt.scatter(x1, y1, color='blue', linewidths=5, label=f'Salida objeto 1\nVx: {vx1} Vy: {vy1}')
             plt.scatter(x2, y2, color='green', linewidths=5, label=f'Salida objeto 2\nVx: {vx2} Vy: {vy2}')
@@ -99,15 +106,3 @@ def detectar_colision(coor1, coor2, vel1, vel2, plot_graph=True):
     except Exception as e:
         print(f"Error: {e}")
         return colision
-
-#### PRUEBAS ####:
-detectar_colision((1, 1), (2, 1), (1, 6), (-1, 6))
-detectar_colision((0, 0), (1, 1), (1, 0), (0, 1))
-detectar_colision((0, 0), (0, 0), (0, 0), (0, 0))
-detectar_colision((0, 0), (2, 2), (1, 1), (1, 1))
-detectar_colision((0, 0), (2, 2), (1, 1), (1, 1))
-detectar_colision((0, 0), (3, 0), (1, 0), (0, 1))
-detectar_colision((0, 0), (3, 0), (1, 0), (2, 0))
-detectar_colision((1, 1), (1, 1), (1, 0), (2, 0))
-detectar_colision((1, 1), (2, 2), (1, 1), (1, 1))
-detectar_colision((0, 0), (1, 1), (1, 0), (2, 0))
