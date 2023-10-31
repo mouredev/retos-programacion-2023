da88f7794ed919e6445824ff570cd51848bccacc
Jose Corredor <jose96corrza@gmail.com>
2023-10-31 14:01:50 -0500

    First challenge FizzBuzz

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/java/jose827corrza.java
@@ -0,0 +1,30 @@
+/*
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */
+
+
+class Jose827corrza {
+    public static void main(String[] args) {
+        fizzBuzz();
+
+    }
+    
+    public static void fizzBuzz() {
+        for (int i = 0 ; i <= 15 ; i++ ){
+		if( i % 3 == 0 && i % 5 == 0 ){
+			System.out.println("fizzbuzz");
+		} else if( i % 3 == 0  ){
+			System.out.println("fizz");
+		} else if( i % 5 == 0  ){
+			System.out.println("buzz");
+		} else {
+			System.out.println(i);
+		}
+		}
+    }
+}
