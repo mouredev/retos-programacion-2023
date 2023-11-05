20c1eca4424bba145bc3c8449018770211aaa6d6
GMontalvo404 <GMontalvo404@gmail.com>
2023-11-04 19:22:55 -0500

    Reto #0 - Python

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/python/gmontalvo404.py
@@ -0,0 +1,12 @@
+def solve():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+
+
+if __name__ == '__main__':
+    solve()
