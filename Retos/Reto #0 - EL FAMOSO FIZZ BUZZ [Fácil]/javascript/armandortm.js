5a77ea7a3f982985522715bdd3ba0303b3775146
Armando <armandortmontes@gmail.com>
2023-11-04 22:54:40 -0500

    Reto #0 JavaScript

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/javascript/armandortm.js
@@ -0,0 +1,28 @@
+
+let myArray = [];
+
+for (let i = 1; i <= 100; i++) { 
+
+    // agrego al array los numeros del 1 al 100
+    myArray.push(i);    
+}
+
+// funcion que muestra los numeros
+function mostrarNumeros() {
+    
+    myArray.forEach(element => {
+        
+        if (element % 3 == 0) {
+            console.log("fizz" + "\n");
+        }
+        else if (element % 5 == 0) {
+            console.log("buzz" + "\n");
+        }
+        else if ((element % 3 == 0) && (element % 5 == 0)) {
+            console.log("fizzbuzz" + "\n");
+        }
+    });
+}
+
+// llamo a la funcion
+mostrarNumeros();
