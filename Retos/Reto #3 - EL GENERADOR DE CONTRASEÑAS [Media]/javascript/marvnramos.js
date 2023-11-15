5dfb97c9cb9a767635889916dc2386f12065b393
Marvin Ramos <mr487223@gmail.com>
2023-11-14 23:21:15 -0600

    initiating logic

--- Retos/Reto #3 - EL GENERADOR DE CONTRASEÑAS [Media]/javascript/marvnramos.js
+++ Retos/Reto #3 - EL GENERADOR DE CONTRASEÑAS [Media]/javascript/marvnramos.js
@@ -11,12 +11,100 @@
 
 
 
-const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-const numbers = [0,1,2,3,4,5,6,7,8,9];
-const symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '[', ']', '{', '}', ';', ':', ',', '.', '<', '>', '/', '?', '|']
+// const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// const numbers = [0,1,2,3,4,5,6,7,8,9];
+// const symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '[', ']', '{', '}', ';', ':', ',', '.', '<', '>', '/', '?', '|']
 
-const passwordGenerator = (password_length, upperCase, withNumbers, withSymbols) =>{
-    
+const parameters = {
+    alphabet: [
+        'a', 'b', 'c', 'd', 'e', 
+        'f', 'g', 'h', 'i', 'j', 
+        'k', 'l', 'm', 'n', 'o', 
+        'p', 'q', 'r', 's', 't', 
+        'u', 'v', 'w', 'x', 'y', 
+        'z'
+    ],
+    symbols: [
+        '!', '@', '#', '$', '%', 
+        '^', '&', '*', '(', ')', 
+        '_', '+', '-', '=', '[', 
+        ']', '{', '}', ';', ':', 
+        ',', '.', '<', '>', '/', 
+        '?', '|'
+    ],
+    numbers: [0,1,2,3,4,5,6,7,8,9]
 }
 
-console.log(Math.floor(Math.random()*10));
+
+const getIndex = (num) => {
+    if(!num){
+        return 'Parámetro requerido.';
+    }
+    
+    const randomNumber = Math.floor(Math.random()*num);
+    
+    return randomNumber;
+};
+
+const getParameter = (obj) => {
+  if (!obj) {
+    return 'Parámetro requerido.';
+  }
+
+  const key = Object.keys(obj);
+  if(key === 0){
+    return 'Objeto vacío.';
+  }
+
+  const randomKey = Math.floor(Math.random() * key.length);
+  const randomObjectMember = obj[key[randomKey]];
+
+  return randomObjectMember;
+  ;
+
+};
+
+const getPasswordWithAllParameters = (passwordLength, upperCase, withNumbers, withSymbols) =>{
+    let password = '';
+    const regex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};:,.<>\/?|]).*$/;
+
+    if(upperCase, withNumbers, withSymbols){
+        for(let i = 0; i <= passwordLength - 1; i++){
+            const parameter = getParameter(parameters);
+            const index = getIndex(parameter.length);
+    
+            password += parameter[index];
+        }
+
+        password = password.toUpperCase();
+
+        return regex.test(password) ? password : getPassword(upperCase, withNumbers, withSymbols);
+    }
+}
+const getPasswordWithLowerCase = ()=>{
+    // logica
+}
+
+const passwordGenerator = (passwordLength, upperCase, withNumbers, withSymbols) =>{
+    let password = '';
+
+    const validPasswordLength = passwordLength >= 8 && passwordLength <= 16;
+
+    if(!validPasswordLength){
+        return 'La longitud de la contraseña debe estar entre 8 y 16';
+    } 
+
+    if(upperCase, withNumbers, withSymbols){
+        password = getPasswordWithAllParameters(passwordLength, upperCase, withNumbers, withSymbols);
+        return password;
+    }
+    else if( upperCase === true && (withNumbers, withSymbols) === false){
+        // logica
+    }
+    
+    
+};
+
+
+console.log(passwordGenerator(8, true, true, true));
+// console.log(getParameter(parameters))
