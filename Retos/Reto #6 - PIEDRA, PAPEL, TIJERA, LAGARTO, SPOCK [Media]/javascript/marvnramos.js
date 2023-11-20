52fc6f8a24873c899bfbc20998617ec2325b625c
Marvin Ramos <mr487223@gmail.com>
2023-11-19 19:25:34 -0600

    raising game logic

---
+++ Retos/Reto #6 - PIEDRA, PAPEL, TIJERA, LAGARTO, SPOCK [Media]/javascript/marvnramos.js
@@ -0,0 +1,27 @@
+/*
+player 1
+player 2
+tie
+*/
+
+/*
+===== Lógica del juego =====
+    🖖 rompe        ✂️
+    🖖 vaporiza     🗿
+
+    ✂️ decapita     🦎
+    ✂️ corta        📄
+
+    📄 desautoriza  🖖
+    📄 envuelve     🗿
+
+    🗿 aplasta      ✂️
+    🗿 aplasta      🦎
+
+    🦎 envenena     🖖
+    🦎 devora       📄
+*/
+
+
+
+
