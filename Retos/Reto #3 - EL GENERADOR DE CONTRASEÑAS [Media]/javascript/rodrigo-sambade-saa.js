570c25384b0035adc502be37fd73fc57b24c6109
Rodrigo Sambade <rodrigosambadesaa@gmail.com>
2023-11-23 17:30:40 +0100

    Añadida solución al reto 3

---
+++ Retos/Reto #3 - EL GENERADOR DE CONTRASEÑAS [Media]/javascript/rodrigo-sambade-saa.js
@@ -0,0 +1,33 @@
+function generatePassword(length, includeUppercase, includeNumbers, includeSymbols) {
+    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numberChars = '0123456789';
+    const symbolChars = '!@#$%^&*()_+[]{}|;:,.<>?';
+  
+    let validChars = lowercaseChars;
+  
+    if (includeUppercase) {
+      validChars += uppercaseChars;
+    }
+  
+    if (includeNumbers) {
+      validChars += numberChars;
+    }
+  
+    if (includeSymbols) {
+      validChars += symbolChars;
+    }
+  
+    let password = '';
+  
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * validChars.length);
+      password += validChars[randomIndex];
+    }
+  
+    return password;
+  }
+  
+  const newPassword = generatePassword(16, true, true, true);
+  console.log(newPassword);
+  
