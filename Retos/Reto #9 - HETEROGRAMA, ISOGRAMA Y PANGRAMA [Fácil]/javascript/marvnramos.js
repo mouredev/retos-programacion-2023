7ec8edd089fd9ba61629f038b59037622f4bdd4a
Marvin Ramos <mr487223@gmail.com>
2023-11-24 21:31:33 -0600

    add isHeterogram feature logic

--- Retos/Reto #9 - HETEROGRAMA, ISOGRAMA Y PANGRAMA [Fácil]/javascript/marvnramos.js
+++ Retos/Reto #9 - HETEROGRAMA, ISOGRAMA Y PANGRAMA [Fácil]/javascript/marvnramos.js
@@ -1,13 +1,29 @@
-
+/**
+ * A function that tests if a string is a heterogram
+ * @param {*} string - a string to be tested
+ * @returns - true if the string is a heterogram, false otherwise
+ */
 const isHeterogram = function(string){
-    // logic here
-}
+    for(let i = 0; i < string.length; i++){
+        let character = string[i];
+
+        if(string.indexOf(character) !== string.lastIndexOf(character)){
+            return false; // the character is repeated
+        };
+    };
+
+    return true;
+};
 
 const isIsogram = function(string){
     // logic here
-}
+};
 
 
 const isPangram = function(string){
     // logic here
-}
+};
+
+let string = "abcdefga";
+
+console.log(isHeterogram(string));
