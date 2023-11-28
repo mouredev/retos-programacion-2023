d0f8f1401d98dfc87743dc71136e9472947e3a5e
Edwin <ed25gonz@gmail.com>
2023-11-28 15:53:14 -0400

    Reto #6 - JavaScript

---
+++ Retos/Reto #6 - PIEDRA, PAPEL, TIJERA, LAGARTO, SPOCK [Media]/javascript/EdGonzz.js
@@ -0,0 +1,41 @@
+//  * Crea un programa que calcule quien gana más partidas al piedra,
+//  * papel, tijera, lagarto, spock.
+//  * - El resultado puede ser: "Player 1", "Player 2", "Tie" (empate)
+//  * - La función recibe un listado que contiene pares, representando cada jugada.
+//  * - El par puede contener combinaciones de "🗿" (piedra), "📄" (papel),
+//  *   "✂️" (tijera), "🦎" (lagarto) o "🖖" (spock).
+//  * - Ejemplo. Entrada: [("🗿","✂️"), ("✂️","🗿"), ("📄","✂️")]. Resultado: "Player 2".
+//  * - Debes buscar información sobre cómo se juega con estas 5 posibilidades.
+
+const rules = {
+    "🦎": ["🖖", "📄"],
+    "🗿": ["🦎", "✂️"],
+    "✂️": ["📄", "🦎"],
+    "🖖": ["✂️", "🗿"],
+    "📄": ["🗿", "🖖"]
+}
+
+const rockPaperScissorsLizardSpock = (games) => {
+
+    let player1 = 0
+    let player2 = 0
+
+    for (const game of games) {
+        let player1_game = game[0]
+        let player2_game = game[1]
+
+        if (player1_game !== player2_game) {
+            if (rules[player1_game].includes(player2_game)) {
+                player1++
+            } else if (rules[player2_game].includes(player1_game)) {
+                player2++
+            }
+        }
+    }
+    return (player1 === player2) ? 'Tie' : (player1 > player2) ? 'Player 1 Win' : 'Player 2 Win'
+}
+
+console.log(rockPaperScissorsLizardSpock([["🦎", "📄"], ["📄", "🖖"], ["📄", "✂️"]]))
+console.log(rockPaperScissorsLizardSpock([["✂️", "🖖"], ["🖖", "✂️"], ["✂️", "🦎"]]))
+console.log(rockPaperScissorsLizardSpock([["📄", "✂️"], ["📄", "📄"], ["🖖", "✂️"]]))
+console.log(rockPaperScissorsLizardSpock([["🖖", "🦎"], ["🗿", "✂️"], ["🦎", "✂️"]]))
