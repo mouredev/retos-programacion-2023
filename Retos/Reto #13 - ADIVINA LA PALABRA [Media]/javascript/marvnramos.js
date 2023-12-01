a4dc0e7f0ed88783aa085c593bbab68511e35f9f
Marvin Ramos <mr487223@gmail.com>
2023-11-30 21:22:35 -0600

    add getRandomWord feature

--- Retos/Reto #13 - ADIVINA LA PALABRA [Media]/javascript/marvnramos.js
+++ Retos/Reto #13 - ADIVINA LA PALABRA [Media]/javascript/marvnramos.js
@@ -5,10 +5,23 @@ const readLine = ReadLine.createInterface({
     output: process.stdout,
 });
 
+const getRandomWord = () => {
+    const words = ["javascript", "developer", "website", "marvnramos", "mauredev"];
+    const randomWord = words[Math.floor(Math.random() * words.length)];
+    
+    let incompleteWord = "";
 
+    for(let i = 0; i < 5; i++) {
+        let index = Math.floor(Math.random() * randomWord.length);
+        incompleteWord = randomWord.split(randomWord[index]).join("_");
+    }
+
+    return incompleteWord;
+};
+
+console.log(getRandomWord());
 
 const guessWord = (word, attempts) => {
-    const words = ["javascript", "developer", "website", "marvnramos", "mauredev"];
     
     readLine.question("Enter a word: ", (answer) => {
         console.log(`Your word is: ${answer}\n`);
