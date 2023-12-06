29aa825cad7ccad2a20959671c2cd24e490227af
Akihiro93 <krystof2ms.sale24@gmail.com>
2023-12-06 18:01:06 -0500

    Reto #37 - python

--- Retos/Reto #37 - COLORES HEX Y RGB [Media]/python/Aknihiro93.py
+++ Retos/Reto #37 - COLORES HEX Y RGB [Media]/python/Aknihiro93.py
@@ -1,23 +1,19 @@
-
-
 def converter_rgb(r:int, g:int, b:int) -> str:
     if r > 255 or g > 255 or b > 255:
         print("Error")
         return ""
-    list_result = list()
-
+    result = "#"
     for i in [r, g, b]:
-        if i < 9:
-            i = hex(i)
-            i = i[2:].zfill(2)
-        else:
-            i = hex(i)
-            i = i[2:].upper()
-        list_result.append(i)
-    return "#" + f"{list_result[0]}{list_result[1]}{list_result[2]}"
+        temp = i
+        i = hex(i)[2:]
+        if temp < 16:
+            i = i.zfill(2)    
+        i = i.upper()
+        result = result + i
+    return result
 
 def converter_hex (HEX:str):
     return f"(r: {int(HEX[1:3].lower(), 16)}, g: {int(HEX[3:5].lower(), 16)}, b: {int(HEX[5:7].lower(), 16)}"
 
-print(converter_rgb(8, 200, 33))
+print(converter_rgb(255, 200, 33))
 print(converter_hex("#08C821"))
