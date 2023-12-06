9cec1061221a96d76978120cf41c77ab329f61c3
Akihiro93 <krystof2ms.sale24@gmail.com>
2023-12-06 17:59:45 -0500

    Reto #37 - go

--- Retos/Reto #37 - COLORES HEX Y RGB [Media]/go/Akihiro93.go
+++ Retos/Reto #37 - COLORES HEX Y RGB [Media]/go/Akihiro93.go
@@ -2,33 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strconv"
 	"strings"
 )
 
 func main() {
 	fmt.Print("")
 	converter_rgb(8, 200, 33)
-	// converter_hex("#8C821")
+	converter_hex("#08C821")
 }
 
 func converter_rgb(r, g, b int) {
+	
 	var result string = "#"
-
 	for _, v := range([]int{r, g,b}) {
 		var hex_v string = ""
-		if v < 10 {
-			hex_v = fmt.Sprintf("%x", v)
-			hex_v = hex_v + "0"
-		} else {	
-			hex_v = fmt.Sprintf("%x", v)
+			hex_v = fmt.Sprintf("%02X", v)
 			hex_v = strings.ToUpper(hex_v)
-		}
-		
 		result = result + hex_v
 	}
 	fmt.Println(result)
-
-
 }
 
-//func converter_hex(hex string)  {}
+func converter_hex(hex string) {
+	var list_values []int64
+	for _, v := range([]string{hex[1:3], hex[3:5], hex[5:7]}) {
+		i, err := strconv.ParseInt(v, 16, 64)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		list_values = append(list_values, i)
+	}
+	fmt.Printf("(r: %d, g: %d, b: %d )",list_values[0], list_values[1], list_values[2])
+}
