1c8b9adb451fedd4e1781b668f038a020926a569
Pablo Veiga <nwpablodeveloper@gmail.com>
2023-12-29 20:45:11 +0100

    Reto #0

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/java/nwpablodeveloper.java
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/java/nwpablodeveloper.java
@@ -1,13 +1,10 @@
 public class nwpablodeveloper {
     public static void main(String[] args) {
-          
-        for(int i = 1; i <=100; i++){
-            String palabra = (i % 3 == 0 && i % 5 == 0) ? "fizzbuzz" 
-                            :(i % 5 == 0 ) ? "buzz"
-                            :(i % 3 == 0 ) ? "buzz" : String.valueOf(i);
-            System.out.println(i + "\t" + palabra);
+        for(int i=1;i<=100;i++){
+            String out= (i%3==0 && i%5==0)?"fizzbuzz" 
+                        :(i%5 == 0 )?"buzz"
+                        :(i%3 == 0 )?"buzz":String.valueOf(i);
+            System.out.println(i+"\t"+out);
         }
-        
     }
-
 }
