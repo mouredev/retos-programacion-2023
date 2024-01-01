910b33f6261a8ed63d00dde972ed64e1d85bd748
ContratopDev <miandroidtabletfirst@gmail.com>
2024-01-01 15:02:42 +0100

    Add files via upload

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/powershell/ContratopDev.ps1
@@ -0,0 +1,15 @@
+
+for ($i = 1; $i -le 100; $i++) {
+    if ($i % 3 -eq 0 -and $i % 5 -eq 0) {
+        Write-Host "FizzBuzz"
+    }
+    elseif ($i % 3 -eq 0) {
+        Write-Host "Fizz"
+    }
+    elseif ($i % 5 -eq 0) {
+        Write-Host "Buzz"
+    }
+    else {
+        Write-Host $i
+    }
+}
