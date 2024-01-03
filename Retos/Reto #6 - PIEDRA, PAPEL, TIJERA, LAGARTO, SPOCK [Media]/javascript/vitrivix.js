0ed8f2d84b9a03013bbebcc02c78b2b99fadd704
VitriVix <joseluismamanimollisaca@gmail.com>
2024-01-03 04:17:21 -0400

    Reto #6 - javascript

--- Retos/Reto #6 - PIEDRA, PAPEL, TIJERA, LAGARTO, SPOCK [Media]/javascript/vitrivix.js
+++ Retos/Reto #6 - PIEDRA, PAPEL, TIJERA, LAGARTO, SPOCK [Media]/javascript/vitrivix.js
@@ -8,3 +8,56 @@
  * - Ejemplo. Entrada: [("🗿","✂️"), ("✂️","🗿"), ("📄","✂️")]. Resultado: "Player 2".
  * - Debes buscar información sobre cómo se juega con estas 5 posibilidades.
  */
+
+const stonePaperScissorsLizardSpock = (arg) => {
+    
+    let = player1 = 0, player2 = 0;
+
+    for (const element of arg) {
+        if (element.player1 === "🗿") {
+            if (element.player2 === "✂️" || element.player2 === "🦎") player1++;
+        }
+        if (element.player1 === "📄") {
+            if (element.player2 === "🗿" || element.player2 === "🖖") player1++;
+        }
+        if (element.player1 === "✂️" ) {
+            if (element.player2 === "📄" || element.player2 === "🦎") player1++;
+        }
+        if (element.player1 === "🦎" ) {
+            if (element.player2 === "📄" || element.player2 === "🖖") player1++;
+        }
+        if (element.player1 === "🖖") {
+            if (element.player2 === "🗿" || element.player2 === "✂️") player1++;
+        }
+        
+        if (element.player2 === "🗿") {
+            if (element.player1 === "✂️" || element.player1 === "🦎") player2++;
+        }
+        if (element.player2 === "📄") {
+            if (element.player1 === "🗿" || element.player1 === "🖖") player2++;
+        }
+        if (element.player2 === "✂️" ) {
+            if (element.player1 === "📄" || element.player1 === "🦎") player2++;
+        }
+        if (element.player2 === "🦎" ) {
+            if (element.player1 === "📄" || element.player1 === "🖖") player2++;
+        }
+        if (element.player2 === "🖖") {
+            if (element.player1 === "🗿" || element.player1 === "✂️") player2++;
+        }
+    } 
+     return player1 === player2 ? "Tie" : player1 > player2 ? "Player 1" :"Player 2"
+
+}
+
+const firstGame = [{player1:"🗿", player2:"✂️"}, {player1:"✂️", player2:"🗿"}, {player1:"📄", player2:"✂️"}]
+
+const secondGame = [{player1:"🦎",  player2:"🖖"}, {player1:"✂️", player2:"🦎"}, {player1:"✂️", player2:"✂️"},
+{player1:"🗿",  player2:"🖖"}, {player1:"✂️", player2:"✂️"}, {player1:"📄", player2:"📄"}]
+
+const thirdGame = [{player1:"🗿",  player2:"✂️"}, {player1:"✂️", player2:"🗿"}, {player1:"📄", player2:"✂️"},
+{player1:"🗿",  player2:"🦎"}, {player1:"✂️", player2:"🦎"}, {player1:"📄", player2:"✂️"}]
+
+console.log(stonePaperScissorsLizardSpock(firstGame))
+console.log(stonePaperScissorsLizardSpock(secondGame))
+console.log(stonePaperScissorsLizardSpock(thirdGame))
