be684d5a96ef57f83af36bb80897c235bfed5948
ContratopDev <miandroidtabletfirst@gmail.com>
2024-01-04 22:54:52 +0100

    funcion mejorada

--- Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/powershell/contratop.ps1
+++ Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/powershell/contratop.ps1
@@ -1,26 +1,39 @@
-function ConvertTo-Leet { # Creamos la función "ConvertTo-Leet"
-    param ( # Definimos los parámetros que recibirá la función
-        [Parameter(Mandatory=$true)] # Caracteristicas del parametro. En este caso, el parámetro de abajo es obligatorio
-        [string]$Text # El parámetro "Text" es de tipo "string"
+function ConvertTo-Leet { # Definir la funcion "ConvertTo-Leet"
+    param( # Definir los parametros de la funcion
+        [Parameter(Mandatory=$true)] # Se establece el parametro como obligatorio
+        [string]$texto # El parametro es de tipo string, se llama -texto y se almacena en la variable $texto
     )
 
-    $LeetText = ""  # Creamos una variable vacía para almacenar el texto convertido
-
-    foreach ($char in $Text) {  # Recorremos cada caracter del texto
-                                              # $char es la variable que contiene el caracter actual
-                                              # $Text es la variable que contiene el texto que recibimos como parámetro
-        switch ($char.ToLower()) {  # Comparamos el caracter actual con cada uno de los caracteres que queremos reemplazar
-            "a" { $LeetText += "4" } # Si el caracter actual es igual a alguno de los caracteres que queremos reemplazar, lo reemplazamos
-            "e" { $LeetText += "3" }
-            "i" { $LeetText += "1" }
-            "o" { $LeetText += "0" }
-            "s" { $LeetText += "5" }
-            "t" { $LeetText += "7" }
-            default { $LeetText += $char } # Si el caracter actual no es igual a ninguno de los caracteres que queremos reemplazar, lo dejamos igual
-        }
+    # Definir un hash table con las conversiones leet
+    $leetTable = @{
+        'a' = '4'
+        'e' = '3'
+        'i' = '1'
+        'o' = '0'
+        's' = '5'
+        't' = '7'
     }
 
-    return $LeetText # Retornamos el texto convertido
+    # Convertir el texto a minusculas
+    $texto = $texto.ToLower()
+
+    # Recorrer el hash table y reemplazar las letras
+    foreach ($letra in $leetTable.Keys) { # por cada $letra en las llaves del hash table
+        $texto = $texto.Replace($letra, $leetTable[$letra]) # reemplazar la letra por el valor del hash table
+    }
+
+    # Devolver el texto convertido
+    return $texto
 }
 
-ConvertTo-Leet -text "Hola Mundo" # Llamamos a la función "ConvertTo-Leet" y le pasamos el texto que queremos convertir
+# Ejemplo de uso ###
+
+# Definir el texto a convert
+$miTexto = "Hola, esto es un ejemplo de lenguaje leet."
+
+# Llamamos a la funcion ConvertTo-Leet y le especificamos el parametro -texto con el valor de $miTexto
+# que posteriormente se almacena en la variable $textoLeet el resultado
+$textoLeet = ConvertTo-Leet -Texto $miTexto
+
+# Mostramos el resultado con Write-Host
+Write-Host $textoLeet
