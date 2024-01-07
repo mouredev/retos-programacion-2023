3737b3eb3db4d9756f596885927a744fa97272f0
switchdays <victor0meseguer@gmail.com>
2024-01-07 12:44:25 +0100

    16-Python switchdays.py

--- Retos/Reto #16 - LA ESCALERA [Media]/python/switchdays.py
+++ Retos/Reto #16 - LA ESCALERA [Media]/python/switchdays.py
@@ -15,28 +15,27 @@
 
 def crear_escaleras(contador_str0):
 
-    if contador_str0 != "fin":
-        contador = int(contador_str0)
-        espacios = 0
+    contador = int(contador_str0)
+    espacios = 0
 
-        if contador == 0:
-            print("__")
+    if contador == 0:
+        print("__")
 
-        elif contador > 0:
-            espacios = contador * 2
-            print((espacios + 2) * " " +"_")
-            while contador != 0:
-                print(espacios * " " + "_|")
-                contador -= 1
-                espacios -= 2
+    elif contador > 0:
+        espacios = contador * 2
+        print((espacios + 2) * " " +"_")
+        while contador != 0:
+            print(espacios * " " + "_|")
+            contador -= 1
+            espacios -= 2
 
-        elif contador < 0:
-            print("_")
-            espacios = 1
-            while contador != 0:
-                print(espacios * " " + "|_")
-                contador += 1
-                espacios += 2
+    elif contador < 0:
+        print("_")
+        espacios = 1
+        while contador != 0:
+            print(espacios * " " + "|_")
+            contador += 1
+            espacios += 2
 
 print("Para salir introduzca: fin")
 contador_str1 = ""
@@ -44,4 +43,5 @@ contador_str1 = ""
 while contador_str1 != "fin":
 
     contador_str1 = input("Introduce el número de escaleras:")
-    crear_escaleras(contador_str1)
+    if contador_str1 != "fin":
+        crear_escaleras(contador_str1)
