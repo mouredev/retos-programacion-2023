6ea40a6b2f334fcc37b823736c0c3df03686c97b
dPenedo <dpenedo@tutanota.com>
2024-01-07 23:22:15 -0300

    Reto #2 - python

--- Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/python/dPenedo.py
+++ Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/python/dPenedo.py
@@ -20,49 +20,61 @@
 
 PUNTUACIONES = ["Love", "15", "30",  "40"]
 
-print("Bienvenidos al partido de tenis. ")
-
 
 def jugar_juego():
     cuenta_p1 = 0
     cuenta_p2 = 0
     deuce = False
     seguir_jugando_juego = True
+
     while seguir_jugando_juego:
-        print("Quién marcó?")
-        tanto = input("pulsa 1 si marcó P1 y 2 si marcó P2")
+        print("¿Quién marcó?")
+        tanto = input("pulsa 1 si marcó P1 y 2 si marcó P2: \n")
+        if tanto not in ["1", "2"]:
+            print("Entrada inválida. Por favor, introduce 1 o 2.")
+            continue
+
         if (tanto == "1"):
             cuenta_p1 += 1
             print("Tanto para el jugador 1!")
-            if (cuenta_p1 < 4 and cuenta_p2 < 4):
-                print(f"Jugador 1 =  {PUNTUACIONES[cuenta_p1]} || Jugador 2 = {PUNTUACIONES[cuenta_p2]}")
-                print("-"*30)
-            if(cuenta_p1 == 3 and cuenta_p2 == 3):
-                print("-"*30)
-                print("Hay Deuce entre los jugadores")
-                print("-"*30)
-                deuce = True
-            if (deuce and cuenta_p1 - 1 == cuenta_p2):
-                print("Hay ventaja para el jugador 1, siguen en Deuce")
-            elif (deuce and (cuenta_p1-1)> cuenta_p2):
-                print("Juego terminadoo! Gana el jugador 1!")
-                seguir_jugando_juego = False
-
-        if (tanto == "2"):
+        elif (tanto == "2"):
             cuenta_p2 += 1
             print("Tanto para el jugador 2!")
-            if (cuenta_p1 < 4 and cuenta_p2 < 4):
-                print(f"Jugador 1 =  {PUNTUACIONES[cuenta_p1]} || Jugador 2 = {PUNTUACIONES[cuenta_p2]}")
-                print("-"*30)
-            if(cuenta_p1 == 3 and cuenta_p2 == 3):
-                print("Hay Deuce entre los jugadores")
-                deuce = True
-            if (deuce and cuenta_p2 - 1 == cuenta_p1):
-                print("Hay ventaja para el jugador 2, siguen en Deuce")
-            elif (deuce and (cuenta_p2-1)> cuenta_p1):
-                print("Juego terminadoo! Gana el jugador 2!")
-                seguir_jugando_juego = False
+
+
+        if (cuenta_p1 < 4 and cuenta_p2 < 4):
+            print("-"*40)
+            print(f"Jugador 1 =  {PUNTUACIONES[cuenta_p1]} || Jugador 2 = {PUNTUACIONES[cuenta_p2]}")
+            print("-"*40)
+
+        if(cuenta_p1 >= 3 and cuenta_p2 >= 3 and cuenta_p1 == cuenta_p2 ):
+            print("-"*40)
+            print("Hay Deuce entre los jugadores")
+            print("-"*40)
+            deuce = True
+        if (deuce and cuenta_p1 - 1 == cuenta_p2):
+            print("-"*40)
+            print("Hay ventaja para el jugador 1, siguen en Deuce")
+            print("-"*40)
+        elif (deuce and cuenta_p2 - 1 == cuenta_p1):
+            print("-"*40)
+            print("Hay ventaja para el jugador 2, siguen en Deuce")
+            print("-"*40)
+
+        elif (deuce and (cuenta_p1-1) > cuenta_p2):
+            print("+"*40)
+            print("Juego terminadoo! Gana el jugador 1!")
+            print("+"*40)
+            seguir_jugando_juego = False
+        elif (deuce and (cuenta_p2-1) > cuenta_p1):
+            print("+"*40)
+            print("Juego terminadoo! Gana el jugador 2!")
+            print("+"*40)
+            seguir_jugando_juego = False
 
 
 if __name__ == "__main__":
+    print("+"*40)
+    print("Bienvenidos al partido de tenis. ")
+    print("+"*40)
     jugar_juego()
