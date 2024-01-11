f6c201b359ce32cfde5e6d74f1ac73126fc269c7
Josep Monclus <monclusj@aia.es>
2024-01-11 09:30:24 +0100

    Reto #48 - Python

---
+++ Retos/Reto #48 - EL RANKING [Difícil]/python/josepmonclus.py
@@ -0,0 +1,13 @@
+'''
+Todo llega a su fin... Este es el último reto de programación 
+semanal de 2023.
+Crea un programa que muestre un listado calculado en tiempo real
+con todos los usuarios que han resuelto algún reto de programación
+de este año.
+- El listado debe estar ordenado por el número de ejercicios resueltos
+  por cada usuario (y mostrar ese contador al lado de su nombre).
+- También se debe de mostrar el número de usuarios que han participado
+  y el número de correcciones enviadas.
+Muchísimas gracias por ayudar a crear este gran recurso
+para la comunidad... ¡Prepárate para 2024!   
+'''
