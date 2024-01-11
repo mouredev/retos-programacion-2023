523cffca03e98a61cc718e1587454ef7cd12df8a
Josep Monclus <josep.monclus@gmail.com>
2024-01-11 11:10:35 +0100

    Reto #48 - Python

--- Retos/Reto #48 - EL RANKING [Difícil]/python/josepmonclus.py
+++ Retos/Reto #48 - EL RANKING [Difícil]/python/josepmonclus.py
@@ -11,3 +11,30 @@ de este año.
 Muchísimas gracias por ayudar a crear este gran recurso
 para la comunidad... ¡Prepárate para 2024!   
 '''
+import os
+
+usuarios = {}
+ejercicios = 0
+
+start_path = os.path.dirname(__file__)
+start_path = os.path.abspath(os.path.join(start_path, os.pardir, os.pardir))
+print(start_path)
+
+for ruta_actual, carpetas, archivos in os.walk(start_path):
+    for archivo in archivos:
+        if archivo not in ['language_stats.py', 'ejercicio.md']:
+            ejercicios += 1
+            usuario = archivo.split('.')[0]
+            if usuario in usuarios:
+                usuarios[usuario] += 1
+            else:
+                usuarios[usuario] = 1
+
+usuarios_ordenados = sorted(usuarios.items(), key=lambda x: x[1], reverse=True)
+
+print(f'Número de correcciones enviadas: {ejercicios}')
+print(f'Número de usuarios participantes: {len(usuarios)}')
+print(f'Ranking de participantes (TOP 50): ')
+for index, usuario in enumerate(usuarios_ordenados):
+    if index < 50:
+        print(f' - {index + 1} {usuario[0]}: {usuario[1]}')
