de60ec70003c18bc848133103006408bcae2f429
jelambrar96 <jelambrar@gmail.com>
2024-02-16 07:49:50 -0500

    reto_05_python_jelambrar96

---
+++ Retos/Reto #5 - HOLA MUNDO [Fácil]/python/jelambrar96.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+# Reto #5: ¡Hola Mundo!
+/*
+ * Escribe un !Hola Mundo! en todos los lenguajes de programación que puedas.
+ * Seguro que hay algún lenguaje que te llama la atención y nunca has utilizado,
+ * o quizás quieres dar tus primeros pasos... ¡Pues este es el momento!
+ *
+ * A ver quién se atreve con uno de esos lenguajes que no solemos ver por ahí... 
+ */
+"""
+
+__author__ = "Jorge Lambraño - jelambrar96"
+__copyright__ = "Copyright 2024, retos-programacion-2023"
+__credits__ = ["Brais Moure - mouredev"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Jorge Lambraño"
+__email__ = "jelambrar@gmail.com"
+__status__ = "Production"
+
+def HolaMundo():
+    print("¡Hola Mundo!")
+
+if __name__ == '__main__':
+    HolaMundo()
+
