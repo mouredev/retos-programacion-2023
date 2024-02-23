2f63ee4b3372aaa215868523f3b213d23547480a
Andres Velasquez <avelasquez@klika-tech.com>
2024-02-23 10:50:49 -0500

    Revert "Reto # 0 - Scala"
    This reverts commit c5b5f282824931a0909b0cff4cf0aa2fbd54e7f6.

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/scala/avelasquezhe84.scala
+++
@@ -1,9 +0,0 @@
-// Scala 3
-
-@main def fizzbuzz() =
-    (1 to 100).foreach(i => 
-        if i % 15 == 0 then println("fizzbuzz")
-        else if i % 3 == 0 then println("fizz")
-        else if i % 5 == 0 then println("buzz")
-        else println(i)
-    )
