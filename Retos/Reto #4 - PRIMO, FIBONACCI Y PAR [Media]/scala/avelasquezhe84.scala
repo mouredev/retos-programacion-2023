21ebc444ab16e5ef31eb6d0d6c3d1767373feadc
Andres Velasquez <avelasquez@klika-tech.com>
2024-02-25 19:04:10 -0500

    Reto #4 - Scala

--- Retos/Reto #4 - PRIMO, FIBONACCI Y PAR [Media]/scala/avelasquezhe84.scala
+++ Retos/Reto #4 - PRIMO, FIBONACCI Y PAR [Media]/scala/avelasquezhe84.scala
@@ -6,7 +6,6 @@ import scala.math.floor
     val prime = if isPrime(number) then "es primo" else "no es primo"
     val fibonacci = if isFibonacci(number) then "es fibonacci" else "no es fibonacci"
     val even = if isEven(number) then "es par" else "es impar"
-
     println(s"$number $prime, $fibonacci, $even")
 
 
@@ -14,6 +13,7 @@ def isEven(num: Int): Boolean = num % 2 == 0
 
 def isPrime(num: Int): Boolean = 
     if num < 4 then true
+    else if isEven(num) then false
     else
         val upperLimit = floor(sqrt(num)).toInt
         val factors = (2 to upperLimit).toList
