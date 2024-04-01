129bee1414bd8492ad77dc196829a17dcb49cfd6
andres-rios-intelex <147648981+andres-rios-intelex@users.noreply.github.com>
2024-04-01 10:08:14 -0500

    new data

---
+++ Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/javascript/andres.rios-gif.js
@@ -0,0 +1,33 @@
+/*
+ * Escribe un programa que muestre cómo transcurre un juego de tenis y quién lo ha ganado.
+ * El programa recibirá una secuencia formada por "P1" (Player 1) o "P2" (Player 2), según quien
+ * gane cada punto del juego.
+ * 
+ * - Las puntuaciones de un juego son "Love" (cero), 15, 30, 40, "Deuce" (empate), ventaja.
+ * - Ante la secuencia [P1, P1, P2, P2, P1, P2, P1, P1], el programa mostraría lo siguiente:
+ *   15 - Love
+ *   30 - Love
+ *   30 - 15
+ *   30 - 30
+ *   40 - 30
+ *   Deuce
+ *   Ventaja P1
+ *   Ha ganado el P1
+ * - Si quieres, puedes controlar errores en la entrada de datos.   
+ * - Consulta las reglas del juego si tienes dudas sobre el sistema de puntos.   
+ */
+
+const score = (array) =>  {
+    let player1; 
+    let player2;
+    let noPoints = "Love"; 
+
+    if(player1 === 0 || player2 === 0) {
+        noPoints = "Love";
+    } else if(array[0] === "P1") {
+        player1 = 15;
+    }
+    console.log(player1 + " - " + noPoints);
+}
+
+score(["P1"]); 
