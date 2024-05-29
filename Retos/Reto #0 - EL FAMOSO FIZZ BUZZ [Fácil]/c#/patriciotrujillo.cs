4a87acc0c30201432e7c33edd5feeee4c0784f4a
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2024-05-29 15:24:12 -0400

    Reto #0 - C#

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/c#/patriciotrujillo.cs
@@ -0,0 +1,27 @@
+/*
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */
+
+public static void Main()
+{
+    for (int i = 1; i <= 100; i++)
+    {
+        if (i % 5 == 0 && i % 3 == 0)
+        {
+            Console.WriteLine(i + "-fizzbuzz");
+        }
+        else if (i % 3 == 0)
+        {
+            Console.WriteLine(i + "-fizz");
+        }
+        else if (i % 5 == 0)
+        {
+            Console.WriteLine(i + "-buzz");
+        }
+    }
+}
