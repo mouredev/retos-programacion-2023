1d039ee8da5df29d272434732b7524fad9206312
Ivan Mauricio Velazquez Trejo <ivanmauriciovt@gmail.com>
2024-06-21 15:18:21 -0600

    Reto #1 - Javascript

--- Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/javascript/ivanmauriciovt.js
+++ Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/javascript/ivanmauriciovt.js
@@ -3,14 +3,13 @@ const alphabetHacker = new Map([
     ['a', '4'], ['b', 'I3'], ['c', '['], ['d', ')'], ['e', '3'], ['f', '|='], ['g', '&'], ['h', '#'], ['i', '1'],
     ['j', ',_|'], ['k', '>|'], ['l', '1'], ['m', '/\\/\\'], ['n', '^/'], ['o', '0'], ['p', '|*'], ['q', '(_,)'],
     ['r', 'I2'], ['s', '5'], ['t', '7'], ['u', '(_)'], ['v', '\/'], ['w', '\/\/'], ['x', '><'], ['y', 'j'], ['z', '2'],
-
 ]);
 
 const convertHacker = (phrase) => {
-
     alphabetHacker.forEach((hacker, abecedario) => {
         phrase = phrase.replaceAll(abecedario, hacker);
     })
+
     console.log(phrase);
 }
 
