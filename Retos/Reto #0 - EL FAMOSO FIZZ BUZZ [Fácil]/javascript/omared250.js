bf9504283f55a48865cd703d32bbd602c7dad6e9
Omared250 <79874354+Omared250@users.noreply.github.com>
2024-07-10 18:05:43 +0200

    Reto #0 - JavaScript

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/javascript/omared250.js
@@ -0,0 +1,16 @@
+
+const fizzBuzz = (num) => {
+    for (let i = 1; i <= num; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("fizzbuzz");
+        } else if (i % 3 === 0) {
+            console.log("fizz");
+        } else if (i % 5 === 0) {
+            console.log("buzz");
+        } else {
+            console.log(i);
+        }
+    }
+};
+
+fizzBuzz(100);
