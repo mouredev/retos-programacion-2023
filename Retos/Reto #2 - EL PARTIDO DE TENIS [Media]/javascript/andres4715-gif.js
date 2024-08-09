64fee201fb9bcb73090c6574eb4f4265a8023221
andres-rios-intelex <147648981+andres-rios-intelex@users.noreply.github.com>
2024-08-08 22:44:02 -0500

    new comments file

--- Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/javascript/andres4715-gif.js
+++ Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/javascript/andres4715-gif.js
@@ -1,18 +1,18 @@
 /*
- * Escribe un programa que muestre cómo transcurre un juego de tenis y quién lo ha ganado.
- * El programa recibirá una secuencia formada por "P1" (Player 1) o "P2" (Player 2), según quien
- * gane cada punto del juego.
- * 
- * - Las puntuaciones de un juego son "Love" (cero), 15, 30, 40, "Deuce" (empate), ventaja.
- * - Ante la secuencia [P1, P1, P2, P2, P1, P2, P1, P1], el programa mostraría lo siguiente:
- *   15 - Love
- *   30 - Love
- *   30 - 15
- *   30 - 30
- *   40 - 30
- *   Deuce
- *   Ventaja P1
- *   Ha ganado el P1
- * - Si quieres, puedes controlar errores en la entrada de datos.   
- * - Consulta las reglas del juego si tienes dudas sobre el sistema de puntos.   
+ Escribe un programa que muestre cómo transcurre un juego de tenis y quién lo ha ganado.
+ El programa recibirá una secuencia formada por "P1" (Player 1) o "P2" (Player 2), según quien
+ gane cada punto del juego.
+ 
+ - Las puntuaciones de un juego son "Love" (cero), 15, 30, 40, "Deuce" (empate), ventaja.
+ - Ante la secuencia [P1, P1, P2, P2, P1, P2, P1, P1], el programa mostraría lo siguiente:
+   15 - Love
+   30 - Love
+   30 - 15
+   30 - 30
+   40 - 30
+   Deuce
+   Ventaja P1
+   Ha ganado el P1
+ - Si quieres, puedes controlar errores en la entrada de datos.   
+ - Consulta las reglas del juego si tienes dudas sobre el sistema de puntos.   
  */
