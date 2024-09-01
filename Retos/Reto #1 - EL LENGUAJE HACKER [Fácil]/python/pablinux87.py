f693dc55c4771c6b7fbee271c0da4d42ef81c1e8
pablinux87 <pabloratero@gmail.com>
2024-08-31 22:33:56 -0300

    Primeros ejercicios

---
+++ Retos/Reto #1 - EL LENGUAJE HACKER [Fácil]/python/pablinux87.py
@@ -0,0 +1,37 @@
+# Reto #1: EL "LENGUAJE HACKER"
+#### Dificultad: Fácil | Publicación: 02/01/23 | Corrección: 09/01/23
+
+## Enunciado
+
+'''
+/*
+ * Escribe un programa que reciba un texto y transforme lenguaje natural a
+ * "lenguaje hacker" (conocido realmente como "leet" o "1337"). Este lenguaje
+ *  se caracteriza por sustituir caracteres alfanuméricos.
+ * - Utiliza esta tabla (https://www.gamehouse.com/blog/leet-speak-cheat-sheet/) 
+ *   con el alfabeto y los números en "leet".
+ *   (Usa la primera opción de cada transformación. Por ejemplo "4" para la "a")
+ */
+'''
+
+def leet_code(text:str)->str:
+    leet_text=''
+    try:
+        for letter in text:
+            if letter.upper() in leet_char.keys():
+                leet_text += leet_char[letter.upper()]
+            else:
+                leet_text += letter
+    except Exception as e:
+        print('Ocurrio un error que no estamos manejando...', e)
+        
+    return leet_text
+
+
+leet_char = {"A": "4", "B": "I3", "C": "[", "D": ")", "E": "3", "F": "|=", "G": "&", "H": "#", "I": "1",
+            "J": ",_|", "K": ">|", "L": "1", "M": "/\/\\", "N": " ^/", "O": "0", "P": " |*", "Q": "(_,)",
+            "R": "I2", "S": "5", "T": "7", "U": "(_)", "V": "\/", "W": "\/\/", "X": "><", "Y": "j", "Z": "2",
+            "1": "L", "2": "R", "3": "E", "4": "A", "5": "S", "6": "b", "7": "T", "8": "B", "9": "g", "0": "o"}
+
+if __name__=='__main__':
+    print(leet_code(input('Ingrese el texto a codificar: ')))
