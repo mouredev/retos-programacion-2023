f07355f43e0469edc26fb03179711743f58683ce
andresRios <andres4715@gmail.com>
2024-10-04 07:17:51 -0500

    reto3 javascript

---
+++ Retos/Reto #3 - EL GENERADOR DE CONTRASEÑAS [Media]/javascript/andres4715-gif.js
@@ -0,0 +1,48 @@
+/*
+ * Escribe un programa que sea capaz de generar contraseñas de forma aleatoria.
+ * Podrás configurar generar contraseñas con los siguientes parámetros:
+ * - Longitud: Entre 8 y 16.
+ * - Con o sin letras mayúsculas.
+ * - Con o sin números.
+ * - Con o sin símbolos.
+ * (Pudiendo combinar todos estos parámetros entre ellos)
+ */
+
+const data = {
+  CAPS: 'ABCDEFGHIJKLMNÑOPKRSTUVWXYZ',
+  LOWERCASE: 'abcdefghijklmnñopkrstuvwxyz',
+  DIGITS: '1234567890',
+  SYMBOLS: '!#$%&*+-:<=>?@_|~',
+};
+
+const howLongThePasswordIs = 10;
+const role = {
+  MIN: 8,
+  MAX: 16,
+};
+
+const mix = [data.CAPS, data.DIGITS, data.LOWERCASE, data.SYMBOLS]
+  .toString()
+  .replace(',', '')
+  .split('')
+  .sort(() => Math.random() - 0.5)
+  .join('');
+
+const mixing = (strData) => {
+  const finalData = strData
+    .slice(0, howLongThePasswordIs)
+    .split('')
+    .sort(() => Math.random() - 0.5)
+    .join('')
+    .toString();
+  return finalData;
+};
+
+if (howLongThePasswordIs >= role.MIN && howLongThePasswordIs <= role.MAX) {
+  console.log('✅✅✅ Generating your new password...');
+  console.log(`Password with CAPS: ${mixing(data.CAPS)}`);
+  console.log(`Password with lowerCase: ${mixing(data.LOWERCASE)}`);
+  console.log(`Password with digits: ${mixing(data.DIGITS)}`);
+  console.log(`Password with symbols: ${mixing(data.SYMBOLS)}`);
+  console.log(`Mixing Password: ${mixing(mix)}`);
+}
