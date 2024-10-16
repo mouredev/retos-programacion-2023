274a1d95aa35d70cb3646c02dd17badf6f8a6c95
andresRios <andres4715@gmail.com>
2024-10-16 17:28:19 -0500

    reto19

---
+++ Retos/Reto #19 - ANÁLISIS DE TEXTO [Media]/javascript/andres4715-gif.js
@@ -0,0 +1,9 @@
+/*
+ * Crea un programa que analice texto y obtenga:
+ * - Número total de palabras.
+ * - Longitud media de las palabras.
+ * - Número de oraciones del texto (cada vez que aparecen un punto).
+ * - Encuentre la palabra más larga.
+ *
+ * Todo esto utilizando un único bucle.
+ */
