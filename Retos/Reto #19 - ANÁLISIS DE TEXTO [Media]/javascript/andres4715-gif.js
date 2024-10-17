a8f3014e9646d9d5e790e9a204623b1452c5a632
andresRios <andres4715@gmail.com>
2024-10-17 09:06:09 -0500

    new data

--- Retos/Reto #19 - ANÁLISIS DE TEXTO [Media]/javascript/andres4715-gif.js
+++ Retos/Reto #19 - ANÁLISIS DE TEXTO [Media]/javascript/andres4715-gif.js
@@ -7,3 +7,35 @@
  *
  * Todo esto utilizando un único bucle.
  */
+
+const text =
+  'La vida es bella. la vida. es linda. y llego la pandemia y todos para la casa en cuarentena';
+
+let totalPalabras = [];
+let totalCaracteres = [];
+let data;
+let filtering;
+let media;
+let numeroOraciones;
+
+const textValidation = (text) => {
+  text.split(' ').forEach((x) => {
+    totalPalabras.push(x);
+    totalCaracteres.push(x.length);
+    data = Math.max(...totalCaracteres);
+    filtering = totalPalabras.filter((x) => x.length === data).join();
+    media = totalPalabras.reduce((a, b) => a + b, 0);
+    numeroOraciones = `${text.split('. ').length}`;
+  });
+};
+
+textValidation(text);
+
+console.log(
+  `- Total Palabras: ${totalPalabras.length}\n`,
+  '- Longitud media:',
+  media.length / totalPalabras.length,
+  '\n',
+  `- Número de oraciones: ${numeroOraciones}\n`,
+  `- Palabra más larga ${filtering} con ${data} caracteres.\n`
+);
