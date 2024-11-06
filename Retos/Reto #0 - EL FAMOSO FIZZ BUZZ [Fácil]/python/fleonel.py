b6f2bfc2c6db4ceadc9592ca2f8c2f5068659c59
Dennis Ramirez <dr4860@gmail.com>
2024-11-05 22:25:57 -0600

    Primer aporte a la comunidad

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/python/fleonel.py
@@ -0,0 +1,13 @@
+def fizzbuzz(n: int):
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("fizzbuzz")
+        elif i % 3 == 0:
+            print("fizz")
+        elif i % 5 == 0:
+            print("buzz")
+        else:
+            print(i)
+
+
+fizzbuzz(100)
