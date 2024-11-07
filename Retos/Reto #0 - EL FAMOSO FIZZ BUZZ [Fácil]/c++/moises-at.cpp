be30540ad287772517d7bc1325adf4a7b46220af
Moisés Acevedo <150295094+moises-at@users.noreply.github.com>
2024-11-06 20:45:26 -0500

    reto 0 en c++

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/c++/moises-at.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void fizzbuzz() {
+    for (int x = 1; x <= 100; ++x) {
+        if (x % 15 == 0) {
+            cout << "fizzbuzz" << endl;
+        } else if (x % 3 == 0) {
+            cout << "fizz" << endl;
+        } else if (x % 5 == 0) {
+            cout << "buzz" << endl;
+        } else {
+            cout << x << endl;
+        }
+    }
+}
+
+int main() {
+    fizzbuzz();
+    return 0;
+}
