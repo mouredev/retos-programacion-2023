f4f8e4690aba440b4dbe6332bbb570f7a28cc550
Oscar Gutiérrez <98203302+iRetr0o@users.noreply.github.com>
2024-11-29 19:18:11 -0600

    feat: Reto #0 - Kotlin

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/kotlin/iRetr0o.kt
@@ -0,0 +1,17 @@
+fun main() {
+    fizzBuzz(100)
+}
+
+fun fizzBuzz(n: Int) {
+    for (i in 1..n) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            println("FizzBuzz")
+        } else if (i % 3 == 0) {
+            println("Fizz")
+        } else if (i % 5 == 0) {
+            println("Buzz")
+        } else {
+            println(i)
+        }
+    }
+}
