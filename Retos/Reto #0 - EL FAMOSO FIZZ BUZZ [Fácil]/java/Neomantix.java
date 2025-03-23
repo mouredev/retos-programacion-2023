620bbaff1a9cab7b0458aa9f6d391afb36d4f1b5
edgar.sanchez <edgar.sanchez@netexlearning.com>
2025-03-23 15:25:53 +0100

    Solucion de Neomantix para Reto #0 - java

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/java/Neomantix.java
@@ -0,0 +1,25 @@
+package neomantix;
+
+public class Neomantix {
+
+	public static void main(String[] args) {
+
+		for(int i=1; i<=100; i++) {
+			boolean divisiblePor3 = (i%3==0);
+			boolean divisiblePor5 = (i%5==0);
+			
+			if(divisiblePor3 && divisiblePor5) {
+				System.out.println(i + " fizzbuzz");
+			} else if(divisiblePor3) {
+				System.out.println(i + " fizz");
+			} else if(divisiblePor5) {
+				System.out.println(i + " buzz");
+			} else {
+				System.out.println(i);
+			}
+		}
+		
+	}
+
+}
+
