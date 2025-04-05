3413a454a82b22c0010945e723bc2c4ce688b957
Oskar Rivera <oscardary@gmail.com>
2025-04-05 14:07:54 -0500

    Update

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/typescript/oscardary.ts
@@ -0,0 +1,25 @@
+/*
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */
+
+console.log("por fiiiiiiinnnn")
+
+/*
+let inicio = 1
+
+while (inicio <= 100) {
+    let multiplo3 = inicio % 3
+    let multiplo5 = inicio % 5
+
+    console.log(inicio)
+    if (multiplo3 == 0 && multiplo5 == 5) {
+        console.log("fizzbuzz")
+    }
+    inicio++
+}
+*/
